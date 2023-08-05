3974fd55a30960754e4a64f5f5f98d74b3a4b6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:41 +0000

    Add package tfx/0.30.0rc0/tfx-0.30.0rc0-py3-none-any.whl

---
+++ packages/tfx/tfx-0.30.0rc0-py3-none-any.whl/tfx/examples/chicago_taxi_pipeline/taxi_pipeline_simple_test.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for tfx.examples.chicago_taxi_pipeline.taxi_pipeline_simple."""
+
+import datetime
+from airflow import models
+
+import tensorflow as tf
+
+from tfx.examples.chicago_taxi_pipeline import taxi_pipeline_simple
+from tfx.orchestration.airflow.airflow_dag_runner import AirflowDagRunner
+from tfx.orchestration.airflow.airflow_dag_runner import AirflowPipelineConfig
+from tfx.utils import test_case_utils
+
+
+class TaxiPipelineSimpleTest(test_case_utils.TfxTest):
+
+  def setUp(self):
+    super(TaxiPipelineSimpleTest, self).setUp()
+    self._test_dir = self.tmp_dir
+
+  def testTaxiPipelineCheckDagConstruction(self):
+    airflow_config = {
+        'schedule_interval': None,
+        'start_date': datetime.datetime(2019, 1, 1),
+    }
+    logical_pipeline = taxi_pipeline_simple._create_pipeline(
+        pipeline_name='Test',
+        pipeline_root=self._test_dir,
+        data_root=self._test_dir,
+        module_file=self._test_dir,
+        serving_model_dir=self._test_dir,
+        metadata_path=self._test_dir,
+        beam_pipeline_args=[])
+    self.assertEqual(9, len(logical_pipeline.components))
+    pipeline = AirflowDagRunner(
+        AirflowPipelineConfig(airflow_config)).run(logical_pipeline)
+    self.assertIsInstance(pipeline, models.DAG)
+
+
+if __name__ == '__main__':
+  tf.test.main()
