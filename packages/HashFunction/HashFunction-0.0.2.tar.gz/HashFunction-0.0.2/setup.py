93544beee6a10ae5cf531748db2e5feaf721295a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package HashFunction/0.0.2/HashFunction-0.0.2.tar.gz

---
+++ packages/HashFunction/HashFunction-0.0.2.tar.gz/HashFunction-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="HashFunction",
+    version="0.0.2",
+    author="Matheus Nobre Gomes",
+    author_email="matt-gomes@live.com",
+    description="Hashing module",
+    license="GPLv3+",
+    keywords=["hashing", "hash", "function"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ccr5/hashing-python-module",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
