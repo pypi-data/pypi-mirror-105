d4bff05aa0252f78e5083b9ef7b8482581a60515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:01 +0000

    Add package pyezxl/1.3.7/pyezxl-1.3.7.tar.gz

---
+++ packages/pyezxl/pyezxl-1.3.7.tar.gz/pyezxl-1.3.7/README.md
@@ -0,0 +1,7 @@
+include LICENSE  
+include README.md   
+include requirements.txt  
+
+This module is for easily read/write with EXCEL using Python.  
+The target is for easily control for EXCEL.  
+Therefore pyezxl is focused to control easily as follows.    
---
+++ packages/pyezxl/pyezxl-1.3.7.tar.gz/pyezxl-1.3.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+setup(name='pyezxl',
+      version='1.3.7',
+      url='https://github.com/sjpark/pyezxl',
+      download_url='https://github.com/sjpark/pyezxl/archive/v0.0.6.tar.gz',
+      author='sjpark',
+      author_email='sjpkorea@yahoo.com',
+      description='Read / Write Excel using Python',
+
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      include_package_data=True,
+      package_data={
+            "pyezxl": ["excel_addin/*.*"],
+            "pyezxl": ["pyezxl_code/*.*"],
+            "pyezxl": ["pyezxl_history/*.*"],
+            "pyezxl": ["pyezxl_manual/*.*"],
+            "pyezxl": ["pyezxl_menu/*.*"],
+            "pyezxl": ["user_code/*.*"],
+            "pyezxl": ["user_menu/*.*"],
+      },
+      long_description=open('README.md').read(),
+      install_requires=[''],
+      python_requires='>=3.5',
+      zip_safe=False,
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+      ],
+)
+
