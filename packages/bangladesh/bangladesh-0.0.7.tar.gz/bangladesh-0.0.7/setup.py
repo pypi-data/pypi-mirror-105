6f3c3656db471c7566251340e0cbb860197a04ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package bangladesh/0.0.7/bangladesh-0.0.7.tar.gz

---
+++ packages/bangladesh/bangladesh-0.0.7.tar.gz/bangladesh-0.0.7/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.txt"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.7'
+DESCRIPTION = 'A python package For Bangladesh information (District, Division, Thana, post code and etc)'
+LONG_DESCRIPTION = ''
+
+setup(
+    name="bangladesh",
+    version=VERSION,
+    author="zhdruvo (Zahid Hasan)",
+    author_email="<zhdruvo@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    license='MIT License',
+    include_package_data=True,
+    package_data={'': ['data/*.json']},
+    url='https://github.com/Druvo',
+    install_requires=[],
+    Platform='python',
+    keywords=['Bangladesh', 'District', 'Division', 'Thana', 'upazilas'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
