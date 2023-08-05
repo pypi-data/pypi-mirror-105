0fffb6025bc5f167cf2285837e36187e9a7e12fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package mypy-boto3-marketplace-catalog/1.17.68.0/mypy-boto3-marketplace-catalog-1.17.68.0.tar.gz

---
+++ packages/mypy-boto3-marketplace-catalog/mypy-boto3-marketplace-catalog-1.17.68.0.tar.gz/mypy-boto3-marketplace-catalog-1.17.68.0/mypy_boto3_marketplace_catalog/literals.py
@@ -0,0 +1,27 @@
+"""
+Type annotations for marketplace-catalog service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_marketplace_catalog/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_marketplace_catalog.literals import ChangeStatus
+
+    data: ChangeStatus = "APPLYING"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("ChangeStatus", "FailureCode", "SortOrder")
+
+
+ChangeStatus = Literal["APPLYING", "CANCELLED", "FAILED", "PREPARING", "SUCCEEDED"]
+FailureCode = Literal["CLIENT_ERROR", "SERVER_FAULT"]
+SortOrder = Literal["ASCENDING", "DESCENDING"]
