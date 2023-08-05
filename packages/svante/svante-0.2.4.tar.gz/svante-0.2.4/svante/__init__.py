bafa7f5770b38925db9e2d4ea0d92c17b4efe4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:29 +0000

    Add package svante/0.2.4/svante-0.2.4.tar.gz

---
+++ packages/svante/svante-0.2.4.tar.gz/svante-0.2.4/svante/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+r"""Svante -- configurable Arrhenius plots and fits.
+
+\b
+For complete help visit https://github.com/hydrationdynamics/svante
+\b
+License: BSD-3-Clause
+Copyright © 2021, GenerisBio LLC.
+All rights reserved.
+"""
