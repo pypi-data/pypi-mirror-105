c3930e10a2a03e0cb3b0e0f5ee1e2ff5155d6d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package apocrypha/2.2.0/apocrypha-2.2.0-py2.py3-none-any.whl

---
+++ packages/apocrypha/apocrypha-2.2.0-py2.py3-none-any.whl/apocrypha/exceptions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''
+exceptions for other modules
+'''
+
+
+class PeerCreateFailed(Exception):
+    '''
+    encapsulates the various network errors that can happen while connecting
+    '''
+
+
+class FailedQuery(Exception):
+    ''' notifies the caller that the query failed, the node will handle
+    attempting to recover the peer
+    '''
+
+
+class DatabaseError(Exception):
+    ''' used by Apocrypha.error()
+
+    these denote something went wrong with a query, incorrect types, incorrect
+    usage, etc
+    '''
