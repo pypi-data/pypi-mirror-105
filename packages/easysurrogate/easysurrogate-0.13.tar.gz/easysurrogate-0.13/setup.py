a98ae4e5d1b7419d92fda0dc1eba0d5d0c61a1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:37 +0000

    Add package easysurrogate/0.13/easysurrogate-0.13.tar.gz

---
+++ packages/easysurrogate/easysurrogate-0.13.tar.gz/easysurrogate-0.13/setup.py
@@ -0,0 +1,20 @@
+from os import path
+from setuptools import setup, find_packages
+
+setup(
+    name='easysurrogate',
+
+    version=0.13,
+
+    description=(''),
+
+#    url='https://readthedocs.org/projects/easyvvuq/',
+
+    author='CWI',
+
+    install_requires=['numpy', 'scipy', 'matplotlib', 'h5py', 'sklearn'],
+
+    packages=find_packages(),
+
+    include_package_data=True,
+)
