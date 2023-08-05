19c9da17515bddf715f9a8b0a1c0f9c8c6117452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:29 +0000

    Add package herdpy/0.0.5/herdpy-0.0.5.tar.gz

---
+++ packages/herdpy/herdpy-0.0.5.tar.gz/herdpy-0.0.5/tests/test_client.py
@@ -0,0 +1,25 @@
+from herdpy.client import Report
+
+
+class FakeResponse:
+    status_code = 200
+
+class FakeSession:
+    def post(self, url, json={}):
+        return FakeResponse()
+
+
+def test_client():
+
+    sess = FakeSession()
+
+    r = Report(sess)
+
+    r.url = "http://nowhere.local"
+    r.label = "testclient"
+    r.system = "testgroup"
+    r.status_code = "E200OK"
+    r.add_tag("test")
+    r.add_tag("dev")
+
+    r.send() # This should not raise an exception
