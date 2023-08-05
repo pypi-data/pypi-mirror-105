2b502ebdb35602e417b041f4b53468f51fdde58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package dkist-processing-common/0.1.0rc4/dkist-processing-common-0.1.0rc4.tar.gz

---
+++ packages/dkist-processing-common/dkist-processing-common-0.1.0rc4.tar.gz/dkist-processing-common-0.1.0rc4/dkist_processing_common/models/constants.py
@@ -0,0 +1,14 @@
+"""
+Components of the Constant model
+"""
+from enum import Enum
+
+
+class BudName(str, Enum):
+    """
+    Controlled list of names for constant stems (buds)
+    """
+
+    instrument = "INSTRUMENT"
+    num_cs_steps = "NUM_CS_STEPS"
+    num_modstates = "NUM_MODSTATES"
