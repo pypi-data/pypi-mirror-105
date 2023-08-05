645b7783d594a2b15af18d3b10fe5929914c1c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:34 +0000

    Add package polidoro-cli/3.0.1/polidoro-cli-3.0.1.tar.gz

---
+++ packages/polidoro-cli/polidoro-cli-3.0.1.tar.gz/polidoro-cli-3.0.1/polidoro_cli/clis/docker.py
@@ -0,0 +1,39 @@
+import os
+from string import Template
+
+import yaml
+from polidoro_argument import Command
+
+from polidoro_cli import CLI
+
+
+class Docker(object):
+    @staticmethod
+    def _get_main_docker_compose_service():
+        for name, info in Docker._gel_all_docker_compose_services().items():
+            if 'build' in info:
+                return name
+
+    @staticmethod
+    def _gel_all_docker_compose_services():
+        if os.path.exists('docker-compose.yml'):
+            with open('docker-compose.yml') as file:
+                return yaml.load(file, Loader=yaml.FullLoader)['services']
+        return {}
+
+    @staticmethod
+    def command_interceptor(command, *remainders):
+        if remainders and list(remainders)[0] in Docker._gel_all_docker_compose_services():
+            service = ''
+        else:
+            service = Docker._get_main_docker_compose_service()
+        return Template(command).safe_substitute(service=service).replace('  ', ' ').strip()
+
+    @staticmethod
+    @Command(help='Stop all containers')
+    def stop_all():
+        services = CLI.execute('docker ps -q', capture_output=True, show_cmd=False).stdout.split()
+        if services:
+            CLI.execute('docker stop ' + ' '.join(services))
+        print('Stopped all containers')
+
