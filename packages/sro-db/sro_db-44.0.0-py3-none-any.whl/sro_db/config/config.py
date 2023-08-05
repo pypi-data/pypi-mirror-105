39911cfc4dee6082158817d116250c3a427d6554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package sro-db/44.0.0/sro_db-44.0.0-py3-none-any.whl

---
+++ packages/sro-db/sro_db-44.0.0-py3-none-any.whl/sro_db/config/config.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+import os
+from pprint import pprint
+
+
+try:
+
+    SQLALCHEMY_DATABASE_URI = f"postgres://{os.environ['USERDB']}:{os.environ['PASSWORDDB']}@{os.environ['HOST']}/{os.environ['DBNAME']}"
+
+except Exception as e:
+    print(e)
+    print("Favor configurar as variáveis de ambiente: [USERDB, PASSWORDDB, HOST, DBNAME]")
+    exit(1)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+session_factory = sessionmaker(bind=engine)
+session = scoped_session(session_factory)
+
+
+session = session()
+Base = declarative_base()
+
+class Config():
+
+    def create_database(self):
+        Base.metadata.create_all(engine)
+
