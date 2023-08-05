4f33dcd9b60e0fc6efca088d6f0d402724bae5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:02 +0000

    Add package Recursive-Symmetry-Aware-Materials-Microstructure-Explorer/0.0.12/Recursive_Symmetry_Aware_Materials_Microstructure_Explorer-0.0.12.tar.gz

---
+++ packages/Recursive-Symmetry-Aware-Materials-Microstructure-Explorer/Recursive_Symmetry_Aware_Materials_Microstructure_Explorer-0.0.12.tar.gz/Recursive_Symmetry_Aware_Materials_Microstructure_Explorer-0.0.12/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('./requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Recursive_Symmetry_Aware_Materials_Microstructure_Explorer',
+    version='0.0.12',
+    packages=find_packages(where="src"),
+    url='',
+    install_requires=requirements,
+    license=' BSD-3-Clause',
+    author='Tri N. M. Nguyen, Yichen Guo, Shuyu Qin, Joshua C. Agar',
+    author_email='jca318@lehigh.edu',
+    description='Tool for recursive symmetry aware searching of materials microstructure images',
+    classifiers = [
+                  "Programming Language :: Python :: 3",
+                  "License :: OSI Approved :: MIT License",
+                  "Operating System :: OS Independent",
+              ],
+              package_dir = {"": "src"},
+                            python_requires = ">=3.6",
+)
+
