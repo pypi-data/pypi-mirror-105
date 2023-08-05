8f364aee39ad46eb6378b0ac861a4b587612b50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:48 +0000

    Add package bluebelt/0.1.3/bluebelt-0.1.3-py3-none-any.whl

---
+++ packages/bluebelt/bluebelt-0.1.3-py3-none-any.whl/bluebelt/__init__.py
@@ -0,0 +1,11 @@
+'''
+Bluebelt
+'''
+from bluebelt.core import series
+from bluebelt.core import dataframe
+from bluebelt.helpers import check
+from bluebelt.helpers import subsets
+
+from bluebelt.styles import paper
+
+import bluebelt.templates
