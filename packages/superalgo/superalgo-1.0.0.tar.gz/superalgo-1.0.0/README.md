9fd52e1625f0cbf21b5eda11929d83d1873d7afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:40 +0000

    Add package superalgo/1.0.0/superalgo-1.0.0.tar.gz

---
+++ packages/superalgo/superalgo-1.0.0.tar.gz/superalgo-1.0.0/README.md
@@ -0,0 +1,44 @@
+# Python sorting/searching algorithms
+
+
+The **algorithms3x** is a python package with 5 sorting/searching algorithms. The package would keep updating as python itself updates to higher versions.
+
+It contains the following algorithms:
+
+- Selection sort
+- Bubble sort
+- Linear search
+- Binary search
+- Merge sort
+
+# Installation
+If not already [install pip](https://pip.pypa.io/en/stable/installing/)
+
+Install the package with `pip` or `pip3`:
+
+```bash
+pip install python-algorithms-3x
+```
+
+# Usage
+### Example:
+
+```Python
+from algorithms3x.search import linear_search
+linear_search([1, 2, 3], 4)
+```
+
+Output:
+```Python
+False
+```
+
+```Python
+from algorithms3x.sort import merge_sort
+print(merge_sort([4, 1, 2, 3]))
+```
+
+Output:
+```Python
+[1, 2, 3, 4]
+```
---
+++ packages/superalgo/superalgo-1.0.0.tar.gz/superalgo-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import pathlib
+
+from setuptools import find_packages, setup
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name="superalgo",
+    version="1.0.0",
+    description="Python sorting and searching algorithms",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Harvard90873/algorithms",
+    author="Harvard90873",
+    author_email="harvard90873@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(exclude=("tests", "build")),
+    include_package_data=False,
+    install_requires=["data-structures3x"],
+)
