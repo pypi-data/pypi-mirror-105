dd7177b38c876e3b416d5279e3f35bbc78f40866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:15 +0000

    Add package mipkit/1.4.3/mipkit-1.4.3-py3-none-any.whl

---
+++ packages/mipkit/mipkit-1.4.3-py3-none-any.whl/mipkit/__init__.py
@@ -0,0 +1,11 @@
+from .images import *
+from .vis import *
+from .faces import *
+from .sys import *
+from .multiprocessing import *
+from .utils import *
+from .pytorch import *
+from .stats import *
+from .debuggers import *
+
+__author__ = "Cong M. Vo"
---
+++ packages/mipkit/mipkit-1.4.3-py3-none-any.whl/mipkit/debuggers.py
@@ -0,0 +1,36 @@
+# ===============================================================================
+# DEBUGGER
+#
+# References
+# [1] https://stackoverflow.com/questions/16867347/step-by-step-debugging-with-ipython
+# ===============================================================================
+import IPython
+import ipdb
+import pdb
+
+
+class Debugger():
+    def __init__(self, method='ipdb'):
+        """Debugger with available methods: ipdb, pdb or ipython
+
+        Args:
+            method (str, optional): debugger method. Defaults to 'ipdb'.
+        """
+        self.tracer = self.init_tracer(method)
+
+    def init_tracer(self, method):
+        assert method in ['ipdb', 'pdb', 'ipython']
+        if method == 'ipdb':
+            return ipdb.set_trace
+        elif method == 'pdb':
+            return pdb.set_trace
+        elif method == 'ipython':
+            return IPython.embed
+
+    def set_trace(self, method=None):
+        if method is not None:
+            self.tracer = self.init_tracer(method)
+        return self.tracer
+
+
+set_trace = Debugger().set_trace()
