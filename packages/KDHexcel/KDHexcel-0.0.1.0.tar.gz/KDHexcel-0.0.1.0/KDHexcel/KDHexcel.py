e4eefe732717f745c52d076f496c0a6abbf8ccbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:33 +0000

    Add package KDHexcel/0.0.1.0/KDHexcel-0.0.1.0.tar.gz

---
+++ packages/KDHexcel/KDHexcel-0.0.1.0.tar.gz/KDHexcel-0.0.1.0/KDHexcel/KDHexcel.py
@@ -0,0 +1,13 @@
+import win32com.client
+import io
+from PyQt5.QtGui import QImage
+from PyQt5.QtWidgets import QApplication
+import pandas
+import numpy
+
+class KDHexcel:
+    __version__ = 1.0
+
+    def _open(self):
+        print("aa")
+
