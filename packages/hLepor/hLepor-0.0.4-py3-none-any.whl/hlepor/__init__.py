6b3a00d84fa501b6bf293959fb48c73b7ac3f270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package hLepor/0.0.4/hLepor-0.0.4-py3-none-any.whl

---
+++ packages/hLepor/hLepor-0.0.4-py3-none-any.whl/hlepor/__init__.py
@@ -0,0 +1,4 @@
+"""hLepor is a python package to calculate hLepor scores"""
+
+from .hlepor import single_hlepor_score
+from .hlepor import hlepor_score
---
+++ packages/hLepor/hLepor-0.0.4-py3-none-any.whl/hlepor/setup.py
@@ -0,0 +1,2 @@
+import nltk
+nltk.download('punkt') #for word_tokenize()
