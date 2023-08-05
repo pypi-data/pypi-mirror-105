2f577af4c02742e81f332325709ca2cabe977756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:42 +0000

    Add package wydtest/2.1.5/wydtest-2.1.5.tar.gz

---
+++ packages/wydtest/wydtest-2.1.5.tar.gz/wydtest-2.1.5/setup.py
@@ -0,0 +1,27 @@
+
+#############################################
+# File Name: setup.py
+# Author: LiangjunFeng
+# Mail: zhumavip@163.com
+# Created Time:  2018-4-16 19:17:34
+#############################################
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "wydtest",      #这里是pip项目发布的名称
+    version = "2.1.5",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "wyd","featureextraction"),
+    description = "Is your pytorch cuda available?",
+    long_description = "s your pytorch cuda available?",
+    license = "MIT Licence",
+
+    url = "https://github.com/bro3/wydtest",     #项目相关文件地址，一般是github
+    author = "bro3",
+    author_email = "yudwang@outlook.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []          #这个项目需要的第三方库
+)
---
+++ packages/wydtest/wydtest-2.1.5.tar.gz/wydtest-2.1.5/wydtest/test.py
@@ -0,0 +1,2 @@
+from torch.cuda import is_available as a
+print(a())
