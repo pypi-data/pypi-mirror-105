b68e08a4b0ab46d6951edee367004dd75a6c70d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package clarinpl-embeddings/0.0.1rc6/clarinpl_embeddings-0.0.1rc6-py3-none-any.whl

---
+++ packages/clarinpl-embeddings/clarinpl_embeddings-0.0.1rc6-py3-none-any.whl/experimental/embeddings/tasks/sequence_tagging.py
@@ -0,0 +1,27 @@
+from flair.data import Dictionary
+from flair.embeddings import TransformerWordEmbeddings
+from flair.models import SequenceTagger
+
+from experimental.embeddings.tasks.base_task import BaseTask
+
+
+class FlairSequenceTagger(BaseTask):
+    def __init__(
+        self,
+        embeddings: TransformerWordEmbeddings,
+        hidden_dim: float,
+        tag_dictionary: Dictionary,
+        output_path: str,
+        use_crf: bool = True,
+    ):
+        self.model_hparams = {
+            "hidden_size": hidden_dim,
+            "use_crf": use_crf,
+        }
+        model = SequenceTagger(
+            embeddings=embeddings,
+            tag_dictionary=tag_dictionary,
+            tag_type="tag",
+            **self.model_hparams
+        )
+        super().__init__(model, output_path)
