46b3db0a1454ad318294b267822d2e68d90e7fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:50 +0000

    Add package get-gifNimage/0.3.3/get_gifNimage-0.3.3.tar.gz

---
+++ packages/get-gifNimage/get_gifNimage-0.3.3.tar.gz/get_gifNimage-0.3.3/__init__.py
@@ -0,0 +1,14 @@
+from .get_gifNimage import get_gifNimage
+
+
+"""
+For unknown reason when I rename CHANGELOG.TXT to CHANGELOG.md -
+ it can't find the file when I try to install the module in Jupyter Notebook.
+ I tried to use the code below but without a success. At this point, I will live CHANGELOG.txt version.
+"""
+# import logging
+# try:
+#     open('CHANGELOG.md').read()
+# except FileNotFoundError as fnfe:
+#     logging.exception('Caught an error [in code used: except FileNotFoundError]' + str(fnfe))
+#     print('Caught an error [in code used: except FileNotFoundError]')
