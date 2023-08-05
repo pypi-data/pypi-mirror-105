09b99451fa867e5014c1d86fa7d5bca6c6e01706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:09 +0000

    Add package TotalCoursePoints/1.1.0/TotalCoursePoints-1.1.0.tar.gz

---
+++ packages/TotalCoursePoints/TotalCoursePoints-1.1.0.tar.gz/TotalCoursePoints-1.1.0/TotalCoursePoints/__init__.py
@@ -0,0 +1,20 @@
+from .grade_bins import Bin, GradeBins, PNP
+from .classroom import Classroom
+from .student import Student
+from .category import Category, StudentCategoryData
+from .assignment import Assignment, StudentAssignmentData
+from .group import Group
+# from .gs_api_client import GradescopeAPIClient
+
+__all__ = [
+    "Classroom",
+    "Bin",
+    "GradeBins",
+    "Student",
+    "Category",
+    "StudentCategoryData",
+    "Assignment",
+    "StudentAssignmentData",
+    "Group",
+    "PNP"
+]
