4abcf3123ba99a3b92608ea0258e00995cbb21dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:17 +0000

    Add package gym-pool/0.1.19/gym-pool-0.1.19.tar.gz

---
+++ packages/gym-pool/gym-pool-0.1.19.tar.gz/gym-pool-0.1.19/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(name='gym-pool',
+      version='0.1.19',
+      url='https://github.com/to314as/gym-pool',
+      author='Tobias Oberkofler',
+      author_email='tobi20083@gmail.com',
+      package_dir={"": "."},
+      packages=find_packages(),
+      install_requires=['gym>=0.2.3',
+                        'numpy',
+                        'pygame',
+                        'zope.event',
+                        'torch',
+                        'torchvision'],
+      classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
+)
