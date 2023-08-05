9b2dd1154f1df021249db9e1e91ddf324b40a8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:30 +0000

    Add package everest-nlp/0.3/everest_nlp-0.3-py3-none-any.whl

---
+++ packages/everest-nlp/everest_nlp-0.3-py3-none-any.whl/everest_nlp/ner_devanagari/ner.py
@@ -0,0 +1,26 @@
+from nltk.tag import StanfordNERTagger
+import sys
+sys.path.append("..")
+from everest_nlp.config import stanford_jar_path,stanford_model_path
+from everest_nlp.sabda2vec.nepali_tokenizer import Tokenizer
+tok = Tokenizer()
+
+
+
+class DevanagriNER():
+    def __init__(self):
+        self.ner_tagger = StanfordNERTagger(stanford_model_path,stanford_jar_path)
+    
+    def tag_ner(self,tokens):
+        tagged_tuples = self.ner_tagger.tag(tokens)
+        return tagged_tuples
+
+
+if __name__ == '__main__':
+    print("Hello")
+    dner_obj = DevanagriNER()
+    test_text = "जित बहादुर खाम्चा आहिले सानेपा ललितपुरमा रहेको इन्फोदेवेलोपेर्स प्राइभेट लिमिटेडमा काम गर्दै हुनुहुन्छ"
+    test_tokens = tok.word_tokenize(test_text)
+    tagged_tuples = dner_obj.tag_ner(test_tokens)
+    print(test_tokens)
+    print(tagged_tuples)
