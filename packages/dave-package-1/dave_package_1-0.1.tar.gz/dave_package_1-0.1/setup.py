2f51d82787c294015878c939185b0dab814177bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:08 +0000

    Add package dave-package-1/0.1/dave_package_1-0.1.tar.gz

---
+++ packages/dave-package-1/dave_package_1-0.1.tar.gz/dave_package_1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dave_package_1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dave_package_1'],
+      zip_safe=False)
