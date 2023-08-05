3176b06bd25a634e18c9dfc50fbc0977a394cea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package pitci/0.1.1/pitci-0.1.1.tar.gz

---
+++ packages/pitci/pitci-0.1.1.tar.gz/pitci-0.1.1/pitci/__init__.py
@@ -0,0 +1,16 @@
+from pitci._version import __version__
+
+import pitci.base as base
+import pitci.checks as checks
+import pitci.dispatchers as dispatchers
+import pitci.helpers as helpers
+
+from pitci.dispatchers import (
+    get_leaf_node_scaled_conformal_predictor,
+    get_absolute_error_conformal_predictor,
+)
+
+try:
+    import pitci.xgboost as xgboost
+except ImportError:
+    pass
