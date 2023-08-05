97d11eee2f1af28a0521fbf9b9a86de5c7019911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:50 +0000

    Add package REPIC/1.2/REPIC-1.2.tar.gz

---
+++ packages/REPIC/REPIC-1.2.tar.gz/REPIC-1.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+	name='REPIC',
+	version='1.2',
+	description='Read Evaluate Print in Comments',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	author='David Pinney',
+	author_email='david@pinney.org',
+	url='https://github.com/dpinney/REPIC/',
+	py_modules=['REPIC']
+)
