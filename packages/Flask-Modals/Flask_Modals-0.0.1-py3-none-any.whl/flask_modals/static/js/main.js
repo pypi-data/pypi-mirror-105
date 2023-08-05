be6445ee0b8e99e90237c2c04df35fd8c0d4332a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:51 +0000

    Add package Flask-Modals/0.0.1/Flask_Modals-0.0.1-py3-none-any.whl

---
+++ packages/Flask-Modals/Flask_Modals-0.0.1-py3-none-any.whl/flask_modals/static/js/main.js
@@ -0,0 +1,19 @@
+// The .modal-backdrop div appears mutliple times when the form is 
+// submitted via ajax. We need to keep only one occurrence.
+document.documentElement.addEventListener('turbo:render', () => {
+  $('.modal').unbind('shown.bs.modal')
+  $('.modal').on('shown.bs.modal', function() {
+    const els = document.querySelectorAll('.modal-backdrop')
+    for (let i=0; i < els.length - 1; i++) {
+      els[i].remove()
+    }
+  })
+})
+
+document.documentElement.addEventListener('turbo:submit-start', () => {
+  NProgress.start()
+})
+
+document.documentElement.addEventListener('turbo:submit-end', () => {
+  NProgress.done()
+})
