a426d16e0140113171502150201497ce382337f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:11 +0000

    Add package limitless/0.0.2/limitless-0.0.2.tar.gz

---
+++ packages/limitless/limitless-0.0.2.tar.gz/limitless-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+        name="limitless", 
+        version=VERSION,
+        author="Obinna Okpokwasili",
+        author_email="obicarl@yahoo.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        entry_points={
+            'console_scripts': [
+                'limitless-ctl=limitless.main:main',
+            ]
+        },
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
