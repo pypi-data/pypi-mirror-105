78148f2c43a3176882fdba8d55ca3f6bf11cf0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:07 +0000

    Add package pyserde/0.3.2/pyserde-0.3.2-py3-none-any.whl

---
+++ packages/pyserde/pyserde-0.3.2-py3-none-any.whl/examples/runner.py
@@ -0,0 +1,44 @@
+import sys
+
+import any
+import collection
+import default
+import env
+import jsonfile
+import newtype
+import rename
+import rename_all
+import simple
+import skip
+import tomlfile
+import union
+import yamlfile
+
+
+def run_all():
+    run(any)
+    run(simple)
+    run(newtype)
+    run(collection)
+    run(default)
+    run(env)
+    run(jsonfile)
+    run(rename)
+    run(rename_all)
+    run(skip)
+    run(tomlfile)
+    run(yamlfile)
+    run(union)
+
+
+def run(module):
+    print('-----------------')
+    print(f'running {module.__name__}')
+    module.main()
+
+
+if __name__ == '__main__':
+    try:
+        run_all()
+    except Exception:
+        sys.exit(1)
