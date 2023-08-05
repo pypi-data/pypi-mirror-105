cba64d82088a2dd4b69de826743591be3fe535f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:37 +0000

    Add package mypy-boto3-textract/1.17.68.0/mypy_boto3_textract-1.17.68.0-py3-none-any.whl

---
+++ packages/mypy-boto3-textract/mypy_boto3_textract-1.17.68.0-py3-none-any.whl/mypy_boto3_textract/literals.py
@@ -0,0 +1,41 @@
+"""
+Type annotations for textract service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_textract/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_textract.literals import BlockType
+
+    data: BlockType = "CELL"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "BlockType",
+    "ContentClassifier",
+    "EntityType",
+    "FeatureType",
+    "JobStatus",
+    "RelationshipType",
+    "SelectionStatus",
+    "TextType",
+)
+
+
+BlockType = Literal["CELL", "KEY_VALUE_SET", "LINE", "PAGE", "SELECTION_ELEMENT", "TABLE", "WORD"]
+ContentClassifier = Literal["FreeOfAdultContent", "FreeOfPersonallyIdentifiableInformation"]
+EntityType = Literal["KEY", "VALUE"]
+FeatureType = Literal["FORMS", "TABLES"]
+JobStatus = Literal["FAILED", "IN_PROGRESS", "PARTIAL_SUCCESS", "SUCCEEDED"]
+RelationshipType = Literal["CHILD", "COMPLEX_FEATURES", "VALUE"]
+SelectionStatus = Literal["NOT_SELECTED", "SELECTED"]
+TextType = Literal["HANDWRITING", "PRINTED"]
