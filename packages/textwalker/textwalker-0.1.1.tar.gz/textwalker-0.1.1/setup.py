276b32e58aeb809c8e9ee7614012d7b8d5fd7c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package textwalker/0.1.1/textwalker-0.1.1.tar.gz

---
+++ packages/textwalker/textwalker-0.1.1.tar.gz/textwalker-0.1.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup()
+
+"""
+from setuptools import setup, find_packages
+
+setup(name='textwalker',
+      version='0.1.0',
+      description='intuitively parse text',
+      python_requires='>=3',
+      packages=find_packages()
+)
+"""
