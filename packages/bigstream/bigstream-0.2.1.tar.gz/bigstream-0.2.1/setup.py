50a490cbf1f6f1173293145ac4a02796e432d963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:02 +0000

    Add package bigstream/0.2.1/bigstream-0.2.1.tar.gz

---
+++ packages/bigstream/bigstream-0.2.1.tar.gz/bigstream-0.2.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name="bigstream",
+    version="0.2.1",
+    author="Greg M. Fleishman",
+    author_email="greg.nli10me@gmail.com",
+    description="Tools for distributed alignment of massive images",
+    url="https://github.com/GFleishman/bigstream",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-image',
+        'opencv-python',
+        'bokeh',
+        'dask',
+        'dask[array]',
+        'dask[delayed]',
+        'ClusterWrap>=0.1.3',
+        'greedypy',
+        'zarr',
+        'numcodecs',
+        'fishspot',
+        'dask-stitch>=0.0.2',
+    ]
+)
