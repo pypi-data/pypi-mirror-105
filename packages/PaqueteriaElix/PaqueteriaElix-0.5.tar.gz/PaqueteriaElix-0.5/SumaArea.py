f0e985ace05ab2fab753c256ca89da692e46bb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:21 +0000

    Add package PaqueteriaElix/0.5/PaqueteriaElix-0.5.tar.gz

---
+++ packages/PaqueteriaElix/PaqueteriaElix-0.5.tar.gz/PaqueteriaElix-0.5/SumaArea.py
@@ -0,0 +1,9 @@
+def sumas_prueba(x,y,z):
+    #suma=x+y+z
+    return x+y+z
+
+def area_cuadrado(Lado1,Lado2):
+    return Lado1*Lado2
+
+def area_triangulo(base,altura):
+    return (base*altura)/2
---
+++ packages/PaqueteriaElix/PaqueteriaElix-0.5.tar.gz/PaqueteriaElix-0.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(
+    name='PaqueteriaElix',
+    version='0.5',
+    packages=find_packages(),
+    py_modules=['SumaArea']
+)
