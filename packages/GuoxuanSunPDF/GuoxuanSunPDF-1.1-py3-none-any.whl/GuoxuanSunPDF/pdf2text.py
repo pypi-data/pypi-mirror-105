2de2fee474f21d37ec4a9cfaeab985a028d25e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:50 +0000

    Add package GuoxuanSunPDF/1.1/GuoxuanSunPDF-1.1-py3-none-any.whl

---
+++ packages/GuoxuanSunPDF/GuoxuanSunPDF-1.1-py3-none-any.whl/GuoxuanSunPDF/pdf2text.py
@@ -0,0 +1,11 @@
+class converter:
+  '''This module is a simple converter to convert PDF to texts'''
+  def convert(self,path):
+    '''Convert the given PDF to text.
+
+    parameters:
+    path(str): The path to a PDF file
+    
+    Returns 
+    str: The content of the PDF file as text.'''
+    print('pdf2text')
