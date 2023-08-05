0cf4e7270bfce621e00c67aa6f67a82ec7303f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:34 +0000

    Add package pytezos/3.2.0/pytezos-3.2.0.tar.gz

---
+++ packages/pytezos/pytezos-3.2.0.tar.gz/pytezos-3.2.0/src/pytezos/rpc/node.py
@@ -0,0 +1,158 @@
+import json
+import time
+from functools import wraps
+from pprint import pformat
+
+import requests
+import requests.exceptions
+from simplejson import JSONDecodeError
+
+from pytezos.logging import logger
+
+REQUEST_RETRY_COUNT = 3
+REQUEST_RETRY_SLEEP = 1
+
+
+def retry(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        for attempt in range(REQUEST_RETRY_COUNT):
+            logger.debug('Node request attempt %s/%s', attempt + 1, REQUEST_RETRY_COUNT)
+            try:
+                return fn(*args, **kwargs)
+            except requests.exceptions.ConnectionError as e:
+                if attempt + 1 == REQUEST_RETRY_COUNT:
+                    raise e
+                logger.warning(e)
+                time.sleep(REQUEST_RETRY_SLEEP)
+
+    return wrapper
+
+def urljoin(*args):
+    return "/".join(map(lambda x: str(x).strip('/'), args))
+
+
+def gen_error_variants(error_id) -> list:
+    chunks = error_id.split('.')
+    variants = [error_id]
+    if len(chunks) > 1:
+        variants.append(chunks[-2])
+        if len(chunks) > 2:
+            variants.append('.'.join(chunks[2:]))
+    return variants
+
+
+class RpcError(Exception):
+    __handlers__ = {}  # type: ignore
+
+    @classmethod
+    def __init_subclass__(cls, error_id=None, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if isinstance(error_id, list):
+            for eid in error_id:
+                cls.__handlers__[eid] = cls
+        else:
+            assert error_id is not None
+            cls.__handlers__[error_id] = cls
+
+    @classmethod
+    def from_errors(cls, errors: list):
+        if not errors:
+            return RpcError('Unspecified error')
+
+        # FIXME: Only first error is being processed
+        error = errors[-1]
+        for key in gen_error_variants(error['id']):
+            if key in cls.__handlers__:
+                handler = cls.__handlers__[key]
+                return handler(error)
+        return RpcError(error)
+
+    @classmethod
+    def from_response(cls, res: requests.Response):
+        if res.headers.get('content-type') == 'application/json':
+            errors = res.json()
+            assert isinstance(errors, list)
+            return cls.from_errors(errors)
+        else:
+            return RpcError(res.text)
+
+    def __str__(self):
+        return pformat(self.args)
+
+
+class RpcNode:
+
+    def __init__(self, uri):
+        self.uri = uri
+        self._session = requests.Session()
+
+    def __repr__(self):
+        res = [
+            super(RpcNode, self).__repr__(),
+            '\nNode address',
+            self.uri
+        ]
+        return '\n'.join(res)
+
+    def request(self, method, path, **kwargs) -> requests.Response:
+        logger.debug('>>>>> %s %s\n%s', method, path, json.dumps(kwargs, indent=4))
+        res = self._session.request(
+            method=method,
+            url=urljoin(self.uri, path),
+            headers={
+                'content-type': 'application/json',
+                'user-agent': 'PyTezos'
+            },
+            **kwargs
+        )
+        if res.status_code == 404:
+            logger.debug('<<<<< %s\n%s', res.status_code, res.text)
+            raise RpcError(f'Not found: {path}')
+        elif res.status_code != 200:
+            logger.debug('<<<<< %s\n%s', res.status_code, pformat(res.text, indent=4))
+            raise RpcError.from_response(res)
+
+        logger.debug('<<<<< %s\n%s', res.status_code, json.dumps(res.json(), indent=4))
+        return res
+
+    @retry
+    def get(self, path, params=None, timeout=None):
+        return self.request('GET', path, params=params, timeout=timeout).json()
+
+    def post(self, path, params=None, json=None):
+        response = self.request('POST', path, params=params, json=json)
+        try:
+            return response.json()
+        except JSONDecodeError:
+            return response.text
+
+    def delete(self, path, params=None):
+        return self.request('DELETE', path, params=params).json()
+
+    def put(self, path, params=None):
+        return self.request('PUT', path, params=params).json()
+
+
+class RpcMultiNode(RpcNode):
+
+    def __init__(self, uri):
+        super(RpcMultiNode, self).__init__(uri=uri)
+        if not isinstance(uri, list):
+            self.uri = [uri]
+        self.nodes = list(map(lambda x: RpcNode(x), self.uri))
+        self._next_i = 0
+
+    def __repr__(self):
+        res = [
+            super(RpcNode, self).__repr__(),
+            f'\nNode addresses',
+            *self.uri
+        ]
+        return '\n'.join(res)
+
+    def request(self, method, path, **kwargs) -> requests.Response:
+        assert self._next_i < len(self.nodes)
+        res = self.nodes[self._next_i].request(method, path, **kwargs)
+        self._next_i = (self._next_i + 1) % len(self.nodes)
+        return res
---
+++ packages/pytezos/pytezos-3.2.0.tar.gz/pytezos-3.2.0/src/pytezos/sandbox/node.py
@@ -0,0 +1,96 @@
+import atexit
+import unittest
+from time import sleep
+from typing import Optional
+
+import requests.exceptions
+from testcontainers.core.generic import DockerContainer  # type: ignore
+
+from pytezos.client import PyTezosClient
+from pytezos.sandbox.parameters import EDO
+
+# NOTE: Container object is a singleton which will be used in all tests inherited from class _SandboxedNodeTestCase
+# and stopped after all tests are completed.
+node_container: Optional[DockerContainer] = None
+node_container_client: PyTezosClient = PyTezosClient()
+node_fitness: int = 1
+
+
+class SandboxedNodeTestCase(unittest.TestCase):
+    IMAGE: str = 'bakingbad/sandboxed-node:v9.0-rc1-1'
+    PORT: Optional[int] = None
+    PROTOCOL: str = EDO
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        global node_container
+        if not node_container:
+            node_container = cls._create_node_container()
+            node_container.start()
+            cls._wait_for_connection()
+            atexit.register(node_container.stop)
+
+        cls.activate(cls.PROTOCOL, reset=True)
+
+    @classmethod
+    def activate(cls, protocol_alias: str, reset: bool = False):
+        return (
+            cls.get_client()
+            .using(key='dictator')
+            .activate_protocol(protocol_alias)
+            .fill(block_id='genesis' if reset else 'head')
+            .sign()
+            .inject()
+        )
+
+    @classmethod
+    def get_node_url(cls) -> str:
+        container = cls.get_node_container()
+        container_id = container.get_wrapped_container().id
+        host = container.get_docker_client().bridge_ip(container_id)
+        return f'http://{host}:8732'
+
+    @classmethod
+    def get_node_container(cls) -> DockerContainer:
+        if node_container is None:
+            raise RuntimeError('Sandboxed node container is not running')
+        return node_container
+
+    @classmethod
+    def get_client(cls):
+        return node_container_client.using(
+            shell=cls.get_node_url(),
+        )
+
+    @classmethod
+    def create_client(cls):
+        global node_container_client
+        node_container_client = PyTezosClient()
+        return cls.get_client()
+
+    @classmethod
+    def _create_node_container(cls) -> DockerContainer:
+        container = DockerContainer(
+            cls.IMAGE,
+        )
+        if cls.PORT:
+            container.ports[8732] = cls.PORT
+        return container
+
+    @classmethod
+    def _wait_for_connection(cls) -> None:
+        client = cls.get_client()
+        while True:
+            try:
+                client.shell.node.get("/version/")
+                break
+            except requests.exceptions.ConnectionError:
+                sleep(0.1)
+
+    @classmethod
+    def bake_block(cls, min_fee: int = 0):
+        return cls.get_client().using(key='bootstrap1').bake_block(min_fee).fill().work().sign().inject()
+
+    @property
+    def client(self) -> PyTezosClient:
+        return self.get_client().using(key='bootstrap2')
