4fd0ea32e11bda55dbfd5dd10d6726760d64edc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:32 +0000

    Add package vaccine-availability-notifier/0.0.2/vaccine_availability_notifier-0.0.2-py3-none-any.whl

---
+++ packages/vaccine-availability-notifier/vaccine_availability_notifier-0.0.2-py3-none-any.whl/vaccineAvailabilityNotifier/client/actionsImpl.py
@@ -0,0 +1,39 @@
+import requests
+
+from vaccineAvailabilityNotifier.client.actions import Actions
+from vaccineAvailabilityNotifier.constants.constants import API_KEY_NAME
+
+
+def build_url(PINCODE, DATE):
+    # /v2/appointment/sessions/public/calendarByPin
+    return 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=' + PINCODE + '&date=' + DATE
+
+
+def get_headers(params=None):
+    # 'accept': 'application/json',
+    # 'Accept-Language': 'hi_IN'
+    return {'accept': 'application/json', 'Accept-Language': 'hi_IN'}
+
+
+class ActionsImpl(Actions):
+
+    def get(self, params={}):
+        r = None
+        try:
+            url = build_url(PINCODE=params["pincode"], DATE=params["date"])
+            r = requests.get(url, {}, headers=get_headers())
+        except requests.exceptions.RequestException as e:
+            exit()
+        return r
+
+    def create(self, uri, payload):
+        r = None
+        try:
+            url = build_url()
+            r = requests.post(url, data=payload, headers=get_headers[API_KEY_NAME])
+        except requests.exceptions.RequestException as e:
+            exit()
+        return r
+
+    def delete(self, uri, payload=None):
+        return self.create(uri, payload)
---
+++ packages/vaccine-availability-notifier/vaccine_availability_notifier-0.0.2-py3-none-any.whl/vaccineAvailabilityNotifier/commands/multiple_arguments.py
@@ -0,0 +1,16 @@
+import ast
+
+import click
+
+
+class MultipleArguments(click.Option):
+
+    def type_cast_value(self, ctx, value):
+        try:
+            value = str(value)
+            assert value.count('[') == 1 and value.count(']') == 1
+            list_as_str = value.replace('"', "'").split('[')[1].split(']')[0]
+            list_of_items = [item.strip().strip("'") for item in list_as_str.split(',')]
+            return list_of_items
+        except:
+            raise click.BadParameter(value)
