f8242a4cc59fe93d70c3498bda8374df66b29995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:38 +0000

    Add package slakh-dataset/0.1.12/slakh-dataset-0.1.12.tar.gz

---
+++ packages/slakh-dataset/slakh-dataset-0.1.12.tar.gz/slakh-dataset-0.1.12/slakh_dataset/data_classes.py
@@ -0,0 +1,23 @@
+from typing import List, NamedTuple
+
+import torch
+
+
+class MusicAnnotation(NamedTuple):
+    """An onset, offset, frame and velocity annotation"""
+
+    onset: torch.FloatTensor  # [num_steps, midi_bins]
+    offset: torch.FloatTensor  # [num_steps, midi_bins]
+    frame: torch.FloatTensor  # [num_steps, midi_bins]
+    velocity: torch.FloatTensor  # [num_steps, midi_bins]
+
+
+class AudioAndLabels(NamedTuple):
+    """Audio and label class with data that will be on GPU"""
+
+    track: str
+    start_frame: int
+    end_frame: int
+    frames_per_second: float
+    audio: torch.FloatTensor  # [num_steps, n_mels]
+    annotation: MusicAnnotation
