8c28904210795b4a221c506ff64ef6f335b46058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package cloudrail-knowledge/0.0.11/cloudrail_knowledge-0.0.11-py3-none-any.whl

---
+++ packages/cloudrail-knowledge/cloudrail_knowledge-0.0.11-py3-none-any.whl/cloudrail/knowledge/rules/aws/non_context_aware/iam_no_human_users_rule.py
@@ -0,0 +1,23 @@
+from typing import Dict, List
+from cloudrail.knowledge.context.environment_context import EnvironmentContext
+from cloudrail.knowledge.rules.aws.aws_base_rule import AwsBaseRule
+from cloudrail.knowledge.rules.base_rule import Issue
+from cloudrail.knowledge.rules.rule_parameters.base_paramerter import ParameterType
+
+
+class IamNoHumanUsersRule(AwsBaseRule):
+
+    def execute(self, env_context: EnvironmentContext, parameters: Dict[ParameterType, any]) -> List[Issue]:
+        issues_list: List[Issue] = []
+        users_login_list = [user_name.name for user_name in env_context.users_login_profile]
+        for user in env_context.users:
+            if user.name in users_login_list:
+                issues_list.append(Issue(f'The {user.get_type()} `{user.get_friendly_name()}` has console access, '
+                                         f'and so is considered human', user, user))
+        return issues_list
+
+    def get_id(self) -> str:
+        return "non_car_iam_no_human_users"
+
+    def should_run_rule(self, environment_context: EnvironmentContext) -> bool:
+        return bool(environment_context.users)
