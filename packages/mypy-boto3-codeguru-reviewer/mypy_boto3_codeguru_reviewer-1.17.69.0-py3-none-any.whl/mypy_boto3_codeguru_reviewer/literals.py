268584b681220e198ed2526b505e85ff40e7bae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:15 +0000

    Add package mypy-boto3-codeguru-reviewer/1.17.69.0/mypy_boto3_codeguru_reviewer-1.17.69.0-py3-none-any.whl

---
+++ packages/mypy-boto3-codeguru-reviewer/mypy_boto3_codeguru_reviewer-1.17.69.0-py3-none-any.whl/mypy_boto3_codeguru_reviewer/literals.py
@@ -0,0 +1,41 @@
+"""
+Type annotations for codeguru-reviewer service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_codeguru_reviewer/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_codeguru_reviewer.literals import EncryptionOption
+
+    data: EncryptionOption = "AWS_OWNED_CMK"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "EncryptionOption",
+    "JobState",
+    "ListRepositoryAssociationsPaginatorName",
+    "ProviderType",
+    "Reaction",
+    "RepositoryAssociationState",
+    "TypeType",
+)
+
+
+EncryptionOption = Literal["AWS_OWNED_CMK", "CUSTOMER_MANAGED_CMK"]
+JobState = Literal["Completed", "Deleting", "Failed", "Pending"]
+ListRepositoryAssociationsPaginatorName = Literal["list_repository_associations"]
+ProviderType = Literal["Bitbucket", "CodeCommit", "GitHub", "GitHubEnterpriseServer"]
+Reaction = Literal["ThumbsDown", "ThumbsUp"]
+RepositoryAssociationState = Literal[
+    "Associated", "Associating", "Disassociated", "Disassociating", "Failed"
+]
+TypeType = Literal["PullRequest", "RepositoryAnalysis"]
