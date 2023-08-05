35f440c1b50e395d149fd586ba78edcc3d216bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:45 +0000

    Add package peopleanalyticsdata/0.1.3/peopleanalyticsdata-0.1.3.tar.gz

---
+++ packages/peopleanalyticsdata/peopleanalyticsdata-0.1.3.tar.gz/peopleanalyticsdata-0.1.3/peopleanalyticsdata/__init__.py
@@ -0,0 +1,4 @@
+from .peopleanalyticsdata import list_sets, charity_donation, employee_survey, health_insurance, job_retention, managers, politics_survey, salespeople, soccer, sociological_data, speed_dating, ugtests
+
+__version__ = '0.1.3'
+__author__ = 'Akshay Kotha'
---
+++ packages/peopleanalyticsdata/peopleanalyticsdata-0.1.3.tar.gz/peopleanalyticsdata-0.1.3/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+setuptools.setup(
+    name="peopleanalyticsdata",
+    version="0.1.3",
+    url="https://github.com/akshayreddykotha/peopleanalyticsdata",
+    author="Akshay Kotha",
+    author_email="kakr2795@gmail.com",
+    description="Allows data access from the book - Handbook of Regression Modeling in People Analytics.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    include_package_data=True,
+    package_data={'': ['data/*.csv']},
+)
