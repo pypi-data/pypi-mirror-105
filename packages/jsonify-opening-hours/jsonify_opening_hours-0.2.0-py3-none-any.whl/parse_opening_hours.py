d31f2477043bba865a782d2ef1e8ce4914cef7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:39 +0000

    Add package jsonify-opening-hours/0.2.0/jsonify_opening_hours-0.2.0-py3-none-any.whl

---
+++ packages/jsonify-opening-hours/jsonify_opening_hours-0.2.0-py3-none-any.whl/parse_opening_hours.py
@@ -0,0 +1,117 @@
+# parse various types of strings representing opening hours for a business into machine-readable JSON
+# parse into the following format:
+#  [
+#     {
+#       day: str as lowercase day of week e.g. monday,
+#       opens: str as time with facility opens on this day in format hh:mm,
+#       closes: str as time with facility closes on this day in format hh:mm,
+#     },
+#     ...
+#   ]
+
+from pyparsing import Word, alphas, nums, oneOf, Optional, Or, OneOrMore, Char
+from patterns import *
+from helpers import detect_if_pm, str_to_day, str_to_days, day_to_str, expand_day_range, value_from_parsed, str_from_parsed, key_exists, concat_from_parsed, raw_from_parsed, Weekday
+from models.time import Time, TimeType
+import logging
+logging.basicConfig()
+
+logger = logging.getLogger(__name__)
+
+class JsonOpeningHours():
+
+	@classmethod
+	def parse(self, hours_string, assume_type=None):
+
+		opening_hours_format = Or([
+			OneOrMore(dates + day_time_separators + timerange),
+			OneOrMore(timerange + dates + notes)
+		])	
+		parsed = opening_hours_format.parseString(hours_string)
+		opening_hours_json = convert_to_dict(parsed, assume_type=assume_type)
+
+
+		return opening_hours_json
+
+
+
+def create_entry(day, opening, closing, notes=None):
+	entry = {
+		"day": day,
+		"opens": opening,
+		"closes": closing
+	}
+	if notes:
+		entry["notes"] = notes
+	return entry
+
+
+def parse_times(result, assume_type=None):
+	# assumes that all three (hours, minutes, am_pm) are the same length
+	start = concat_from_parsed(result, "starttime")
+	end = concat_from_parsed(result, "endtime")
+	start = Time.from_string(start, assume_type=assume_type)
+	end = Time.from_string(end, assume_type=assume_type)
+
+	if start.is_am() and end.is_am() and start.get_hours() > end.get_hours():
+		end.set_type(TimeType.PM)
+
+
+	return (
+		start.get_as_military_time().to_string(),
+		end.get_as_military_time().to_string()
+		)
+
+# TODO: testme
+def is_day_range(result):
+	return key_exists(result, "startday")
+
+def is_day_list(result):
+	return key_exists(result, "day")
+
+def is_day_shortcut(result):
+	return key_exists(result, "day_shortcuts")
+
+# TODO: testme
+def parse_days(result):
+	days = []
+
+	if is_day_range(result):
+		logger.info("range date detected")
+		# this is a date range that includes the intervening days
+		start_day = str_to_day(str_from_parsed(result, "startday"))
+		end_day = str_from_parsed(result, "endday", default=None)
+		end_day = str_to_day(end_day[0]) if end_day is not None else end_day
+		days = expand_day_range(start_day, end_day)
+	elif is_day_list(result):
+		logger.info("list date detected")
+
+		days = [ str_to_day(day) for day in raw_from_parsed(result, "day") ]
+	elif is_day_shortcut(result):
+		logger.info("shortcut date detected")
+		days = str_to_days(concat_from_parsed(result, "day_shortcuts"))
+	else:
+		logger.info("unspecified date detected")
+		# nothing specified, assumeit means every day
+		return expand_day_range(Weekday.MONDAY, Weekday.SUNDAY)
+	return days
+
+def convert_to_dict(result, assume_type=None):
+
+	opening_hours_json = []
+
+	days = parse_days(result)
+	
+	start_time, end_time = parse_times(result, assume_type=assume_type)
+	
+
+	for day in days:
+		opening_hours_json.append(
+			create_entry(
+				day_to_str(day),
+				start_time,
+				end_time
+			)
+		)
+	logger.debug(vars(result))
+	return opening_hours_json
---
+++ packages/jsonify-opening-hours/jsonify_opening_hours-0.2.0-py3-none-any.whl/patterns.py
@@ -0,0 +1,67 @@
+from pyparsing import Word, alphas, nums, oneOf, Optional, Or, OneOrMore, Char, Combine
+
+def caselessWord(some_str):
+	return Word(some_str.lower() + some_str.upper())
+
+def caselessChar(some_str):
+	return Char(some_str.lower() + some_str.upper())
+
+space = Word(" ")
+words_for_range = Or([
+	caselessWord("to"),
+	caselessWord("thru"),
+	caselessWord("through"),
+	caselessWord("until"),
+	caselessWord("'til")
+])
+word_range_separators = Optional(space) + words_for_range + Optional(space)
+range_separator = Or([Word(" –—‐-"), word_range_separators])
+
+word_list_separators = Optional(space) + oneOf("and") + Optional(space)
+list_separator = Or([Word(" ,+/"), word_list_separators])
+
+day_time_separators = Optional(Word(": "))
+
+#TODO: support multiple sections like M 8am-2pm, W 9am-2pm
+section_separator = Optional(",")
+time_separator = Optional(":")
+
+# this is all the unique characters in the string
+# "monday tuesday wednesday thursday friday saturday sunday"
+day_of_week_start = caselessChar("mtwhfs")
+day_of_week_rest = Optional(caselessWord("ondayuesrit'"))
+day = Combine(day_of_week_start + day_of_week_rest)
+
+day_shortcuts = Or([
+	Or([
+		caselessWord("Work"),
+		caselessWord("All")
+	]) + caselessWord("Week"),
+	Char("57") + caselessWord("days") + Optional(caselessWord("a week")),
+	caselessWord("Every Day"),
+	caselessWord("daily"),
+	caselessWord("Week") + Or([
+		caselessWord("day"),
+		caselessWord("end")
+	]) + Optional(caselessWord("'s")),
+	caselessWord("Business Days"),
+])
+
+days = Or([day, day_shortcuts])
+
+time_number = Word(nums, max=2)
+am_or_pm = Optional(Word("AaPpMm.").setResultsName('am_pm', listAllMatches=True))
+
+time = time_number("hour*") + time_separator + Optional(time_number("minute*")) + am_or_pm
+
+dateShortcuts = OneOrMore(days.setResultsName('day_shortcuts', listAllMatches=True) + Optional(list_separator))
+
+dateList = OneOrMore(day.setResultsName('day', listAllMatches=True) + Optional(list_separator))
+
+daterange = day.setResultsName('startday', listAllMatches=True) + range_separator + day.setResultsName('endday', listAllMatches=True)
+
+dates = Optional(Or([daterange, dateList, dateShortcuts]))
+
+timerange = time.setResultsName('starttime', listAllMatches=True) + Optional(range_separator + time.setResultsName('endtime', listAllMatches=True))
+
+notes = section_separator + Optional(OneOrMore(Word(alphas))).setResultsName('notes', listAllMatches=True)
