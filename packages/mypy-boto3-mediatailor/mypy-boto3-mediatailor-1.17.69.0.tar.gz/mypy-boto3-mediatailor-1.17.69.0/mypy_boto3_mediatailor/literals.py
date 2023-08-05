e0ecbd4efaf330dd80a2ae232803ff9eeb6210bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:59 +0000

    Add package mypy-boto3-mediatailor/1.17.69.0/mypy-boto3-mediatailor-1.17.69.0.tar.gz

---
+++ packages/mypy-boto3-mediatailor/mypy-boto3-mediatailor-1.17.69.0.tar.gz/mypy-boto3-mediatailor-1.17.69.0/mypy_boto3_mediatailor/literals.py
@@ -0,0 +1,51 @@
+"""
+Type annotations for mediatailor service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_mediatailor/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_mediatailor.literals import AccessType
+
+    data: AccessType = "S3_SIGV4"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "AccessType",
+    "ChannelState",
+    "GetChannelSchedulePaginatorName",
+    "ListChannelsPaginatorName",
+    "ListPlaybackConfigurationsPaginatorName",
+    "ListSourceLocationsPaginatorName",
+    "ListVodSourcesPaginatorName",
+    "MessageType",
+    "Mode",
+    "OriginManifestType",
+    "PlaybackMode",
+    "RelativePosition",
+    "TypeType",
+)
+
+
+AccessType = Literal["S3_SIGV4"]
+ChannelState = Literal["RUNNING", "STOPPED"]
+GetChannelSchedulePaginatorName = Literal["get_channel_schedule"]
+ListChannelsPaginatorName = Literal["list_channels"]
+ListPlaybackConfigurationsPaginatorName = Literal["list_playback_configurations"]
+ListSourceLocationsPaginatorName = Literal["list_source_locations"]
+ListVodSourcesPaginatorName = Literal["list_vod_sources"]
+MessageType = Literal["SPLICE_INSERT"]
+Mode = Literal["BEHIND_LIVE_EDGE", "OFF"]
+OriginManifestType = Literal["MULTI_PERIOD", "SINGLE_PERIOD"]
+PlaybackMode = Literal["LOOP"]
+RelativePosition = Literal["AFTER_PROGRAM", "BEFORE_PROGRAM"]
+TypeType = Literal["DASH", "HLS"]
