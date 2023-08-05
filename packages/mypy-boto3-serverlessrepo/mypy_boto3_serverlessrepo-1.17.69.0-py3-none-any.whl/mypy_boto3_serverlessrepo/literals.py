829ffef649d1e06e59c387cfba65bf615e8ea977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:10 +0000

    Add package mypy-boto3-serverlessrepo/1.17.69.0/mypy_boto3_serverlessrepo-1.17.69.0-py3-none-any.whl

---
+++ packages/mypy-boto3-serverlessrepo/mypy_boto3_serverlessrepo-1.17.69.0-py3-none-any.whl/mypy_boto3_serverlessrepo/literals.py
@@ -0,0 +1,37 @@
+"""
+Type annotations for serverlessrepo service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_serverlessrepo/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_serverlessrepo.literals import Capability
+
+    data: Capability = "CAPABILITY_AUTO_EXPAND"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "Capability",
+    "ListApplicationDependenciesPaginatorName",
+    "ListApplicationVersionsPaginatorName",
+    "ListApplicationsPaginatorName",
+    "Status",
+)
+
+
+Capability = Literal[
+    "CAPABILITY_AUTO_EXPAND", "CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_RESOURCE_POLICY"
+]
+ListApplicationDependenciesPaginatorName = Literal["list_application_dependencies"]
+ListApplicationVersionsPaginatorName = Literal["list_application_versions"]
+ListApplicationsPaginatorName = Literal["list_applications"]
+Status = Literal["ACTIVE", "EXPIRED", "PREPARING"]
