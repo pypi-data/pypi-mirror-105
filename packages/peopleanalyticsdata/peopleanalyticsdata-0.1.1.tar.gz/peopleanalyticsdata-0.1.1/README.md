47b5f99e138a307bc7fec55da58318807515303a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:45 +0000

    Add package peopleanalyticsdata/0.1.1/peopleanalyticsdata-0.1.1.tar.gz

---
+++ packages/peopleanalyticsdata/peopleanalyticsdata-0.1.1.tar.gz/peopleanalyticsdata-0.1.1/README.md
@@ -0,0 +1,8 @@
+# peopleanalytics-python
+
+This effort is to port the package in R to Python. Please refer this [article](https://towardsdatascience.com/beginner-friendly-data-science-projects-accepting-contributions-3b8e26f7e88e) for more details on the same.
+
+As an initial idea, trying to make the data available as an accessible python package which can be 'pip install ...' and called for statistical analysis and inference modeling.
+
+This package will be providing a different set of data sets for budding data scientists to explore and analyze. Consider it as a alternative version of iris data sets and the similar ones on the UCI Machine Learning repository.
+
---
+++ packages/peopleanalyticsdata/peopleanalyticsdata-0.1.1.tar.gz/peopleanalyticsdata-0.1.1/setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+
+setuptools.setup(
+    name="peopleanalyticsdata",
+    version="0.1.1",
+    url="https://github.com/akshayreddykotha/peopleanalyticsdata",
+    author="Akshay Kotha",
+    author_email="kakr2795@gmail.com",
+    description="Allows data access from the book - Handbook of Regression Modeling in People Analytics.",
+    long_description=open('DESCRIPTION.rst').read(),
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    include_package_data=True,
+    package_data={'': ['data/*.csv']},
+)
