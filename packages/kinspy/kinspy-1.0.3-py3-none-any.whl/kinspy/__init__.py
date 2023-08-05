75cd4d01c9203a1e381013733da9d6888c679784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:45 +0000

    Add package kinspy/1.0.3/kinspy-1.0.3-py3-none-any.whl

---
+++ packages/kinspy/kinspy-1.0.3-py3-none-any.whl/kinspy/__init__.py
@@ -0,0 +1,10 @@
+# Name: kinspy
+# Describes: provide convenient functions and constants
+# Version: v1.0.3
+# Date: 2021-04-25
+# Author: Yunxiao Zhang
+# E-mail: yunxiao9277@gmail.com
+
+from numpy import sqrt, sin, cos, exp, log
+from numpy import pi, e
+from numpy import zeros, ones
