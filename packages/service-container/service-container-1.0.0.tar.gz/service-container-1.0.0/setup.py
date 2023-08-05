19fe81598dfb16412dad67b7d60b74b40ed39ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:53 +0000

    Add package service-container/1.0.0/service-container-1.0.0.tar.gz

---
+++ packages/service-container/service-container-1.0.0.tar.gz/service-container-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='service-container',
+    version='1.0.0',
+    packages=['servicecontainer'],
+    license='Apache 2.0',
+    author='Heureka.cz',
+    author_email='fialovi@heureka.cz',
+    description='Dependency injection container with parameters and transaction control.',
+)
