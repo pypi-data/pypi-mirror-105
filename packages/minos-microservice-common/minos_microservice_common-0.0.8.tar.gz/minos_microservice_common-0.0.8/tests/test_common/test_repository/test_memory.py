fb9f336e2a8d2e21e86e1bfe92d7309eb836127e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:53 +0000

    Add package minos-microservice-common/0.0.8/minos_microservice_common-0.0.8.tar.gz

---
+++ packages/minos-microservice-common/minos_microservice_common-0.0.8.tar.gz/minos_microservice_common-0.0.8/tests/test_common/test_repository/test_memory.py
@@ -0,0 +1,192 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+import unittest
+
+from minos.common import (
+    MinosInMemoryRepository,
+    MinosRepository,
+    MinosRepositoryAction,
+    MinosRepositoryEntry,
+)
+
+
+class TestStringMethods(unittest.IsolatedAsyncioTestCase):
+    def test_constructor(self):
+        repository = MinosInMemoryRepository()
+        self.assertIsInstance(repository, MinosRepository)
+
+    async def test_insert(self):
+        async with MinosInMemoryRepository() as repository:
+            await repository.insert(MinosRepositoryEntry(0, "example.Car", 1, bytes("foo", "utf-8")))
+
+            expected = [
+                MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8"), 1, MinosRepositoryAction.INSERT)
+            ]
+            self.assertEqual(expected, [v async for v in repository.select()])
+
+    async def test_update(self):
+        async with MinosInMemoryRepository() as repository:
+            await repository.update(MinosRepositoryEntry(0, "example.Car", 1, bytes("foo", "utf-8")))
+            expected = [
+                MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8"), 1, MinosRepositoryAction.UPDATE)
+            ]
+            self.assertEqual(expected, [v async for v in repository.select()])
+
+    async def test_delete(self):
+        async with MinosInMemoryRepository() as repository:
+            await repository.delete(MinosRepositoryEntry(0, "example.Car", 1, bytes()))
+            expected = [MinosRepositoryEntry(1, "example.Car", 1, bytes(), 1, MinosRepositoryAction.DELETE)]
+            self.assertEqual(expected, [v async for v in repository.select()])
+
+    async def test_select(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8"), 1, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.Car", 2, bytes("bar", "utf-8"), 2, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(2, "example.Car", 1, bytes("hello", "utf-8"), 3, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.Car", 3, bytes("foobar", "utf-8"), 4, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.Car", 4, bytes(), 5, MinosRepositoryAction.DELETE),
+            MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8"), 6, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.MotorCycle", 1, bytes("one", "utf-8"), 7, MinosRepositoryAction.INSERT),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select()])
+
+    async def test_select_empty(self):
+        repository = MinosInMemoryRepository()
+        self.assertEqual([], [v async for v in repository.select()])
+
+    async def test_select_id(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 2, bytes("bar", "utf-8"), 2, MinosRepositoryAction.UPDATE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(id=2)])
+
+    async def test_select_id_lt(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8"), 1, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.Car", 2, bytes("bar", "utf-8"), 2, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(2, "example.Car", 1, bytes("hello", "utf-8"), 3, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.Car", 3, bytes("foobar", "utf-8"), 4, MinosRepositoryAction.UPDATE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(id_lt=5)])
+
+    async def test_select_id_gt(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 4, bytes(), 5, MinosRepositoryAction.DELETE),
+            MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8"), 6, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.MotorCycle", 1, bytes("one", "utf-8"), 7, MinosRepositoryAction.INSERT),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(id_gt=4)])
+
+    async def test_select_id_le(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8"), 1, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.Car", 2, bytes("bar", "utf-8"), 2, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(2, "example.Car", 1, bytes("hello", "utf-8"), 3, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.Car", 3, bytes("foobar", "utf-8"), 4, MinosRepositoryAction.UPDATE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(id_le=4)])
+
+    async def test_select_id_ge(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 4, bytes(), 5, MinosRepositoryAction.DELETE),
+            MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8"), 6, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.MotorCycle", 1, bytes("one", "utf-8"), 7, MinosRepositoryAction.INSERT),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(id_ge=5)])
+
+    async def test_select_aggregate_id(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(2, "example.Car", 1, bytes("hello", "utf-8"), 3, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8"), 6, MinosRepositoryAction.UPDATE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(aggregate_id=2)])
+
+    async def test_select_aggregate_name(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.MotorCycle", 1, bytes("one", "utf-8"), 7, MinosRepositoryAction.INSERT),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(aggregate_name="example.MotorCycle")])
+
+    async def test_select_version(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 4, bytes(), 5, MinosRepositoryAction.DELETE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(version=4)])
+
+    async def test_select_version_lt(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8"), 1, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(2, "example.Car", 1, bytes("hello", "utf-8"), 3, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.MotorCycle", 1, bytes("one", "utf-8"), 7, MinosRepositoryAction.INSERT),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(version_lt=2)])
+
+    async def test_select_version_gt(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 2, bytes("bar", "utf-8"), 2, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.Car", 3, bytes("foobar", "utf-8"), 4, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.Car", 4, bytes(), 5, MinosRepositoryAction.DELETE),
+            MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8"), 6, MinosRepositoryAction.UPDATE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(version_gt=1)])
+
+    async def test_select_version_le(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8"), 1, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(2, "example.Car", 1, bytes("hello", "utf-8"), 3, MinosRepositoryAction.INSERT),
+            MinosRepositoryEntry(1, "example.MotorCycle", 1, bytes("one", "utf-8"), 7, MinosRepositoryAction.INSERT),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(version_le=1)])
+
+    async def test_select_version_ge(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 2, bytes("bar", "utf-8"), 2, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.Car", 3, bytes("foobar", "utf-8"), 4, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.Car", 4, bytes(), 5, MinosRepositoryAction.DELETE),
+            MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8"), 6, MinosRepositoryAction.UPDATE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(version_ge=2)])
+
+    async def test_select_combine(self):
+        repository = await self._build_repository()
+        expected = [
+            MinosRepositoryEntry(1, "example.Car", 3, bytes("foobar", "utf-8"), 4, MinosRepositoryAction.UPDATE),
+            MinosRepositoryEntry(1, "example.Car", 4, bytes(), 5, MinosRepositoryAction.DELETE),
+            MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8"), 6, MinosRepositoryAction.UPDATE),
+        ]
+        self.assertEqual(expected, [v async for v in repository.select(aggregate_name="example.Car", id_ge=4)])
+
+    @staticmethod
+    async def _build_repository():
+        repository = MinosInMemoryRepository()
+        await repository.insert(MinosRepositoryEntry(1, "example.Car", 1, bytes("foo", "utf-8")))
+        await repository.update(MinosRepositoryEntry(1, "example.Car", 2, bytes("bar", "utf-8")))
+        await repository.insert(MinosRepositoryEntry(2, "example.Car", 1, bytes("hello", "utf-8")))
+        await repository.update(MinosRepositoryEntry(1, "example.Car", 3, bytes("foobar", "utf-8")))
+        await repository.delete(MinosRepositoryEntry(1, "example.Car", 4))
+        await repository.update(MinosRepositoryEntry(2, "example.Car", 2, bytes("bye", "utf-8")))
+        await repository.insert(MinosRepositoryEntry(1, "example.MotorCycle", 1, bytes("one", "utf-8")))
+
+        return repository
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/minos-microservice-common/minos_microservice_common-0.0.8.tar.gz/minos_microservice_common-0.0.8/tests/test_storage_lmdb.py
@@ -0,0 +1,110 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+
+import shutil
+
+import pytest
+from minos.common import (
+    MinosStorageLmdb,
+)
+
+
+@pytest.fixture
+def path():
+    return "./tests/test_db.lmdb"
+
+
+@pytest.fixture(autouse=True)
+def clear_database(path):
+    yield
+    # Code that will run after your test, for example:
+    shutil.rmtree(path, ignore_errors=True)
+
+
+def test_storage_add_text(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", "Text Value")
+
+    value_returned = minosEnv.get("TestOne", "first")
+    assert value_returned == "Text Value"
+
+
+def test_storage_add_int(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", 123)
+
+    value_returned = minosEnv.get("TestOne", "first")
+    assert value_returned == 123
+
+
+def test_storage_add_dict(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", {"key_one": "hello", "key_two": "minos"})
+
+    value_returned = minosEnv.get("TestOne", "first")
+    assert value_returned["key_one"] == "hello"
+    assert value_returned["key_two"] == "minos"
+
+
+def test_storage_add_multi_dict(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", {"key_one": "hello", "key_two": {"sub_key": "this is a sub text"}})
+
+    value_returned = minosEnv.get("TestOne", "first")
+    assert value_returned["key_one"] == "hello"
+    assert value_returned["key_two"]["sub_key"] == "this is a sub text"
+
+
+def test_storage_add_list(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", ["hello", "minos"])
+
+    value_returned = minosEnv.get("TestOne", "first")
+    assert value_returned[0] == "hello"
+    assert value_returned[1] == "minos"
+
+
+def test_storage_add_multi_table(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", "Text Value")
+    minosEnv.add("TestTwo", "first_double", "Text Double Value")
+    minosEnv.add("TestTwo", "first", "Text Value Diff")
+
+    value_returned = minosEnv.get("TestOne", "first")
+    assert value_returned == "Text Value"
+
+    value_returned = minosEnv.get("TestTwo", "first_double")
+    assert value_returned == "Text Double Value"
+
+    value_returned = minosEnv.get("TestTwo", "first")
+    assert value_returned == "Text Value Diff"
+
+
+def test_storage_delete(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", "Text Value")
+    minosEnv.add("TestOne", "second", "Text Second Value")
+
+    minosEnv.delete("TestOne", "first")
+    value_returned = minosEnv.get("TestOne", "second")
+    assert value_returned == "Text Second Value"
+
+    exception_value = minosEnv.get("TestOne", "first")
+    assert exception_value is None
+
+
+def test_storage_update(path):
+    minosEnv = MinosStorageLmdb.build(path)
+    minosEnv.add("TestOne", "first", "Text Value")
+
+    value_returned = minosEnv.get("TestOne", "first")
+    assert value_returned == "Text Value"
+
+    minosEnv.update("TestOne", "first", "Updated Text Value")
+    updated_value = minosEnv.get("TestOne", "first")
+    assert updated_value == "Updated Text Value"
