7ccc13afa6912437fc4bc82c4b657f543c9c3ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package tinc/0.9.6/tinc-0.9.6-py3-none-any.whl

---
+++ packages/tinc/tinc-0.9.6-py3-none-any.whl/tinc/cachemanager.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 14:32:03 2020
+
+@author: Andres
+"""
+
+import json
+import os
+import jsonschema
+
+# FIXME use more robust cache metadata defined in the metadata schema
+
+class CacheManager(object):
+    def __init__(self, directory = "python_cache"):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        self._cache_dir = directory
+        self._validator = None
+        # FIXME implement schema validation
+        # with open("tinc_cache_schema.json")as f:
+        #     schema = json.load(f)
+        #     self._validator = jsonschema.Draft7Validator(schema)
+            
+    def construct_filename(self, args):
+        # TODO allow setting filename template.
+        filename = self._cache_dir + '/cache_'
+        # TODO more robust checks for arguments and source process
+        for param_id, value in args.items():
+            filename += f'{value}_'
+        filename = filename[:-1]
+        return filename
+        
+    def store_cache(self, data, args):
+        with open(self.construct_filename(args), 'w') as fp:
+            json.dump(data, fp)
+            print(f"stored cache: {self.construct_filename(args)}")
+    
+    def load_cache(self, args):
+        data = None
+        filename = self.construct_filename(args)
+        if os.path.exists(filename):
+            with open(filename) as fp:
+                data = json.load(fp)
+                
+                print(f"loaded cache: {filename}")
+        return data
+    
+    def remove_cache_file(self, args):
+        
+        filename = self.construct_filename(args)
+        if os.path.exists(filename):
+            print(f"removing {filename}")
+            os.remove(filename)
+
+
+if __name__ == "__main__":
+    c = CacheManager()
+    #print(c._validator)
