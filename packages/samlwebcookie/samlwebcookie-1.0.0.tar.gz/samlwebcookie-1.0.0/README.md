56efd47e03d45045b77c81c10bb002e4304d4a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:07 +0000

    Add package samlwebcookie/1.0.0/samlwebcookie-1.0.0.tar.gz

---
+++ packages/samlwebcookie/samlwebcookie-1.0.0.tar.gz/samlwebcookie-1.0.0/README.md
@@ -0,0 +1,79 @@
+# SAML Web Cookie
+
+This utility allows you to authenticate using **Azure AD SAML** and
+2FA from the command line and then connect to your VPN using
+OpenConnect.
+
+## Setup and usage
+
+Start by installing OpenConnect if you don't have it already. ([Ubuntu instructions](https://grepitout.com/install-openconnect-ubuntu-vpn-client/))
+
+### Using pip
+
+To install `samlwebcookie`:
+```
+pip install samlwebcookie
+```
+
+To run `samlwebcookie` and pass the result to `openconnect` (after filling in your server, username, and password):
+```
+export VPN_SERVER="vpn.company.org"
+export VPN_USERNAME="username"
+export VPN_PASSWORD="password"
+export SWC_OUTPUT_FILE=<(:) && samlwebcookie $VPN_SERVER --username="$VPN_USERNAME" --password="$VPN_PASSWORD" --output-file=$SWC_OUTPUT_FILE && . $SWC_OUTPUT_FILE && sudo openconnect $SWC_SERVER --cookie=$SWC_COOKIE
+```
+
+Example output:
+```
+Waiting for 2FA code prompt...
+Verification code: 123456
+Got VPN cookie:
+AAAAAAABBABABABAB@@AAAEXAMPLECOOKLIEPLEASEIGNORETHIOSVALYEHERE
+[sudo] password for user:
+Attempting to connect to server 111.111.111.11:443
+Connected to 111.111.111.11:443
+```
+
+### Using Docker
+
+#### 1. Clone this code and build the container
+
+```bash
+git clone git@github.com:libcthorne/samlwebcookie.git
+cd samlwebcookie
+docker build -t samlwebcookie .
+```
+
+#### 2. Create the .env file with the local config
+
+```
+VPN_SERVER=vpn.company.org
+VPN_USERNAME=username@company.org
+VPN_PASSWORD=password
+```
+
+#### 3. Run connect.sh
+
+The `connect.sh` script will first ask you for your VPN password, then your 2FA verification code, and finally it will ask you to `sudo` - do not be alarmed when it prompts you for these.
+
+Once connected, the `openconnect` command will remain running.
+
+```bash
+./connect.sh
+```
+
+You should see output similar to:
+
+```
+Running samlwebcookie
+Waiting for 2FA code prompt...
+Verification code: 999999
+Got VPN cookie:
+AAAAAAABBABABABAB@@AAAEXAMPLECOOKLIEPLEASEIGNORETHIOSVALYEHERE
+[sudo] password for ubuntu:
+Attempting to connect to server 111.111.111.11:443
+Connected to 111.111.111.11:443
+...
+```
+
+To disconnect from the VPN, simply hit Ctrl+C to terminate the `connect.sh` script.
---
+++ packages/samlwebcookie/samlwebcookie-1.0.0.tar.gz/samlwebcookie-1.0.0/webcookie.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+import time
+import urllib3
+
+import requests
+from bs4 import BeautifulSoup
+from requests_ntlm import HttpNtlmAuth
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vpn_server', type=str, help='VPN server to obtain cookie for, e.g. vpn.company.org')
+    parser.add_argument('--username', required=True, type=str)
+    parser.add_argument('--password', required=True, type=str)
+    parser.add_argument('--output-file', required=True, type=str, help='path to file to write cookie and server address to for subsequent bash evaluation')
+
+    args = parser.parse_args()
+
+    vpn_server = args.vpn_server
+    vpn_username = args.username
+    vpn_password = args.password
+    output_file = args.output_file
+
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    s = requests.Session()
+
+    r = s.post(
+        url=f"https://{vpn_server}/+CSCOE+/saml/sp/login",
+        headers={
+            "Host": vpn_server,
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-GB,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": f"https://{vpn_server}/+CSCOE+/logon.html",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Cookie": "webvpnlogin=1; webvpnLang=en",
+            "Upgrade-Insecure-Requests": "1",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+        },
+        data="tgroup=&next=&tgcookieset=&group_list=SAML&Login=Login",
+        verify=False,
+        allow_redirects=False,
+    )
+
+    fs_auth_login_url = r.headers["Location"]
+    fs_auth_host = fs_auth_login_url.split("/")[2]
+
+    r = s.get(
+        url=fs_auth_login_url,
+        headers={
+            "Host": fs_auth_host,
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-GB,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": f"https://{vpn_server}/",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+        },
+        verify=False,
+        allow_redirects=False,
+    )
+    if r.status_code == 302:
+        # When the server detects an existing session, it prompts for
+        # confirmation of username and password using NTLM auth
+        print("Existing session found")
+        login_url = r.headers['Location']
+        r = s.get(
+            url=login_url,
+            headers={
+                "Host": fs_auth_host,
+                "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-GB,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Referer": f"https://{vpn_server}",
+                "DNT": "1",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1",
+            },
+            verify=False,
+            allow_redirects=False,
+            auth=HttpNtlmAuth(vpn_username, vpn_password),
+        )
+    else:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        login_form = soup.find('form', id='loginForm')
+        if not login_form:
+            raise Exception("Failed to find #loginForm")
+        login_url = f"https://{fs_auth_host}{login_form.attrs['action']}"
+
+        r = s.post(
+            login_url,
+            data={
+                'UserName': f'{vpn_username}',
+                'Password': f'{vpn_password}',
+            },
+        )
+
+    while True:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        error_field = soup.find('label', {'id': 'errorText'}) or soup.find('span', {'id': 'errorText'})
+        if error_field and error_field.text:
+            print(error_field.text)
+            print("=" * 80)
+            sys.exit(1)
+
+        if not 'Multi-Factor Authentication' in soup.title:
+            break
+
+        mfa_form = soup.find('form', {'id': 'loginForm'})
+        context = mfa_form.find('input', {'id': 'context'}).attrs['value']
+        code_input = soup.find('input', {'id': 'verificationCodeInput'})
+        if not code_input:
+            print("Waiting for 2FA code prompt...")
+            time.sleep(1)
+            r = s.post(
+                login_url,
+                data={
+                    'AuthMethod': 'AzureMfaAuthentication',
+                    'Context': context,
+                    '__EVENTTARGET': '',
+                },
+                verify=False,
+                allow_redirects=False,
+                auth=HttpNtlmAuth(vpn_username, vpn_password),
+            )
+            continue
+
+        verification_code = input("Verification code: ")
+
+        r = s.post(
+            login_url,
+            headers={
+                "Host": fs_auth_host,
+                "Referer": login_url,
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0",
+            },
+            data={
+                'AuthMethod': 'AzureMfaAuthentication',
+                'Context': context,
+                '__EVENTTARGET': '',
+                'SignIn': 'Sign+in',
+                'VerificationCode': verification_code,
+            },
+            auth=HttpNtlmAuth(vpn_username, vpn_password),
+        )
+        soup = BeautifulSoup(r.text, 'html.parser')
+        error_field = soup.find('label', {'id': 'errorText'}) or soup.find('span', {'id': 'errorText'})
+        if error_field and error_field.text:
+            print(error_field.text)
+            print("=" * 80)
+            sys.exit(1)
+        else:
+            # Code accepted
+            break
+
+    vpn_form = soup.find("form", {'name': 'hiddenform'})
+    if not vpn_form:
+        print("Login failed for unknown reason:")
+        print(soup)
+        print("=" * 80)
+        sys.exit(1)
+    vpn_url = vpn_form.attrs['action']
+    saml_response = vpn_form.find('input').attrs['value']
+
+    r = s.post(
+        vpn_url,
+        data={
+            'SAMLResponse': saml_response,
+        },
+        verify=False,
+    )
+    soup = BeautifulSoup(r.text, 'html.parser')
+    saml_form = soup.find("form") # name='samlform'
+    saml_url = saml_form.attrs['action']
+    saml_response = saml_form.find('input', {'name': 'SAMLResponse'}).attrs['value']
+
+    r = s.post(
+        f'https://{vpn_server}{saml_url}',
+        data={
+            'tgroup': '',
+            'next': '',
+            'tgcookieset': '',
+            'group_list': 'SAML',
+            'username': '',
+            'password': '',
+            'SAMLResponse': saml_response,
+            'Login': 'Login',
+        },
+        headers={
+            "Cookie": "webvpnlogin=1",
+        },
+        verify=False,
+    )
+    cookie = s.cookies['webvpn']
+    print("Got VPN cookie:")
+    print(cookie)
+
+    with open(output_file, 'w') as output_fd:
+        output_fd.write(f"SWC_SERVER={vpn_server}\n")
+        output_fd.write(f"SWC_COOKIE={cookie}\n")
