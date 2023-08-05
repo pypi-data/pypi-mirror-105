5e27c7b991744d03288eb6c6c8bc5630cec55998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:34 +0000

    Add package ausmints_calculator/0.2.1/ausmints_calculator-0.2.1.tar.gz

---
+++ packages/ausmints_calculator/ausmints_calculator-0.2.1.tar.gz/ausmints_calculator-0.2.1/ausmints_calculator/__init__.py
@@ -0,0 +1,10 @@
+"""
+A calculator that can do basic arithmetic operations via the objects
+made from Calculator class
+
+class Calculator - has a variety of arithmetic operations as methods
+"""
+
+from ausmints_calculator.calculator import Calculator
+
+__version__ = "0.2.1"
