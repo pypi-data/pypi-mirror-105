ee0adc6a13851c4e60c2cdf425525cc4da286eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:59 +0000

    Add package ankipandas/0.3.10/ankipandas-0.3.10.tar.gz

---
+++ packages/ankipandas/ankipandas-0.3.10.tar.gz/ankipandas-0.3.10/ankipandas/__init__.py
@@ -0,0 +1,7 @@
+# ours
+import ankipandas.raw
+import ankipandas.util
+from ankipandas.collection import Collection
+from ankipandas.paths import find_db, db_path_input
+from ankipandas.ankidf import AnkiDataFrame
+from ankipandas.util.log import log, set_log_level, set_debug_log_level
---
+++ packages/ankipandas/ankipandas-0.3.10.tar.gz/ankipandas-0.3.10/ankipandas/util/__init__.py
@@ -0,0 +1,13 @@
+""" Various utilities of this package.
+
+.. warning::
+
+    These utilities are less aimed at end users and might therefore be subject
+    to change.
+
+"""
+
+import ankipandas.util.checksum
+import ankipandas.util.dataframe
+import ankipandas.util.log
+import ankipandas.util.misc
