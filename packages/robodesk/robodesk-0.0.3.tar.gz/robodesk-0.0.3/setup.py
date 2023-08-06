bec905f1816342ab2a57068330c4d08b3bbe07ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:41 +0000

    Add package robodesk/0.0.3/robodesk-0.0.3.tar.gz

---
+++ packages/robodesk/robodesk-0.0.3.tar.gz/robodesk-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import pathlib
+
+
+setuptools.setup(
+    name='robodesk',
+    version='0.0.3',
+    description='Multi-task reinforcement learning benchmark.',
+    url='https://github.com/google-research/robodesk',
+    author='Harini Kannan',
+    author_email='hkannan@google.com',
+    packages=['robodesk'],
+    package_data={'robodesk': ['assets/*']},
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'dm_control', 'gym'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Games/Entertainment',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
