62fcdb4931f343bc98b3067a8b827f837535f3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:57 +0000

    Add package mypy-boto3-amplify/1.17.70.post2/mypy_boto3_amplify-1.17.70.post2-py3-none-any.whl

---
+++ packages/mypy-boto3-amplify/mypy_boto3_amplify-1.17.70.post2-py3-none-any.whl/mypy_boto3_amplify/literals.py
@@ -0,0 +1,54 @@
+"""
+Type annotations for amplify service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_amplify.literals import DomainStatus
+
+    data: DomainStatus = "AVAILABLE"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "DomainStatus",
+    "JobStatus",
+    "JobType",
+    "ListAppsPaginatorName",
+    "ListBranchesPaginatorName",
+    "ListDomainAssociationsPaginatorName",
+    "ListJobsPaginatorName",
+    "Platform",
+    "Stage",
+)
+
+
+DomainStatus = Literal[
+    "AVAILABLE",
+    "CREATING",
+    "FAILED",
+    "IN_PROGRESS",
+    "PENDING_DEPLOYMENT",
+    "PENDING_VERIFICATION",
+    "REQUESTING_CERTIFICATE",
+    "UPDATING",
+]
+JobStatus = Literal[
+    "CANCELLED", "CANCELLING", "FAILED", "PENDING", "PROVISIONING", "RUNNING", "SUCCEED"
+]
+JobType = Literal["MANUAL", "RELEASE", "RETRY", "WEB_HOOK"]
+ListAppsPaginatorName = Literal["list_apps"]
+ListBranchesPaginatorName = Literal["list_branches"]
+ListDomainAssociationsPaginatorName = Literal["list_domain_associations"]
+ListJobsPaginatorName = Literal["list_jobs"]
+Platform = Literal["WEB"]
+Stage = Literal["BETA", "DEVELOPMENT", "EXPERIMENTAL", "PRODUCTION", "PULL_REQUEST"]
