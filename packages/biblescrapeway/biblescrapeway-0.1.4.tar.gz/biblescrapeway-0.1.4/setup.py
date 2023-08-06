993ad17919aab087319686d105fb8c0fb095555b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:12 +0000

    Add package biblescrapeway/0.1.4/biblescrapeway-0.1.4.tar.gz

---
+++ packages/biblescrapeway/biblescrapeway-0.1.4.tar.gz/biblescrapeway-0.1.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='biblescrapeway',
+    version='0.1.4',
+    author="Jonathan D B Van Schenck",
+    author_email="vanschej@oregonstate.edu",
+    description='A tool for scraping bible verses from the web',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonathanvanschenck/biblescrapeway",
+    packages=["biblescrapeway"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'beautifulsoup4',
+        'requests'
+    ],
+    entry_points='''
+        [console_scripts]
+        bsw=biblescrapeway.cli:scrap
+    ''',
+)
