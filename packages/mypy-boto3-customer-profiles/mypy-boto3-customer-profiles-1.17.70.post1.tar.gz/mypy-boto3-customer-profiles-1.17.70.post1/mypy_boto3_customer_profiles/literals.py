567f4af04e7626e13f01b58eb2b2b2b857a8b7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:48 +0000

    Add package mypy-boto3-customer-profiles/1.17.70.post1/mypy-boto3-customer-profiles-1.17.70.post1.tar.gz

---
+++ packages/mypy-boto3-customer-profiles/mypy-boto3-customer-profiles-1.17.70.post1.tar.gz/mypy-boto3-customer-profiles-1.17.70.post1/mypy_boto3_customer_profiles/literals.py
@@ -0,0 +1,165 @@
+"""
+Type annotations for customer-profiles service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_customer_profiles/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_customer_profiles.literals import DataPullMode
+
+    data: DataPullMode = "Complete"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "DataPullMode",
+    "FieldContentType",
+    "Gender",
+    "MarketoConnectorOperator",
+    "OperatorPropertiesKeys",
+    "PartyType",
+    "S3ConnectorOperator",
+    "SalesforceConnectorOperator",
+    "ServiceNowConnectorOperator",
+    "SourceConnectorType",
+    "StandardIdentifier",
+    "TaskType",
+    "TriggerType",
+    "ZendeskConnectorOperator",
+)
+
+
+DataPullMode = Literal["Complete", "Incremental"]
+FieldContentType = Literal["EMAIL_ADDRESS", "NAME", "NUMBER", "PHONE_NUMBER", "STRING"]
+Gender = Literal["FEMALE", "MALE", "UNSPECIFIED"]
+MarketoConnectorOperator = Literal[
+    "ADDITION",
+    "BETWEEN",
+    "DIVISION",
+    "GREATER_THAN",
+    "LESS_THAN",
+    "MASK_ALL",
+    "MASK_FIRST_N",
+    "MASK_LAST_N",
+    "MULTIPLICATION",
+    "NO_OP",
+    "PROJECTION",
+    "SUBTRACTION",
+    "VALIDATE_NON_NEGATIVE",
+    "VALIDATE_NON_NULL",
+    "VALIDATE_NON_ZERO",
+    "VALIDATE_NUMERIC",
+]
+OperatorPropertiesKeys = Literal[
+    "CONCAT_FORMAT",
+    "DATA_TYPE",
+    "DESTINATION_DATA_TYPE",
+    "LOWER_BOUND",
+    "MASK_LENGTH",
+    "MASK_VALUE",
+    "MATH_OPERATION_FIELDS_ORDER",
+    "SOURCE_DATA_TYPE",
+    "SUBFIELD_CATEGORY_MAP",
+    "TRUNCATE_LENGTH",
+    "UPPER_BOUND",
+    "VALIDATION_ACTION",
+    "VALUE",
+    "VALUES",
+]
+PartyType = Literal["BUSINESS", "INDIVIDUAL", "OTHER"]
+S3ConnectorOperator = Literal[
+    "ADDITION",
+    "BETWEEN",
+    "DIVISION",
+    "EQUAL_TO",
+    "GREATER_THAN",
+    "GREATER_THAN_OR_EQUAL_TO",
+    "LESS_THAN",
+    "LESS_THAN_OR_EQUAL_TO",
+    "MASK_ALL",
+    "MASK_FIRST_N",
+    "MASK_LAST_N",
+    "MULTIPLICATION",
+    "NOT_EQUAL_TO",
+    "NO_OP",
+    "PROJECTION",
+    "SUBTRACTION",
+    "VALIDATE_NON_NEGATIVE",
+    "VALIDATE_NON_NULL",
+    "VALIDATE_NON_ZERO",
+    "VALIDATE_NUMERIC",
+]
+SalesforceConnectorOperator = Literal[
+    "ADDITION",
+    "BETWEEN",
+    "CONTAINS",
+    "DIVISION",
+    "EQUAL_TO",
+    "GREATER_THAN",
+    "GREATER_THAN_OR_EQUAL_TO",
+    "LESS_THAN",
+    "LESS_THAN_OR_EQUAL_TO",
+    "MASK_ALL",
+    "MASK_FIRST_N",
+    "MASK_LAST_N",
+    "MULTIPLICATION",
+    "NOT_EQUAL_TO",
+    "NO_OP",
+    "PROJECTION",
+    "SUBTRACTION",
+    "VALIDATE_NON_NEGATIVE",
+    "VALIDATE_NON_NULL",
+    "VALIDATE_NON_ZERO",
+    "VALIDATE_NUMERIC",
+]
+ServiceNowConnectorOperator = Literal[
+    "ADDITION",
+    "BETWEEN",
+    "CONTAINS",
+    "DIVISION",
+    "EQUAL_TO",
+    "GREATER_THAN",
+    "GREATER_THAN_OR_EQUAL_TO",
+    "LESS_THAN",
+    "LESS_THAN_OR_EQUAL_TO",
+    "MASK_ALL",
+    "MASK_FIRST_N",
+    "MASK_LAST_N",
+    "MULTIPLICATION",
+    "NOT_EQUAL_TO",
+    "NO_OP",
+    "PROJECTION",
+    "SUBTRACTION",
+    "VALIDATE_NON_NEGATIVE",
+    "VALIDATE_NON_NULL",
+    "VALIDATE_NON_ZERO",
+    "VALIDATE_NUMERIC",
+]
+SourceConnectorType = Literal["Marketo", "S3", "Salesforce", "Servicenow", "Zendesk"]
+StandardIdentifier = Literal["LOOKUP_ONLY", "NEW_ONLY", "PROFILE", "SECONDARY", "UNIQUE"]
+TaskType = Literal["Arithmetic", "Filter", "Map", "Mask", "Merge", "Truncate", "Validate"]
+TriggerType = Literal["Event", "OnDemand", "Scheduled"]
+ZendeskConnectorOperator = Literal[
+    "ADDITION",
+    "DIVISION",
+    "GREATER_THAN",
+    "MASK_ALL",
+    "MASK_FIRST_N",
+    "MASK_LAST_N",
+    "MULTIPLICATION",
+    "NO_OP",
+    "PROJECTION",
+    "SUBTRACTION",
+    "VALIDATE_NON_NEGATIVE",
+    "VALIDATE_NON_NULL",
+    "VALIDATE_NON_ZERO",
+    "VALIDATE_NUMERIC",
+]
