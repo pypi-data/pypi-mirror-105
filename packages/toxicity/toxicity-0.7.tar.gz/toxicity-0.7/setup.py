ff0eb253fd8c80afe1606ba5a59a373abcc38531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:29 +0000

    Add package toxicity/0.7/toxicity-0.7.tar.gz

---
+++ packages/toxicity/toxicity-0.7.tar.gz/toxicity-0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme:
+  long_description = readme.read()
+
+setup(
+  name = 'toxicity',
+  version = '0.7',
+  packages = find_packages(),
+  license='MIT',
+  description = 'Deep learning classifier of russian toxic comments',
+  # long_description = long_description,
+  author = 'Barsukov Nikita',
+  author_email = 'nikita.s.barsukov@gmail.com',
+  url = 'https://github.com/nsbarsukov/toxic-comments-detector',
+  download_url = 'https://github.com/nsbarsukov/toxic-comments-detector/archive/refs/tags/0.1.tar.gz',
+  keywords = ['toxic', 'comments', 'tensorflow', 'classifier'],
+  install_requires=[
+      'tensorflow',
+      'numpy',
+      'navec'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
