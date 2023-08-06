634676380c17036763157922494733b476e38fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:01 +0000

    Add package audformat/0.11.2/audformat-0.11.2-py3-none-any.whl

---
+++ packages/audformat/audformat-0.11.2-py3-none-any.whl/audformat/utils/__init__.py
@@ -0,0 +1,11 @@
+from audformat.core.utils import (
+    concat,
+    intersect,
+    join_labels,
+    join_schemes,
+    map_language,
+    read_csv,
+    to_filewise_index,
+    to_segmented_index,
+    union,
+)
