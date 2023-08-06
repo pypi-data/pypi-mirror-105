556e3d0ba1b57dd487dc5fd43b74381293e00174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:45 +0000

    Add package distributions-implement/0.1/distributions_implement-0.1.tar.gz

---
+++ packages/distributions-implement/distributions_implement-0.1.tar.gz/distributions_implement-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_implement',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_implement'],
+      email='tai.kirstenbelle@yahoo.com',
+      author='Kirsten Tai',
+      zip_safe=False)
