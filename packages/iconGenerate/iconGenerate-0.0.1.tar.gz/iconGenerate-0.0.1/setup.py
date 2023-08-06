27898803229497ff0597d6b3dfa99bb8761829c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:22 +0000

    Add package iconGenerate/0.0.1/iconGenerate-0.0.1.tar.gz

---
+++ packages/iconGenerate/iconGenerate-0.0.1.tar.gz/iconGenerate-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+setup(
+    name='iconGenerate',
+    version='0.0.1',
+    author='Yukun Shang',
+    author_email='ykshang4119@gmail.com',
+    url='https://yukun4119.github.io/',
+    description='Generate img for ios project',
+    packages=['iconGenerate'],
+    install_requires=[],
+)
