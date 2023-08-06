b453b76b2b852fad72c0639592d62bd9994cbeb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:04 +0000

    Add package mypy-boto3-servicecatalog-appregistry/1.17.69.0.post1/mypy_boto3_servicecatalog_appregistry-1.17.69.0.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-servicecatalog-appregistry/mypy_boto3_servicecatalog_appregistry-1.17.69.0.post1-py3-none-any.whl/mypy_boto3_servicecatalog_appregistry/literals.py
@@ -0,0 +1,37 @@
+"""
+Type annotations for servicecatalog-appregistry service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_servicecatalog_appregistry.literals import ListApplicationsPaginatorName
+
+    data: ListApplicationsPaginatorName = "list_applications"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "ListApplicationsPaginatorName",
+    "ListAssociatedAttributeGroupsPaginatorName",
+    "ListAssociatedResourcesPaginatorName",
+    "ListAttributeGroupsPaginatorName",
+    "ResourceType",
+    "SyncAction",
+)
+
+
+ListApplicationsPaginatorName = Literal["list_applications"]
+ListAssociatedAttributeGroupsPaginatorName = Literal["list_associated_attribute_groups"]
+ListAssociatedResourcesPaginatorName = Literal["list_associated_resources"]
+ListAttributeGroupsPaginatorName = Literal["list_attribute_groups"]
+ResourceType = Literal["CFN_STACK"]
+SyncAction = Literal["NO_ACTION", "START_SYNC"]
