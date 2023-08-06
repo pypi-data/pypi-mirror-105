3680586131667d8c01c1c50992f9d90b34a26db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:03 +0000

    Add package qrand/0.3.0a0/qrand-0.3.0a0.tar.gz

---
+++ packages/qrand/qrand-0.3.0a0.tar.gz/qrand-0.3.0a0/qrand/protocols/hadamard.py
@@ -0,0 +1,62 @@
+##    _____  _____
+##   |  __ \|  __ \    AUTHOR: Pedro Rivero
+##   | |__) | |__) |   ---------------------------------
+##   |  ___/|  _  /    DATE: May 12, 2021
+##   | |    | | \ \    ---------------------------------
+##   |_|    |_|  \_\   https://github.com/pedrorrivero
+##
+
+## Copyright 2021 Pedro Rivero
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+from typing import List, Literal
+
+from ..platforms.circuit import QuantumCircuit
+from ..platforms.factory import QuantumFactory
+from ..platforms.job import QuantumJob
+from .protocol import BareQuantumProtocol
+from .result import ProtocolResult, SimpleResult
+
+
+###############################################################################
+## HADAMARD PROTOCOL
+###############################################################################
+class HadamardProtocol(BareQuantumProtocol):
+    ############################### PUBLIC API ###############################
+    def run(self, factory: QuantumFactory) -> ProtocolResult:
+        circuit: QuantumCircuit = self._build_quantum_circuit(factory)
+        _, repetitions = factory.job_partition
+        job: QuantumJob = factory.create_job(circuit, repetitions)
+        output: List[str] = job.execute()
+        return self._parse_output(output)
+
+    def verify(self) -> Literal[False]:
+        return False
+
+    ############################### PRIVATE API ###############################
+    def _build_quantum_circuit(
+        self, factory: QuantumFactory
+    ) -> QuantumCircuit:
+        num_qubits, _ = factory.job_partition
+        circuit: QuantumCircuit = factory.create_circuit(num_qubits)
+        for q in range(circuit.num_qubits):
+            circuit.h(q)
+            circuit.measure(q)
+        return circuit
+
+    def _parse_output(self, output: List[str]) -> ProtocolResult:
+        bitstring: str = ""
+        for stream in output:
+            bitstring += stream
+        return SimpleResult(bitstring)
