99ac92df74cbf2dcdaa0556e452f91b97ce9e0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:17 +0000

    Add package projects-manager/0.8.1.4/projects_manager-0.8.1.4-py3-none-any.whl

---
+++ packages/projects-manager/projects_manager-0.8.1.4-py3-none-any.whl/pmanager/open_project.py
@@ -0,0 +1,28 @@
+from subprocess import run
+from os import path, getcwd
+from pmanager.res import *
+
+
+def open_project(namespace):
+
+    project_name = namespace.project_name[0]
+
+    
+    if not path.exists("config/default_path.conf"):
+
+        dirpath = get_home_dir_path()+"/projects/"+project_name + "/"
+
+    else:
+
+        with open("config/default_path.conf","r",encoding="utf-8") as f:
+
+            dirpath = f.read()+"/"+ project_name +"/"
+
+    pinfo(f"opening project folder : \n {dirpath}")
+
+    if not path.exists(dirpath):
+        perror("This project doest not exists")
+    else:
+        with open("config/default_ide.conf","r") as f:
+            
+            run([f.read(),""+dirpath+"."],shell=True)
