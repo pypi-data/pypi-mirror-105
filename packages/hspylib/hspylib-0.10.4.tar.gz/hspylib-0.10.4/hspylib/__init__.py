9314c2430acd25b0b7fa09bc9839696a5ea08b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:21 +0000

    Add package hspylib/0.10.4/hspylib-0.10.4.tar.gz

---
+++ packages/hspylib/hspylib-0.10.4.tar.gz/hspylib-0.10.4/hspylib/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.10.4
+#
+# Package: main.hspylib
+"""Package initialization."""
+
+__all__ = [
+  '__main__', 
+  'addins', 
+  'core', 
+  'modules'
+]
+__version__ = "0.10.4"
---
+++ packages/hspylib/hspylib-0.10.4.tar.gz/hspylib-0.10.4/hspylib/modules/cli/icons/font_awesome/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.10.4
+#
+# Package: main.hspylib.modules.cli.icons.font_awesome
+"""Package initialization."""
+
+__all__ = [
+  'awesome', 
+  'dashboard_icons', 
+  'form_icons'
+]
