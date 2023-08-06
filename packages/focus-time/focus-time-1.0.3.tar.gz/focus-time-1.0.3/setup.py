08d884036c53ca48a5905d74d90503f64c40a463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:25 +0000

    Add package focus-time/1.0.3/focus-time-1.0.3.tar.gz

---
+++ packages/focus-time/focus-time-1.0.3.tar.gz/focus-time-1.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="focus-time",
+    version="1.0.3",
+    author="Philip Shamash",
+    author_email="philip.shamash.17@ucl.ac.uk",
+    description="Focus time is an application in python to help you focus for a set amount of time",
+    license = "GNU GENERAL PUBLIC LICENSE",
+    homepage = "https://github.com/philshams/focus-time",
+    packages=["focus"],
+    package_dir={'focus':'focus'},
+    package_data={'focus': ['../data/*.mp3']},
+    install_requires=['DateTime>=4.3',
+                      'numpy>=1.20.2',
+                      'playsound>=1.2.2'                
+                      ],
+    entry_points={
+        "console_scripts": [
+            "focus = focus.__main__:main"
+        ]
+    }
+)
