9ada51e87230f87c6ea34be88626d47dada38249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:12 +0000

    Add package awyes/1.0.45/awyes-1.0.45.tar.gz

---
+++ packages/awyes/awyes-1.0.45.tar.gz/awyes-1.0.45/awyes/image.py
@@ -0,0 +1,25 @@
+from re import sub
+from pprint import pprint
+from os.path import normpath, join
+
+
+def deploy_images(self):
+    client = self.__class__.docker_client
+
+    for dockerfile in self.images:
+        image_name = sub(".Dockerfile", "", dockerfile)
+        remote_tag = f"{self.account_id}.dkr.ecr.{self.region}.amazonaws.com/{image_name}"
+
+        image, *_ = client.images.build(
+            tag=image_name,
+            path=self.root_path,
+            dockerfile=normpath(join(
+                self.root_path,
+                self.config_path,
+                dockerfile
+            ))
+        )
+
+        image.tag(repository=remote_tag)
+
+        print([line for line in client.images.push(repository=remote_tag)])
