9b2df317936291b1078315e63f9c2bbd8274322a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:43 +0000

    Add package quantclean/0.0.1/quantclean-0.0.1.tar.gz

---
+++ packages/quantclean/quantclean-0.0.1.tar.gz/quantclean-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='quantclean',
+    version='0.0.1',
+    description='Quantclean is a program that reformats every financial dataset to US Equity TradeBar',
+    py_modules=['quantclean'],
+    package_dir={'':'src'},
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url='https://github.com/ssantoshp/quantclean',
+    author = "Santosh Passoubady",
+    author_email = "santoshpassoubady@gmail.com",
+    license='MIT',
+    install_requires=[
+          'pandas_datareader',
+          'pandas'
+      ],
+)
