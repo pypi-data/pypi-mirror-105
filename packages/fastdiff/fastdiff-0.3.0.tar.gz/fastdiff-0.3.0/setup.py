920a22f79cf90768fc59fe277452b4a65a7952a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:17 +0000

    Add package fastdiff/0.3.0/fastdiff-0.3.0.tar.gz

---
+++ packages/fastdiff/fastdiff-0.3.0.tar.gz/fastdiff-0.3.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'wasmer>=1.0.0',
+    'wasmer-compiler-cranelift>=1.0.0'
+]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Syrus Akbary",
+    author_email='me@syrusakbary.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="A fast native implementation of diff algorithm with a pure python fallback",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='fastdiff',
+    name='fastdiff',
+    packages=find_packages(include=['fastdiff']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/syrusakbary/fastdiff',
+    version='0.3.0',
+    zip_safe=False,
+)
