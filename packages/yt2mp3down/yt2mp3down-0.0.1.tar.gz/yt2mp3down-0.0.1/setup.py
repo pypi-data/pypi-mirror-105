47db2cec0a5d6a848c4f740a8ce96c1c38f7a560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:31 +0000

    Add package yt2mp3down/0.0.1/yt2mp3down-0.0.1.tar.gz

---
+++ packages/yt2mp3down/yt2mp3down-0.0.1.tar.gz/yt2mp3down-0.0.1/setup.py
@@ -0,0 +1,25 @@
+  
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='yt2mp3down',
+  version='0.0.1',
+  description='A very basic calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Hasinthaka Piyumal',
+  author_email='hasinthakapiyumal@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='yt2mp3', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
