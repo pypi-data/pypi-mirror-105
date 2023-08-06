96aaa67b51bde72e66c00b276b0efdcf565a6408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:57 +0000

    Add package fb-sapp/0.4.0/fb_sapp-0.4.0-py3-none-any.whl

---
+++ packages/fb-sapp/fb_sapp-0.4.0-py3-none-any.whl/sapp/ui/frontend/build/precache-manifest.44772c258f3d09fcc639328575acc5bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002c59bb799000f488f863a025604afb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "587f9ea89d420c0bb6b7",
+    "url": "/static/css/2.4e69708a.chunk.css"
+  },
+  {
+    "revision": "2e87d48eb809f50f6068",
+    "url": "/static/css/main.de24d001.chunk.css"
+  },
+  {
+    "revision": "587f9ea89d420c0bb6b7",
+    "url": "/static/js/2.90c7946e.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.90c7946e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e87d48eb809f50f6068",
+    "url": "/static/js/main.f594b70c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
