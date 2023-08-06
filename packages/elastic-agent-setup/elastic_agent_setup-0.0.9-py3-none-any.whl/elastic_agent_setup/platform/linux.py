b9dbdf4054ba982be6b804c4c0c8f5e5fd831d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:42 +0000

    Add package elastic-agent-setup/0.0.9/elastic_agent_setup-0.0.9-py3-none-any.whl

---
+++ packages/elastic-agent-setup/elastic_agent_setup-0.0.9-py3-none-any.whl/elastic_agent_setup/platform/linux.py
@@ -0,0 +1,12 @@
+import os
+from .platform import Platform
+from ..settings import Settings
+
+
+class Linux(Platform):
+
+    def extract(self):
+        import tarfile
+        tar = tarfile.open(Settings.download_path, "r:gz")
+        tar.extractall()
+        tar.close()
