f42d1c003176edb193b0e6b9dafd35a9cf016db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:20 +0000

    Add package twofourseven/0.1.11/twofourseven-0.1.11.tar.gz

---
+++ packages/twofourseven/twofourseven-0.1.11.tar.gz/twofourseven-0.1.11/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pandas>=1", "requests>=2", "lxml>=4", "numpy>=1"]
+
+setup(
+    name = "twofourseven",
+    version = "0.1.11",
+    author = "Nathan Reeb",
+    author_email = "Nathan.Reeb94@outlook.com",
+    description = "Package to scrape 247Sports website for recruiting data",
+    long_description = readme,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Natron0919/twofourseven/",
+    packages = find_packages(),
+    install_requires = requirements,
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ]
+)
