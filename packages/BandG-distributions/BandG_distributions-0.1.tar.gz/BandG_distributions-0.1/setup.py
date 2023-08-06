e99564ade01b9b8f24563e3735ad72b36ea3eb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:46 +0000

    Add package BandG-distributions/0.1/BandG_distributions-0.1.tar.gz

---
+++ packages/BandG-distributions/BandG_distributions-0.1.tar.gz/BandG_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BandG_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['BandG_distributions'],
+      author = "Cristhian Garcia",
+      author_email = "cristhiancjgs20@gmail.com",
+      zip_safe=False)
