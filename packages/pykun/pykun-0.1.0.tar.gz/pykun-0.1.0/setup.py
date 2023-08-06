55fa3cf68086a6947a3d99c6deee26bb2e91ae5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:51 +0000

    Add package pykun/0.1.0/pykun-0.1.0.tar.gz

---
+++ packages/pykun/pykun-0.1.0.tar.gz/pykun-0.1.0/setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup, find_packages
+
+
+
+setup(name='pykun',
+
+      version='0.1.0',
+
+      url='https://github.com/kunrunic/pykun',
+
+      license='MIT',
+
+      author='kunrunic',
+
+      author_email='kunrunic7272@gmail.com',
+
+      description='Develop a Callection of useful packages',
+
+      packages=find_packages(),
+
+      long_description=open('README.md').read(),
+
+      zip_safe=False,
+
+      setup_requires=[],
+
+      test_suite='')
