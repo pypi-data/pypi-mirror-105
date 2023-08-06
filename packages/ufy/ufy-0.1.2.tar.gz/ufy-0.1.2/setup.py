3709e5e3bfb060cca355dac798cbafaebda04979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:39 +0000

    Add package ufy/0.1.2/ufy-0.1.2.tar.gz

---
+++ packages/ufy/ufy-0.1.2.tar.gz/ufy-0.1.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2021/4/22 19:36
+# @Author   : ufy
+# @Email    : antarm@outlook.com / 549147808@qq.com
+# @file     : setup.py.py
+# @info     :
+
+from setuptools import setup
+setup(name='ufy',
+      version = '0.1.2',
+      description = 'some tools for ufy',
+      author = 'ufy',
+      author_eamil = 'antarm@outlook.com',
+      url = 'https://github.com/antarm/ufy',
+      packages=['ufy','ufy.core','ufy.core.ML','ufy.core.DL','ufy.core.ML.compaction','ufy.CV','ufy.datasets','ufy.utils','ufy.HX','ufy.HX.data_process'])
---
+++ packages/ufy/ufy-0.1.2.tar.gz/ufy-0.1.2/ufy/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2021/4/22 19:04
+# @Author   : ufy
+# @Email    : antarm@outlook.com / 549147808@qq.com
+# @file     : __init__.py
+# @info     :
+
+__version__ = '0.1.0'
+
+print('ufy version:',__version__)
