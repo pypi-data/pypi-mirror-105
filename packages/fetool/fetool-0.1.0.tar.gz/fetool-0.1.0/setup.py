ffb33bdc574517c2117c6c25b6096df6e80ef328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:47 +0000

    Add package fetool/0.1.0/fetool-0.1.0.tar.gz

---
+++ packages/fetool/fetool-0.1.0.tar.gz/fetool-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 4Paradigm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="fetool",
+    version="0.1.0",
+    author="4Paradigm",
+    author_email="chendihao@4paradigm.com",
+    url="https://github.com/4paradigm/SparkFE",
+    description= "The featue extraction tools for SparkFE",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "fetool=fetool:main"
+        ],
+    })
