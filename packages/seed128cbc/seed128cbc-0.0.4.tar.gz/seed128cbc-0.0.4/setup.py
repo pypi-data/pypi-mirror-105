98bf7008d5aa341b1b778eef8822fbaf36f58534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:46 +0000

    Add package seed128cbc/0.0.4/seed128cbc-0.0.4.tar.gz

---
+++ packages/seed128cbc/seed128cbc-0.0.4.tar.gz/seed128cbc-0.0.4/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+	name='seed128cbc',
+	version='0.0.4',
+	description='SEED128_CBC TYPE ENCRYPTION & DECRYPTION',
+	author='Yoonseok',
+	author_email='ys12247@gmail.com',
+	py_modules=['kisa_seed_cbc.py'],
+	license="MIT",
+	packages=setuptools.find_packages()
+	
+)
