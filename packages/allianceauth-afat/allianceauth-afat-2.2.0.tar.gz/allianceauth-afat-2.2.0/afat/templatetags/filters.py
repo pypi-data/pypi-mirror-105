765c27981549f218738a6cf3b299a58e3f093074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:57 +0000

    Add package allianceauth-afat/2.2.0/allianceauth-afat-2.2.0.tar.gz

---
+++ packages/allianceauth-afat/allianceauth-afat-2.2.0.tar.gz/allianceauth-afat-2.2.0/afat/templatetags/filters.py
@@ -0,0 +1,19 @@
+import calendar
+
+from django.template.defaulttags import register
+
+
+@register.filter
+def month_name(value):
+    """
+    template tag :: get month name from month number
+    example: {{ event.month|month_name }}
+    :param value:
+    :type value:
+    :return:
+    :rtype:
+    """
+
+    value = int(value)
+
+    return calendar.month_name[value]
