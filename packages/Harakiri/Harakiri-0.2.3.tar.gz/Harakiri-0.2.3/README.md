b8774ab197167a0f170ddfa919af21a13e3fdc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:14 +0000

    Add package Harakiri/0.2.3/Harakiri-0.2.3.tar.gz

---
+++ packages/Harakiri/Harakiri-0.2.3.tar.gz/Harakiri-0.2.3/README.md
@@ -0,0 +1,25 @@
+# Harakiri
+
+> 만당에 온 걸 환영한다
+
+[만당 API](https://github.com/ManjiKR/ManjiApi) 래퍼
+
+## 설치
+
+```
+pip install Harakiri
+```
+
+## 사용
+
+```py
+import harakiri
+
+client = harakiri.Client()
+```
+
+### Manji
+
+[프론트엔드](https://github.com/ManjiKR/Manji)
+[백엔드](https://github.com/ManjiKR/ManjiApi)
+[데이터](https://github.com/ManjiKR/yoshimitsu_frame_data)
---
+++ packages/Harakiri/Harakiri-0.2.3.tar.gz/Harakiri-0.2.3/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name="Harakiri",
+    version="0.2.3",
+    license="Apache-2.0",
+    author="ombe1229",
+    author_email="h3236516@gmail.com",
+    description="ManjiApi Wrapper",
+    long_description=open("pypi/README.md").read(),
+    url="https://github.com/ManjiKR/Harakiri",
+    packages=setuptools.find_packages(),
+)
