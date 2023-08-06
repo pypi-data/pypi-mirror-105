29676fe9c0e6c4d2c149d73a72d70df47680a3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:53 +0000

    Add package rethinkdbcm/0.1.6/rethinkdbcm-0.1.6-py3-none-any.whl

---
+++ packages/rethinkdbcm/rethinkdbcm-0.1.6-py3-none-any.whl/rethinkdbcm/__init__.py
@@ -0,0 +1,23 @@
+"""
+RethinkDB context manager
+"""
+
+__author__ = 'Fedorov Mikhail'
+__maintainer__ = 'Fedorov Mikhail'
+
+__email__ = 'jwvsol@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.1.6'
+__doc__ = 'RethinkDB context manager'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+    '__doc__',
+)
+
+from .rethinkdbcm import WorkRethinkDB
