7e9cd8305202ca0a8c218b052f25beb016973b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:32 +0000

    Add package elastic-agent-setup/0.0.3/elastic-agent-setup-0.0.3.tar.gz

---
+++ packages/elastic-agent-setup/elastic-agent-setup-0.0.3.tar.gz/elastic-agent-setup-0.0.3/elastic_agent_setup/download.py
@@ -0,0 +1,17 @@
+import os
+from .core import Core, Settings
+
+
+class Download(Core):
+
+    host = 'https://artifacts.elastic.co/downloads/beats/elastic-agent/{endpoint}'
+    endpoint = Settings.download_endpoint
+    kwargs = {
+        'stream': True
+    }
+
+    def parse_response(self, response):
+        with open(Settings.download_path, 'wb') as f:
+            for chunk in response.raw.stream(1024, decode_content=False):
+                if chunk:
+                    f.write(chunk)
