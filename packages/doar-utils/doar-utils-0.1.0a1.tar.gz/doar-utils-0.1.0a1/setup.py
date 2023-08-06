056ac8d410081b9e94905cd030dcf547d05b263b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:32 +0000

    Add package doar-utils/0.1.0a1/doar-utils-0.1.0a1.tar.gz

---
+++ packages/doar-utils/doar-utils-0.1.0a1.tar.gz/doar-utils-0.1.0a1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['doar', 'doar.utils']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'doar-utils',
+    'version': '0.1.0a1',
+    'description': '',
+    'long_description': None,
+    'author': 'Yehuda Deutsch',
+    'author_email': 'yeh@uda.co.il',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
