2743ac170c860bf0b99b901e7260a6925993b7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:14 +0000

    Add package pickledir/0.1.5/pickledir-0.1.5.tar.gz

---
+++ packages/pickledir/pickledir-0.1.5.tar.gz/pickledir-0.1.5/setup.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from setuptools import setup
+
+
+def load_module_dict(filename: str) -> dict:
+    import importlib.util as ilu
+    filename = Path(__file__).parent / filename
+    spec = ilu.spec_from_file_location('', filename)
+    module = ilu.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__dict__
+
+
+setup(
+    name="pickledir",
+    version=load_module_dict('pickledir/_constants.py')['__version__'],
+    author="Artёm IG",
+    author_email="ortemeo@gmail.com",
+    url='https://github.com/rtmigo/pickledir_py#readme',
+
+    python_requires='>=3.6',
+    install_requires=[],
+    packages=['pickledir'],
+
+    description="File-based key-value storage. Keys are strings, values are "
+                "objects pickle serializable.",
+
+    keywords="cache pickle file directory".split(),
+
+    long_description=(Path(__file__).parent / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
