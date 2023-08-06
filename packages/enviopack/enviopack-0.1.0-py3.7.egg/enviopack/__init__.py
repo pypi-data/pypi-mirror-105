b85814706f033ae7af9560a278a0102d4b8c235a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:05 +0000

    Add package enviopack/0.1.0/enviopack-0.1.0-py3.7.egg

---
+++ packages/enviopack/enviopack-0.1.0-py3.7.egg/enviopack/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from .Auth.Auth import Auth
+from .Collects.Collects import Collects
+from .Orders.Orders import Orders
+from .Pickings.Pickings import Pickings
+from .Quote.Quote import Quote
+
+__version__ = "0.1.0"
+__author__ = "Federico Gobea"
+
