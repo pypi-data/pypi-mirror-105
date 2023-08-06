b7c13dc0fd747bf4735121dc420f667e995e8697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:06 +0000

    Add package habr-parser/1.1/habr_parser-1.1.tar.gz

---
+++ packages/habr-parser/habr_parser-1.1.tar.gz/habr_parser-1.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+setup(
+    name='habr_parser',
+    packages=['habr_parser'],
+    version='1.1',
+    license='gpl-3.0',
+    description='Parser for habr.com',
+    author='goonate',
+    author_email='goonate@cock.li',
+    url='https://gitlab.com/goonate/habr_parser',
+    download_url='https://gitlab.com/goonate/habr-parser/-/archive/v1.1/habr-parser-v1.1.tar.gz',
+    keywords=['habr_parser', 'habr', 'parser'],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6'
+)
