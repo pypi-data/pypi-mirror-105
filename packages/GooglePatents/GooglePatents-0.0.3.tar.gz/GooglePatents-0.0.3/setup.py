3329f8e3924b3517648614967cd8da6099d4bf3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:03 +0000

    Add package GooglePatents/0.0.3/GooglePatents-0.0.3.tar.gz

---
+++ packages/GooglePatents/GooglePatents-0.0.3.tar.gz/GooglePatents-0.0.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="GooglePatents",
+    version='0.0.3',
+    long_description=Path('README.md').read_text(),
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=[
+        'requests',
+        'pandas',
+        'beautifulsoup4',
+        'lxml',
+    ],
+)
