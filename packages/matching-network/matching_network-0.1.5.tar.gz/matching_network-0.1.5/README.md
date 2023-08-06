d9bbe79ae307e6e211426260508cf8913d9d9e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:49 +0000

    Add package matching-network/0.1.5/matching_network-0.1.5.tar.gz

---
+++ packages/matching-network/matching_network-0.1.5.tar.gz/matching_network-0.1.5/README.md
@@ -0,0 +1,104 @@
+<div align="right" style="text-align:right"><i><a href="https://urbanij.github.io/">Francesco Urbani</a></i></div>
+
+<!-- Index of Jupyter (IPython) Notebooks -->
+
+|Jupyter Notebooks                                                                                                                              |
+|-----------------------------------------------------------------------------------------------------------------------------------------------|
+|<a href="https://github.com/urbanij/matching-network/blob/master/aux/L-section_matching_calculations.ipynb">L-section_matching_calculations</a>|
+|<a href="https://github.com/urbanij/matching-network/blob/master/aux/calculations.ipynb">Calculations</a>                                      |
+
+
+---
+
+
+[![Downloads](https://pepy.tech/badge/matching-network)](https://pepy.tech/project/matching-network)
+
+
+Installation
+============
+
+```sh
+pip install matching_network
+```
+
+
+Usage
+=============
+
+#### From the CLI
+```bash
+$ matching_network --from 100 --to 20+43j --freq 1e9
+```
+```
+From 100 Ω to (20+43j) Ω
+
+normalized starting impedance = 100Ω / (20+43j)Ω = 0.88928-1.912j
+
+#solutions: 4
+
+shunt-series
+	Shunt Inductor:
+	X = 50 Ω ⇔ B = -20 mS
+	L = 7.9577 nH  (@ 1 GHz)
+	Series Inductor:
+	X = 3 Ω ⇔ B = -333.33 mS
+	L = 477.46 pH  (@ 1 GHz)
+shunt-series
+	Shunt Capacitor:
+	X = -50 Ω ⇔ B = 20 mS
+	C = 3.1831 pF  (@ 1 GHz)
+	Series Inductor:
+	X = 83 Ω ⇔ B = -12.048 mS
+	L = 13.21 nH  (@ 1 GHz)
+series-shunt
+	Series Inductor:
+	X = 35.285 Ω ⇔ B = -28.341 mS
+	L = 5.6157 nH  (@ 1 GHz)
+	Shunt Inductor:
+	X = 62.571 Ω ⇔ B = -15.982 mS
+	L = 9.9585 nH  (@ 1 GHz)
+series-shunt
+	Series Capacitor:
+	X = -35.285 Ω ⇔ B = 28.341 mS
+	C = 4.5106 pF  (@ 1 GHz)
+	Shunt Inductor:
+	X = 44.929 Ω ⇔ B = -22.257 mS
+	L = 7.1507 nH  (@ 1 GHz)
+```
+
+#### Inside Python
+
+```python
+>>> import matching_network as mn
+>>>
+>>> impedance_you_have         = 90 + 32j # Ω
+>>> impedance_you_want_to_have = 175      # Ω
+>>>
+>>> frequency                  = 900e6    # Hz
+>>>
+>>> mn.L_section_matching(impedance_you_have, impedance_you_want_to_have, frequency).match()
+From (90+32j) Ω to 175 Ω
+
+normalized starting impedance = (90+32j)Ω/175Ω = 0.51429+0.18286j
+
+#solutions: 2
+
+series-shunt
+    Series Inductor:
+    X = 55.464 Ω ⇔ B = -18.03 mS
+    L = 9.8082 nH  (@ 900 MHz)
+    Shunt Capacitor:
+    X = -180.07 Ω ⇔ B = 5.5533 mS
+    C = 982.04 fF  (@ 900 MHz)
+
+series-shunt
+    Series Capacitor:
+    X = -119.46 Ω ⇔ B = 8.3707 mS
+    C = 1.4803 pF  (@ 900 MHz)
+    Shunt Inductor:
+    X = 180.07 Ω ⇔ B = -5.5533 mS
+    L = 31.844 nH  (@ 900 MHz)
+
+>>>
+```
+
---
+++ packages/matching-network/matching_network-0.1.5.tar.gz/matching_network-0.1.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['matching_network']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0', 'quantiphy>=2.13.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['matching_network = matching_network.__main__:cli']}
+
+setup_kwargs = {
+    'name': 'matching-network',
+    'version': '0.1.5',
+    'description': 'Design lumped-parameters matching networks (L-sections)',
+    'long_description': '<div align="right" style="text-align:right"><i><a href="https://urbanij.github.io/">Francesco Urbani</a></i></div>\n\n<!-- Index of Jupyter (IPython) Notebooks -->\n\n|Jupyter Notebooks                                                                                                                              |\n|-----------------------------------------------------------------------------------------------------------------------------------------------|\n|<a href="https://github.com/urbanij/matching-network/blob/master/aux/L-section_matching_calculations.ipynb">L-section_matching_calculations</a>|\n|<a href="https://github.com/urbanij/matching-network/blob/master/aux/calculations.ipynb">Calculations</a>                                      |\n\n\n---\n\n\n[![Downloads](https://pepy.tech/badge/matching-network)](https://pepy.tech/project/matching-network)\n\n\nInstallation\n============\n\n```sh\npip install matching_network\n```\n\n\nUsage\n=============\n\n#### From the CLI\n```bash\n$ matching_network --from 100 --to 20+43j --freq 1e9\n```\n```\nFrom 100 Ω to (20+43j) Ω\n\nnormalized starting impedance = 100Ω / (20+43j)Ω = 0.88928-1.912j\n\n#solutions: 4\n\nshunt-series\n\tShunt Inductor:\n\tX = 50 Ω ⇔ B = -20 mS\n\tL = 7.9577 nH  (@ 1 GHz)\n\tSeries Inductor:\n\tX = 3 Ω ⇔ B = -333.33 mS\n\tL = 477.46 pH  (@ 1 GHz)\nshunt-series\n\tShunt Capacitor:\n\tX = -50 Ω ⇔ B = 20 mS\n\tC = 3.1831 pF  (@ 1 GHz)\n\tSeries Inductor:\n\tX = 83 Ω ⇔ B = -12.048 mS\n\tL = 13.21 nH  (@ 1 GHz)\nseries-shunt\n\tSeries Inductor:\n\tX = 35.285 Ω ⇔ B = -28.341 mS\n\tL = 5.6157 nH  (@ 1 GHz)\n\tShunt Inductor:\n\tX = 62.571 Ω ⇔ B = -15.982 mS\n\tL = 9.9585 nH  (@ 1 GHz)\nseries-shunt\n\tSeries Capacitor:\n\tX = -35.285 Ω ⇔ B = 28.341 mS\n\tC = 4.5106 pF  (@ 1 GHz)\n\tShunt Inductor:\n\tX = 44.929 Ω ⇔ B = -22.257 mS\n\tL = 7.1507 nH  (@ 1 GHz)\n```\n\n#### Inside Python\n\n```python\n>>> import matching_network as mn\n>>>\n>>> impedance_you_have         = 90 + 32j # Ω\n>>> impedance_you_want_to_have = 175      # Ω\n>>>\n>>> frequency                  = 900e6    # Hz\n>>>\n>>> mn.L_section_matching(impedance_you_have, impedance_you_want_to_have, frequency).match()\nFrom (90+32j) Ω to 175 Ω\n\nnormalized starting impedance = (90+32j)Ω/175Ω = 0.51429+0.18286j\n\n#solutions: 2\n\nseries-shunt\n    Series Inductor:\n    X = 55.464 Ω ⇔ B = -18.03 mS\n    L = 9.8082 nH  (@ 900 MHz)\n    Shunt Capacitor:\n    X = -180.07 Ω ⇔ B = 5.5533 mS\n    C = 982.04 fF  (@ 900 MHz)\n\nseries-shunt\n    Series Capacitor:\n    X = -119.46 Ω ⇔ B = 8.3707 mS\n    C = 1.4803 pF  (@ 900 MHz)\n    Shunt Inductor:\n    X = 180.07 Ω ⇔ B = -5.5533 mS\n    L = 31.844 nH  (@ 900 MHz)\n\n>>>\n```\n\n',
+    'author': 'Francesco Urbani',
+    'author_email': 'francescourbanidue@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://pypi.org/project/matching-network/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
