99d92a03c5f58c95df75aed670f9c6e8205d5a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:13 +0000

    Add package lumo/0.1.1.18/lumo-0.1.1.18-py3-none-any.whl

---
+++ packages/lumo/lumo-0.1.1.18-py3-none-any.whl/lumo/__init__.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+__version__ = "0.1.1.18"
+
+from .utils.paths import global_config_path as _
+from .utils.keys import K
+
+from .kit import (
+    F, Q,
+    BaseParams, Params,
+    Meter, AvgMeter,
+
+    DatasetBuilder, DataDelegate, DataModule,
+    Saver, Random,
+
+    TrainerExperiment, exphook,
+    globs, Trainer, callbacks
+)
+
+from . import calculate  # initialize schedule attr classes
+
+_()  # check and initialize global config
