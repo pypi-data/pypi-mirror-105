a7c6534a00491d543c2613bade0e3b187207d1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:31 +0000

    Add package strawberry-graphql-django/0.1.5/strawberry_graphql_django-0.1.5-py3-none-any.whl

---
+++ packages/strawberry-graphql-django/strawberry_graphql_django-0.1.5-py3-none-any.whl/strawberry_django/__init__.py
@@ -0,0 +1,9 @@
+from .fields import field, mutation
+from .mutations.fields import mutations
+from .mutations.auth import AuthMutation
+from .queries.fields import queries
+from .registers import TypeRegister
+from . import filters
+from .filters import filter
+from .resolvers import django_resolver
+from .type import input, type
