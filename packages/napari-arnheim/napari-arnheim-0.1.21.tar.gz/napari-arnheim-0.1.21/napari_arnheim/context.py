b1359022c54a2830cf06090ebe180a357856c033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:30 +0000

    Add package napari-arnheim/0.1.21/napari-arnheim-0.1.21.tar.gz

---
+++ packages/napari-arnheim/napari-arnheim-0.1.21.tar.gz/napari-arnheim-0.1.21/napari_arnheim/context.py
@@ -0,0 +1,43 @@
+import sys
+from napari_arnheim.widgets.arnheim import ArnheimWidget
+import napari
+from PyQt5 import QtCore
+from qasync import QEventLoop
+import asyncio
+
+class gui_qt:
+    
+    def __init__(self, *args, bergen_params= {}):
+        self.args = args
+        self.bergen_params = bergen_params
+
+    
+          
+    def __enter__(self): 
+        self.napari_context = napari.gui_qt()
+        self.app = self.napari_context.__enter__()
+        print("hahahah")
+        self.viewer = napari.Viewer()
+        self.widget = ArnheimWidget(bergen_params=self.bergen_params)
+        self.viewer.window.add_dock_widget(self.widget, area="right")
+
+        self.loop = QEventLoop(self.app)
+        asyncio.set_event_loop(self.loop)
+        self.loop.__enter__()
+        self.loop.run_until_complete(self.widget.connectBergen())
+
+        self.client = self.widget.bergen
+        self.helper = self.widget.helper
+
+        print("ffffffffffffffffffff")
+        return self
+      
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print(exc_type, exc_value, exc_traceback)
+
+        self.loop.__exit__(exc_type, exc_value, exc_traceback)
+        self.napari_context.__exit__(exc_type, exc_value, exc_traceback)
+        print("^^^^^")
+        print("To be ignored")
+        sys.exit()
+        
