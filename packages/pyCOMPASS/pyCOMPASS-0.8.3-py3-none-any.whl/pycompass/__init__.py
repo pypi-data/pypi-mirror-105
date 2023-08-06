d97cb49435c6ca6362675847c0572e078ffdc129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:35 +0000

    Add package pyCOMPASS/0.8.3/pyCOMPASS-0.8.3-py3-none-any.whl

---
+++ packages/pyCOMPASS/pyCOMPASS-0.8.3-py3-none-any.whl/pycompass/__init__.py
@@ -0,0 +1,15 @@
+from pycompass.compendium import Compendium
+from pycompass.connect import Connect
+from pycompass.module import Module
+from pycompass.biological_feature import BiologicalFeature
+from pycompass.experiment import Experiment
+from pycompass.ontology import Ontology
+from pycompass.ontology_node import OntologyNode
+from pycompass.platform import Platform
+from pycompass.plot import Plot
+from pycompass.sample import Sample
+from pycompass.sample_set import SampleSet
+from pycompass.annotation import Annotation
+from pycompass.sparql import Sparql
+
+__version__ = "0.8.3"
