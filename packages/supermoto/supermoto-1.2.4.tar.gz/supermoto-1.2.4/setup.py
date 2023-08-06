8fca11f3049059ed2101db5841701f44e695571c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:05 +0000

    Add package supermoto/1.2.4/supermoto-1.2.4.tar.gz

---
+++ packages/supermoto/supermoto-1.2.4.tar.gz/supermoto-1.2.4/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="supermoto",
+    version="1.2.4",
+    description='Helpers for "moto" tests',
+    author="Ville M. Vainio",
+    author_email="ville.vainio@basware.com",
+    url="https://github.com/vivainio/supermoto",
+    packages=["supermoto"],
+    package_data={
+        "supermoto": ["py.typed"],
+    },
+    install_requires=[],
+)
