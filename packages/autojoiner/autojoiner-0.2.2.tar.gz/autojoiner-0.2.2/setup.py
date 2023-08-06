a818b938f184db3ef7ad94844f539f3f37cf29a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:51 +0000

    Add package autojoiner/0.2.2/autojoiner-0.2.2.tar.gz

---
+++ packages/autojoiner/autojoiner-0.2.2.tar.gz/autojoiner-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from os import path
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="autojoiner",
+    packages=["autojoiner"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.2.2",
+    install_requires=[
+        "click",
+        "selenium",
+    ],
+    python_requires=">=3.8",
+    entry_points="""
+        [console_scripts]
+        autojoiner=autojoiner.__main__:main
+    """,
+    author="Rishikesh Rachchh",
+    author_email="rishikeshrachchh@gmail.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="MIT",
+    url="https://github.com/rishi255/autojoiner",
+)
