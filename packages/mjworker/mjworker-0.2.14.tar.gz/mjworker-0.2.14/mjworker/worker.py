78091e0c548f68eabab302651ab76b35e8df72fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:06 +0000

    Add package mjworker/0.2.14/mjworker-0.2.14.tar.gz

---
+++ packages/mjworker/mjworker-0.2.14.tar.gz/mjworker-0.2.14/mjworker/worker.py
@@ -0,0 +1,70 @@
+import time
+from  multiprocessing import Process
+import click
+import requests
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
+
+from mjworker.variables import sync_pidfile
+from mjworker.sync import sync_runner
+from mjworker.publish import publish_runner
+from mjworker.task import task_runner
+
+
+@click.group()
+def cli():
+    '''
+    This is the monitor agent
+    '''
+
+@click.command()
+@click.option("--timeout", default=10, help="timeout")
+@click.option("--server", required=True, help="Controller infomation")
+@click.option("--label", required=True, help="worker unique identification")
+@click.option("--tag", multiple=True, help="worker authentication code")
+def register(server, label, tag, timeout):
+    timestamp = str(round(time.time() * 1000000))
+    tags = {}
+    for t in tag:
+        key = t.strip().split('=', 1)[0]
+        value = t.strip().split('=', 1)[1]
+        tags[key] = value
+    data = {"sessionId": "", "timestamp": timestamp, "label": label, "tags": tags}
+    headers = {"Content-Type": "application/json"}
+    url = urljoin(server, "/worker/RegisterWorker")
+    try:
+        r = requests.post(url=url, json=data, timeout=timeout, headers=headers)
+    except Exception as e:
+        print(e)
+    else:
+        if r.status_code != 200:
+            print(r)
+        print(r.text)
+
+@click.command()
+@click.option("--timeout", default=10, help="command timeout")
+@click.option("--server", required=True, help="Controller infomation")
+@click.option("--workerId", required=True, help="worker unique id")
+@click.option("--sessionId", required=True, help="session id")
+def run(server, workerid, sessionid, timeout):
+    master_data = {"server": server, "workerId": workerid, "sessionId": sessionid, "timeout": timeout}
+    task_proc = Process(target=task_runner, args=(master_data,))
+    publish_proc = Process(target=publish_runner, args=(master_data,))
+    sync_proc = Process(target=sync_runner, args=(master_data,))
+    task_proc.daemon = True
+    publish_proc.daemon = True
+    sync_proc.daemon = True
+    task_proc.start()
+    publish_proc.start()
+    sync_proc.start()
+    sync_proc.join()
+    publish_proc.join()
+    publish_proc.join()
+
+cli.add_command(register)
+cli.add_command(run)
+
+if __name__ == '__main__':
+    cli()
