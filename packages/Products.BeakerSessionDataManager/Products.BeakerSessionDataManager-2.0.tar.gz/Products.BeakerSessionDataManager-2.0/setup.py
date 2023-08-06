bdba518c2471cc1f25205f20a89df5aee21d0d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:43 +0000

    Add package Products.BeakerSessionDataManager/2.0/Products.BeakerSessionDataManager-2.0.tar.gz

---
+++ packages/Products.BeakerSessionDataManager/Products.BeakerSessionDataManager-2.0.tar.gz/Products.BeakerSessionDataManager-2.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+version = "2.0"
+
+requires = [
+    "setuptools",
+    "collective.beaker",
+    "Zope2",
+    "zope.interface",
+    "unittest2",
+]
+
+setup(
+    name="Products.BeakerSessionDataManager",
+    version=version,
+    description="Zope2 session implementation using Beaker",
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: Zope :: 4",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="zope sessions beaker",
+    author="David Glick, Groundwire",
+    author_email="davidglick@groundwire.org",
+    url="http://github.com/davisagli/Products.BeakerSessionDataManager",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["Products"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require={
+        "test": [
+            "Products.Sessions",
+        ],
+    },
+)
