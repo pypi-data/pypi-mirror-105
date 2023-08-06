c3f31c7427c3b112f619d39a0378bb0eeaa91694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:07 +0000

    Add package tools-09d3/0.0.3/tools_09d3-0.0.3-py3-none-any.whl

---
+++ packages/tools-09d3/tools_09d3-0.0.3-py3-none-any.whl/tools_09d3/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------------------------
+# documentation
+
+r"""
+tools_09d3
+==========
+
+Problem
+-------
+
+How to re-use often used structures ?
+
+Solution
+-------- 
+
+Accumulate them in a package.
+"""
+
+# --------------------------------------------------------------------------------
+# interface
+
+from .impl import *
