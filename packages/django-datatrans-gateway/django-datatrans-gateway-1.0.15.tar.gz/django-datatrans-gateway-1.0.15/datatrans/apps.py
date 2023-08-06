c540ad715fc3df000f38fc05a24c12ccd02ce2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:18 +0000

    Add package django-datatrans-gateway/1.0.15/django-datatrans-gateway-1.0.15.tar.gz

---
+++ packages/django-datatrans-gateway/django-datatrans-gateway-1.0.15.tar.gz/django-datatrans-gateway-1.0.15/datatrans/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DatatransConfig(AppConfig):
+    name = 'datatrans'
+    verbose_name = 'Datatrans'
+    default_auto_field = 'django.db.models.AutoField'
