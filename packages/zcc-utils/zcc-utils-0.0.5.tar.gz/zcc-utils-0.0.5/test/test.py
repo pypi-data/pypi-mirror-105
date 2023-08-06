4a3a856ef1dd2b6da982b4fe2fb7181b481b106d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:01 +0000

    Add package zcc-utils/0.0.5/zcc-utils-0.0.5.tar.gz

---
+++ packages/zcc-utils/zcc-utils-0.0.5.tar.gz/zcc-utils-0.0.5/test/test.py
@@ -0,0 +1,25 @@
+import requests
+from typing import List
+from src.zcc_utils import json_tools
+
+
+class HQ2:
+    def __init__(self):
+        self.Today:List[HQ2Item]=[]
+class HQ2Item:
+    def __init__(self):
+        self.E7=None
+        self.E8=None
+        self.E9=None
+        self.E10=None
+        self.E11=None
+        self.E12=None
+        self.E13=None
+        self.V1=None
+
+response = requests.get("https://www.2hq.com.7xq.com/data/GamesDataArray.json")
+# print(response.text)
+# obj = json.loads(response.text)
+# res:HQ2 = dict_to_object(obj)
+res:HQ2 = json_tools.json_to_object(response.text)
+print(res.Today[0].E7)
