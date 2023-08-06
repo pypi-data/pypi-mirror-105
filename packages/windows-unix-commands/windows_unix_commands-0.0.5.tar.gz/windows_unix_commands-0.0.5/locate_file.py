b6e516136e9d45d1c28696a8b7bed64b6581431c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:59 +0000

    Add package windows-unix-commands/0.0.5/windows_unix_commands-0.0.5.tar.gz

---
+++ packages/windows-unix-commands/windows_unix_commands-0.0.5.tar.gz/windows_unix_commands-0.0.5/locate_file.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from pathlib import Path
+import time
+
+def main(args=None):
+    """The main routine."""
+    file_name = None
+    if args is None:
+        try:
+            file_name = sys.argv[1]
+        except Exception:
+            print("please specify a file name")
+            return 
+    import time
+
+    t_end = time.monotonic() + 100
+    files_found = False
+    for root, dirs, files in os.walk("C:\\"):
+        if(files_found and time.monotonic() > t_end):
+            return 
+        for curr_file in files:
+            if curr_file ==  file_name:
+                print(str(Path(root) / curr_file))
+                files_found= True
+                break
+
+
+    # Do argument parsing here (eg. with argparse) and anything else
+    # you want your project to do. Return values are exit codes.
+
+
+# if __name__ == "__main__":
+#     sys.exit(main())
---
+++ packages/windows-unix-commands/windows_unix_commands-0.0.5.tar.gz/windows_unix_commands-0.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "windows_unix_commands",
+    version = "0.0.5",
+    description = "hey",
+    py_modules=["ls", "locate_file"], 
+    entry_points = {
+        'console_scripts': [
+            'ls = ls:find_files',
+            'locate = locate_file:main'
+        ],
+    }
+)
