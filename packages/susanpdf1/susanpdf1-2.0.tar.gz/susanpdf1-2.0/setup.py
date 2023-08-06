1dd91438951cb850bbf29d7df1c291de21bd4bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:23 +0000

    Add package susanpdf1/2.0/susanpdf1-2.0.tar.gz

---
+++ packages/susanpdf1/susanpdf1-2.0.tar.gz/susanpdf1-2.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="susanpdf1",
+    version=2.0,
+    long_decription=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
