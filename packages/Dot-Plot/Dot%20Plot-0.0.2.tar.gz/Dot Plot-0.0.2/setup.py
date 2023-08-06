a64f758d99c3e7a7a1a0aa971fe319d14855057d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:34 +0000

    Add package Dot-Plot/0.0.2/Dot%20Plot-0.0.2.tar.gz

---
+++ packages/Dot-Plot/Dot%20Plot-0.0.2.tar.gz/Dot Plot-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Dot Plot',
+      version='0.0.2',
+      description="Nico's dot plotter",
+      url='',
+      author='N to the Icky',
+      author_email='flyingcircus@example.com',
+      license='',
+      packages=['library'],
+      zip_safe=False)
