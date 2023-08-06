6336d72ccfa4852f4ca1760f486d0e752050b88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package toggl-report/1.0.0/toggl_report-1.0.0-py3-none-any.whl

---
+++ packages/toggl-report/toggl_report-1.0.0-py3-none-any.whl/test/snapshots/snap_gulp_pdf_test.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestGulpPdf::test_match_snapshot 1'] = '''%PDF-1.3
+3 0 obj
+<</Type /Page
+/Parent 1 0 R
+/Resources 2 0 R
+/Contents 4 0 R>>
+endobj
+4 0 obj
+<</Filter /FlateDecode /Length 681>>
+stream
+x\x9c\x85\x95KoÓ@\x10Çïý\x14s\x84C\x97}?Ê\x89\x8aR\x04DTj\x90¸:í6M\x1b»\x92\x1f\x14øô\x8cc¯í]\x13G²\x12\x8d½óûÏìÌÎrørF\x892ðzv¹\x86w\x9f\x180I(\x85õ\x03\\\xadÛW\x82\x11æÀXG\x14¾½\x877ßü®ª\x9bb[\x15ÙÝã+\x1aoaýÔ/nýíÿÜµ"R\x1dÜW/EV_\x04\x1fN5q\x02¿3b\x15ä ¸"Î\x05{\x0f·\x80\x0cN\x1d1&\x82d\x7fð-s\x13é^H\x89°æCóP\x97Ùvë7¾\x84¯MqïgªÒÅª\x9d\x9d¨\x8eÄUSÕÈºÎ7\x9fçÂ\x92\x85e\x97\x1e\x97í÷E\x93ç¾\x9ci
+\x13kvv¢9Â\x18\x17Ré¹\x1cwaÅP\x0f_nÊ]±õe*\x89J\x91dg\'\x92#p\x95ý\x86.Õ<+\x8a¹43aåMùòä\x9fë_¾lwº:\x921\x13±|g\'ò\tô®\x86\x9fQ_ñi_qK\x84\x02í0\x1e\x0bÒ\x10Íá\x9cñö{é;l\x17ª¶\x9c\x18{\xa0~Ìê&\x0fD\x83\x81\x98Á\x9f\x11mR\x7fc\x89eSÿÛ:+ëàÏ0Z-\x17\x01\x8cS"í\x94p\x85\x1d8\x00\x94#\x82\x05\x80PÄÌ2`\x9a\x13¦¦\x80\x9b¬©F\x82S\x84\x8a\x81 \x0e¼\x14á°·ô\x141ôÊÆWw\x8f¥ßmÐ\x08H%$\x11r1(\x85ÝÊÌ\x94xí«,\x1f6¦/\x8cq§
+ch\x00´Çø\x9c2|\x92êô\x90\x85ê\x8c\x10ê.(Å\'-Ð\x02#\x14h\x840\x91@ú"u\x90Å"M"a\t¤¯S\x0fY¬ÓHYgÕ3°÷pøçi\x81\x16\x02
+\x05\x9a\x04\x94d\x85\x8b$\x1eDÜ\x10\xadìÐÝíÉ\x85Ûº\x9d\x92\x05à/¬vESû"\x95Öæ¤ôH\x15±¬À\xad°X{©OÊÂ½ÿ»Ë³}*¯ÔIù\x91.\x08\x9d\r/\x8d\xa0ä,@;7ñ*\x9b\\J]\x17ÓC®9\x84fêìý´\x8f\x95\x1b\x92ý>\x1f\x0f\x01Ð\x0fÂ\x18À,^»rJ\x88F\x14w\x9ap12\x84UDñ\x84Á\x9d#T\x1f\x89"öÈñ8ÊÃ©\x89\x08RZ"ùñ(\x0e;¡\x0c\x8b\x12éíx\'ô0\x12~\x14xu´ãe÷PÃÂíÔe8ÀûøbxÈð\x08}zÇ\x07î?¡?\x13£
+endstream
+endobj
+1 0 obj
+<</Type /Pages
+/Kids [3 0 R ]
+/Count 1
+/MediaBox [0 0 595.28 841.89]
+>>
+endobj
+5 0 obj
+<</Type /Font
+/BaseFont /Helvetica-Bold
+/Subtype /Type1
+/Encoding /WinAnsiEncoding
+>>
+endobj
+6 0 obj
+<</Type /Font
+/BaseFont /Helvetica
+/Subtype /Type1
+/Encoding /WinAnsiEncoding
+>>
+endobj
+2 0 obj
+<<
+/ProcSet [/PDF /Text /ImageB /ImageC /ImageI]
+/Font <<
+/F1 5 0 R
+/F2 6 0 R
+>>
+/XObject <<
+>>
+>>
+endobj
+7 0 obj
+<<
+/Producer (PyFPDF 1.7.2 http://pyfpdf.googlecode.com/)
+/CreationDate (D:20190101000000)
+>>
+endobj
+8 0 obj
+<<
+/Type /Catalog
+/Pages 1 0 R
+/OpenAction [3 0 R /FitH null]
+/PageLayout /OneColumn
+>>
+endobj
+xref
+0 9
+0000000000 65535 f 
+0000000838 00000 n 
+0000001122 00000 n 
+0000000009 00000 n 
+0000000087 00000 n 
+0000000925 00000 n 
+0000001026 00000 n 
+0000001236 00000 n 
+0000001345 00000 n 
+trailer
+<<
+/Size 9
+/Root 8 0 R
+/Info 7 0 R
+>>
+startxref
+1448
+%%EOF
+'''
