485c8b6cfc1b3954f1887ceb20e276c42d19e555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:15 +0000

    Add package tempowork/0.0.16/tempowork-0.0.16.tar.gz

---
+++ packages/tempowork/tempowork-0.0.16.tar.gz/tempowork-0.0.16/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "tempowork",
+    version = "0.0.16",
+    author = "Ali Jazayeri",
+    author_email = "ali.jazayeri@drexel.edu",
+    description = ("A library for mining temporal networks where time is represented as a continuous dimension!"),
+    license = "MIT",
+    keywords = "subgraph mining; temporal networks",
+    url = "https://github.com/alijazayeri/tempowork",
+    py_modules=['tempowork'],
+    package_dir={'':'tempowork'},
+    long_description=read('README.rst'),
+long_description_content_type='text/x-rst'
+)
