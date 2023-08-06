e834d45104c4cb279598a02354b5d3d98559c9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:24 +0000

    Add package mypy-boto3-mobile/1.17.71/mypy_boto3_mobile-1.17.71-py3-none-any.whl

---
+++ packages/mypy-boto3-mobile/mypy_boto3_mobile-1.17.71-py3-none-any.whl/mypy_boto3_mobile/literals.py
@@ -0,0 +1,28 @@
+"""
+Type annotations for mobile service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_mobile.literals import ListBundlesPaginatorName
+
+    data: ListBundlesPaginatorName = "list_bundles"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("ListBundlesPaginatorName", "ListProjectsPaginatorName", "Platform", "ProjectState")
+
+
+ListBundlesPaginatorName = Literal["list_bundles"]
+ListProjectsPaginatorName = Literal["list_projects"]
+Platform = Literal["ANDROID", "JAVASCRIPT", "LINUX", "OBJC", "OSX", "SWIFT", "WINDOWS"]
+ProjectState = Literal["IMPORTING", "NORMAL", "SYNCING"]
