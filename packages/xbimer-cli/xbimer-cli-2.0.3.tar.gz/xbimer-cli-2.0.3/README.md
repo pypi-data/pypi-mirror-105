a1a647ad7cd764bf4f72f42a95a6814bef9fe207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:11 +0000

    Add package xbimer-cli/2.0.3/xbimer-cli-2.0.3.tar.gz

---
+++ packages/xbimer-cli/xbimer-cli-2.0.3.tar.gz/xbimer-cli-2.0.3/README.md
@@ -0,0 +1,6 @@
+<h1 align="center" >xBIMer CLI</h1>
+<p align="center">Command line tools for xBIMer</p>
+
+<p align="center">
+<a href="https://github.com/xbimer/xbimer-docs">Read Documentation</a>
+</p>
---
+++ packages/xbimer-cli/xbimer-cli-2.0.3.tar.gz/xbimer-cli-2.0.3/xbimer_cli/assets/module/src/__entry__.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+#include "xir-cast.h"
+#include "xir-native.h"
+
+#include "Python.h"
+#include "pybind11.h"
+
+
+// https://pybind11.readthedocs.io/en/stable/
+using namespace pybind11;
+
+#define PROJECT_VERSION "1.0.0"
+
+
+
+PYBIND11_MODULE(__entry__, m) {
+	m.add_object("version", str(PROJECT_VERSION));//Please do not delete the module version number
+}
