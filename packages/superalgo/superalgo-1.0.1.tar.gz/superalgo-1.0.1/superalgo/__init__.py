8f299536a5554babe178c5e063d506af874a0fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:47 +0000

    Add package superalgo/1.0.1/superalgo-1.0.1.tar.gz

---
+++ packages/superalgo/superalgo-1.0.1.tar.gz/superalgo-1.0.1/superalgo/__init__.py
@@ -0,0 +1,7 @@
+
+__all__ = [
+    "algorithms3x"
+]
+from algorithms3x.search import binary_search, linear_search
+from algorithms3x.sort import bubble_sort, selection_sort, merge_sort
+__version__ = "1.0.0"
