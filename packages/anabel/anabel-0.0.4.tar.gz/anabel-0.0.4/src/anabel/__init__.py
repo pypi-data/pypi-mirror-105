ba25a26dcb40b17b4ceeaae543951aa367f9ec78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:35 +0000

    Add package anabel/0.0.4/anabel-0.0.4.tar.gz

---
+++ packages/anabel/anabel-0.0.4.tar.gz/anabel-0.0.4/src/anabel/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "0.0.4"
+
+
+import anabel.aisc
+import anabel.analysis
+import anabel.sections
+
+from anabel.assemble import *
+from anabel.matrices import *
+
+from anabel.graphics import *
+#from anabel.solvers import *
+
+#import anabel.utilities
+#import anabel.matlib
+
+#__all__ = anabel.assemblers.__all__ + ["elements", "assemblers", "graphics"]
+
+settings = {
+    "DATAFRAME_LATEX": True,
+}
