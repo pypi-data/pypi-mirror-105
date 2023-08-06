3f2f471294400e366142cd63e331a03da6099e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:04 +0000

    Add package STree/1.1/STree-1.1.tar.gz

---
+++ packages/STree/STree-1.1.tar.gz/STree-1.1/stree/__init__.py
@@ -0,0 +1,10 @@
+from .Strees import Stree, Snode, Siterator, Splitter
+
+__version__ = "1.1"
+
+__author__ = "Ricardo Montañana Gómez"
+__copyright__ = "Copyright 2020-2021, Ricardo Montañana Gómez"
+__license__ = "MIT License"
+__author_email__ = "ricardo.montanana@alu.uclm.es"
+
+__all__ = ["Stree", "Snode", "Siterator", "Splitter"]
