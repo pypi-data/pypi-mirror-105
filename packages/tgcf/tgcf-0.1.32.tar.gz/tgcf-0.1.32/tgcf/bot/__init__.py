03c06e0e0485dbdf1c1b9054b9ea757e2ee153e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:11 +0000

    Add package tgcf/0.1.32/tgcf-0.1.32.tar.gz

---
+++ packages/tgcf/tgcf-0.1.32.tar.gz/tgcf-0.1.32/tgcf/bot/__init__.py
@@ -0,0 +1,8 @@
+"""The subpackage for interative bot for tgcf."""
+
+from .live_bot import (
+    forward_command_handler,
+    help_command_handler,
+    remove_command_handler,
+    start_command_handler,
+)
