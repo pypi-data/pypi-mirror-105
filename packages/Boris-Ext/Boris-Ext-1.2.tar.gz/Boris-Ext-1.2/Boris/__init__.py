73f89d02929f724f5e8396f07f1eeef137b4d743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:28 +0000

    Add package Boris-Ext/1.2/Boris-Ext-1.2.tar.gz

---
+++ packages/Boris-Ext/Boris-Ext-1.2.tar.gz/Boris-Ext-1.2/Boris/__init__.py
@@ -0,0 +1,10 @@
+
+"""
+Boris Package extensions. Inlcluding NSClient, Shapes, Elementary Shapes.
+Elementary Shapes can now be accessed through .Shaper
+
+E.g. For NSClient. From Boris.Netsocks import NSClient
+"""
+
+__version__ = "1.2"
+__author__ = 'Serban Lepadatu'
