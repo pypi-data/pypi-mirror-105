3b5a62999dd89379305b3133b4655c11945829da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:09 +0000

    Add package metapredict/1.1/metapredict-1.1.tar.gz

---
+++ packages/metapredict/metapredict-1.1.tar.gz/metapredict-1.1/metapredict/__init__.py
@@ -0,0 +1,24 @@
+"""
+metapredict
+A protein disorder predictor based on a BRNN (IDP-Parrot) trained on the consensus disorder values from 8 disorder predictors from 12 proteomes.
+"""
+
+# Add imports here
+from .meta import *
+from .backend.brnn_architecture import *
+from .backend.domain_definition import *
+from .backend.encode_sequence import *
+from .backend.meta_graph import *
+from .backend.meta_predict_disorder import *
+from .backend.uniprot_predictions import *
+
+
+
+
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
