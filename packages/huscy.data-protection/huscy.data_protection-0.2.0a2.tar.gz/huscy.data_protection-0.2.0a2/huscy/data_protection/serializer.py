ae7fe197b7aa062429071dbd3282282d190c38fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:22 +0000

    Add package huscy.data-protection/0.2.0a2/huscy.data_protection-0.2.0a2.tar.gz

---
+++ packages/huscy.data-protection/huscy.data_protection-0.2.0a2.tar.gz/huscy.data_protection-0.2.0a2/huscy/data_protection/serializer.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from huscy.attributes.services import get_or_create_attribute_set
+from huscy.data_protection.models import DataAccessRequest, DataRevocationRequest
+from huscy.subjects.serializers import SubjectSerializer
+
+
+class DataAccessRequestSerializer(serializers.ModelSerializer):
+    creator = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    creator_name = serializers.CharField(source='creator.get_full_name', read_only=True)
+
+    class Meta:
+        model = DataAccessRequest
+        fields = 'created_at', 'creator', 'creator_name', 'id', 'subject'
+        read_only_fields = 'created_at',
+
+
+class DataAccessSerializer(serializers.Serializer):
+    attributes = serializers.SerializerMethodField()
+    subject = serializers.SerializerMethodField()
+
+    def get_attributes(self, subject):
+        attribute_set = get_or_create_attribute_set(subject)
+        return attribute_set.attributes
+
+    def get_subject(self, subject):
+        return SubjectSerializer(subject).data
+
+
+class DataRevocationRequestSerializer(serializers.ModelSerializer):
+    creator = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    creator_name = serializers.CharField(source='creator.get_full_name', read_only=True)
+
+    class Meta:
+        model = DataRevocationRequest
+        fields = 'created_at', 'creator', 'creator_name', 'id', 'subject', 'type'
+        read_only_fields = 'created_at',
