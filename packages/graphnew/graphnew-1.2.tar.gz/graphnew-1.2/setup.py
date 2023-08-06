f068f7b55532be5d5fbe6bdb22a7b9ecdc2c2eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:07 +0000

    Add package graphnew/1.2/graphnew-1.2.tar.gz

---
+++ packages/graphnew/graphnew-1.2.tar.gz/graphnew-1.2/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='graphnew',
+    version='1.2',
+    py_modules=['graphnew'],
+    requires = ["tkinter"],
+    description = "graphnew",
+    author_email = "graphnew@mail.ru",
+    long_description = """
+ПЕРЕЗАЛИВ ОТ ПОЛЯКОВА
+"""
+    )
