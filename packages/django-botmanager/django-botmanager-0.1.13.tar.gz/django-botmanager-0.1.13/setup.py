13cf9574bb23ae213a453b5ac508a5e470443cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:52 +0000

    Add package django-botmanager/0.1.13/django-botmanager-0.1.13.tar.gz

---
+++ packages/django-botmanager/django-botmanager-0.1.13.tar.gz/django-botmanager-0.1.13/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='django-botmanager',
+    version='0.1.13',
+    description='Async tasks for django',
+    author='Dimoha',
+    author_email='dimoha@controlstyle.ru',
+    url='https://github.com/dimoha/django-botmanager',
+    install_requires=[
+        "setproctitle==1.1.10",
+        "jsonfield==1.0.3"
+        "psutil==5.8.0"
+    ],
+    packages=find_packages(),
+)
