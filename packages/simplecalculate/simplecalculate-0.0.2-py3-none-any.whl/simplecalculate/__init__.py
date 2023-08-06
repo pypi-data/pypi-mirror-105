f15217b23399f0088c12c3edfbb5e650d0af6d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:42 +0000

    Add package simplecalculate/0.0.2/simplecalculate-0.0.2-py3-none-any.whl

---
+++ packages/simplecalculate/simplecalculate-0.0.2-py3-none-any.whl/simplecalculate/__init__.py
@@ -0,0 +1,256 @@
+''' 
+CREATED BY : --1Moretime--GiantCoder
+***---------------------------------------
+""" BASIC CALCULATIONS """
+__________________________________________
+It is Only Contain Functions,Not Any Class
+___________________________________________
+___________________________________________
+
+******************************** Addition function types ********************************
+add() ===>>>add()
+            add3()
+            add4()
+            add5()
+            add6(), 
+            
+******************************** Subtraction function types ********************************           
+subtract() ===>>>subtract()
+                 subtract3()
+                 subtract4()
+                 subtract5()
+                 subtract6(),
+                 
+******************************** Multiplication function types ********************************                 
+multiplication() ===>>>multiplication()
+                       multiplication3() 
+                       multiplication4()
+                       multiplication5()
+                       multiplication6(), 
+
+******************************** Division function types ********************************
+division() ===>>>division()
+                division3()
+                division4()
+                division5()
+                division6(),
+
+******************************** Modulas function types ********************************                
+modulas() ===>>>modulas()
+                modulas3()
+                modulas4()
+                modulas5()
+                modulas6(),
+
+******************************** Exponent function types ********************************                
+exponent() ===>>>exponent()
+                exponent3()
+                exponent4()
+                exponent5()
+                exponent6(),
+                
+******************************** Floor Division function types ********************************                
+floordivision() ===>>>floordivision()
+                      floordivision3()
+                      floordivision4()
+                      floordivision5()
+                      floordivision6()
+
+'''
+
+
+##--------------------- ADDITION STARTS:::
+#-----Addition Only 2 Values Add------------------------#1
+def add(a,b):
+    return a + b
+
+#-----Addition Only 3 Values Add------------------------#1
+def add3(a,b,c):
+    return a + b + c
+
+#-----Addition Only 4 Values Add------------------------#1
+def add4(a,b,c,d):
+    return a + b + c + d
+
+#-----Addition Only 5 Values Add------------------------#1
+def add5(a,b,c,d,e):
+    return a + b + c + d + e
+
+#-----Addition Only 6 Values Add------------------------#1
+def add6(a,b,c,d,e,f):
+    return a + b + c + d + e + f
+
+##---------------------- ADDITION ENDS:::
+
+
+
+
+##--------------------- SUBTRACTION STARTS:::
+
+#-----Subtract Only 2 Values Sub------------------------#2
+def subtract(a,b):
+    return a - b
+
+#-----Subtract Only 3 Values Sub------------------------#2
+def subtract3(a,b,c):
+    return a - b - c
+
+#-----Subtract Only 4 Values Sub------------------------#2
+def subtract4(a,b,c,d):
+    return a - b - c - d
+
+#-----Subtract Only 5 Values Sub------------------------#2
+def subtract5(a,b,c,d,e):
+    return a - b - c - d - e
+
+#-----Subtract Only 6 Values Sub------------------------#2
+def subtract6(a,b,c,d,e,f):
+    return a - b - c - d - f
+
+##--------------------- SUBTRACTION ENDS:::
+
+
+
+
+##--------------------- MULTIPLICATION STARTS:::
+
+#-----Multiplication Only 2 Values Multiply------------------#3
+def multiplication(a,b):
+    return a * b
+
+#-----Multiplication Only 3 Values Multiply------------------#3
+def multiplication3(a,b,c):
+    return a * b * c
+
+#-----Multiplication Only 4 Values Multiply------------------#3
+def multiplication4(a,b,c,d):
+    return a * b * c * d
+
+#-----Multiplication Only 5 Values Multiply------------------#3
+def multiplication5(a,b,c,d,e):
+    return a * b * c * d * e
+
+#-----Multiplication Only 6 Values Multiply------------------#3
+def multiplication6(a,b,c,d,e,f):
+    return a * b * c * d * e * f 
+
+##--------------------- MULTIPLICATION ENDS:::
+
+
+
+
+##--------------------- DIVISION STARTS:::
+
+#-----Division Only 2 Values Divide------------------------#4
+def division(a,b):
+    return a / b
+
+#-----Division Only 3 Values Divide------------------------#4
+def division3(a,b,c):
+    return a / b / c
+
+#-----Division Only 4 Values Divide------------------------#4
+def division4(a,b,c,d):
+    return a / b / c / d
+
+#-----Division Only 5 Values Divide------------------------#4
+def division5(a,b,c,d,e):
+    return a / b / c / d / e
+
+#-----Division Only 6 Values Divide------------------------#4
+def division6(a,b,c,d,e,f):
+    return a / b / c / d / e / f
+
+##--------------------- DIVISION ENDS:::
+
+
+
+
+##--------------------- MODULAS STARTS:::
+
+#-----Modulas Only 2 Values Modu-------------------------#5
+def modulas(a,b):
+    return a % b
+
+#-----Modulas Only 3 Values Modu-------------------------#5
+def modulas3(a,b,c):
+    return a % b % c
+
+#-----Modulas Only 4 Values Modu-------------------------#5
+def modulas4(a,b,c,d):
+    return a % b % c % d
+
+#-----Modulas Only 5 Values Modu-------------------------#5
+def modulas5(a,b,c,d,e):
+    return a % b % c % d % e
+
+#-----Modulas Only 6 Values Modu-------------------------#5
+def modulas6(a,b,c,d,e,f):
+    return a % b % c % d % e % f
+
+##--------------------- MODULAS ENDS:::
+
+
+
+
+##--------------------- EXPONENT STARTS:::
+
+#-----Exponent Only 2 Values Exponent------------------------#6
+def exponent(a,b):
+    return a ** b
+
+#-----Exponent Only 3 Values Exponent------------------------#6
+def exponent3(a,b,c):
+    return a ** b ** c
+
+#-----Exponent Only 4 Values Exponent------------------------#6
+def exponent4(a,b,c,d):
+    return a ** b ** c ** d
+
+#-----Exponent Only 5 Values Exponent------------------------#6
+def exponent5(a,b,c,d,e):
+    return a ** b ** c ** d ** e
+
+#-----Exponent Only 6 Values Exponent------------------------#6
+def exponent6(a,b,c,d,e,f):
+    return a ** b ** c ** d ** e ** f
+
+##--------------------- EXPONENT ENDS:::
+
+
+
+
+##--------------------- FLOOR DIVISION STARTS:::
+
+#-----FloorDivision Only 2 Values FloorDivision-------------------#7
+def floordivision(a,b):
+    return a // b
+
+#-----FloorDivision Only 3 Values FloorDivision-------------------#7
+def floordivision3(a,b,c):
+    return a // b // c
+
+#-----FloorDivision Only 4 Values FloorDivision-------------------#7
+def floordivision4(a,b,c,d):
+    return a // b // c // d
+
+#-----FloorDivision Only 5 Values FloorDivision-------------------#7
+def floordivision5(a,b,c,d,e):
+    return a // b // c // d // e
+
+#-----FloorDivision Only 6 Values FloorDivision-------------------#7
+def floordivision6(a,b,c,d,e,f):
+    return a // b // c // d // e // f
+
+##--------------------- FLOOR DIVISION ENDS:::
+
+'''
+CREATED BY : --1Moretime--GiantCode
+***---------------------------------------
+""" BASIC CALCULATIONS """
+__________________________________________
+It is Only Contain Functions,Not Any Class
+add(), subtract(), multiplication(), 
+division(), modulas(), exponent(), floordivision().
+
+'''
