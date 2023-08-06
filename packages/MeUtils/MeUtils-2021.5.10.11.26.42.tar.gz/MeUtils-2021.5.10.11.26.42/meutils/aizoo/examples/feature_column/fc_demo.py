b7d0312f22a046cbff4c3460afae9cb7d0a738cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:20 +0000

    Add package MeUtils/2021.5.10.11.26.42/MeUtils-2021.5.10.11.26.42.tar.gz

---
+++ packages/MeUtils/MeUtils-2021.5.10.11.26.42.tar.gz/MeUtils-2021.5.10.11.26.42/meutils/aizoo/examples/feature_column/fc_demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : fc_demo
+# @Time         : 2021/4/29 6:24 下午
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+
