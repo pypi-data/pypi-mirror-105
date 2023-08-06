8726f5394c8134dd5ca266c9d600ba63b504293b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:19 +0000

    Add package mypy-boto3-lex-models/1.17.71/mypy_boto3_lex_models-1.17.71-py3-none-any.whl

---
+++ packages/mypy-boto3-lex-models/mypy_boto3_lex_models-1.17.71-py3-none-any.whl/mypy_boto3_lex_models/literals.py
@@ -0,0 +1,93 @@
+"""
+Type annotations for lex-models service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_lex_models.literals import ChannelStatus
+
+    data: ChannelStatus = "CREATED"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "ChannelStatus",
+    "ChannelType",
+    "ContentType",
+    "Destination",
+    "ExportStatus",
+    "ExportType",
+    "FulfillmentActivityType",
+    "GetBotAliasesPaginatorName",
+    "GetBotChannelAssociationsPaginatorName",
+    "GetBotVersionsPaginatorName",
+    "GetBotsPaginatorName",
+    "GetBuiltinIntentsPaginatorName",
+    "GetBuiltinSlotTypesPaginatorName",
+    "GetIntentVersionsPaginatorName",
+    "GetIntentsPaginatorName",
+    "GetSlotTypeVersionsPaginatorName",
+    "GetSlotTypesPaginatorName",
+    "ImportStatus",
+    "Locale",
+    "LogType",
+    "MergeStrategy",
+    "ObfuscationSetting",
+    "ProcessBehavior",
+    "ResourceType",
+    "SlotConstraint",
+    "SlotValueSelectionStrategy",
+    "Status",
+    "StatusType",
+)
+
+
+ChannelStatus = Literal["CREATED", "FAILED", "IN_PROGRESS"]
+ChannelType = Literal["Facebook", "Kik", "Slack", "Twilio-Sms"]
+ContentType = Literal["CustomPayload", "PlainText", "SSML"]
+Destination = Literal["CLOUDWATCH_LOGS", "S3"]
+ExportStatus = Literal["FAILED", "IN_PROGRESS", "READY"]
+ExportType = Literal["ALEXA_SKILLS_KIT", "LEX"]
+FulfillmentActivityType = Literal["CodeHook", "ReturnIntent"]
+GetBotAliasesPaginatorName = Literal["get_bot_aliases"]
+GetBotChannelAssociationsPaginatorName = Literal["get_bot_channel_associations"]
+GetBotVersionsPaginatorName = Literal["get_bot_versions"]
+GetBotsPaginatorName = Literal["get_bots"]
+GetBuiltinIntentsPaginatorName = Literal["get_builtin_intents"]
+GetBuiltinSlotTypesPaginatorName = Literal["get_builtin_slot_types"]
+GetIntentVersionsPaginatorName = Literal["get_intent_versions"]
+GetIntentsPaginatorName = Literal["get_intents"]
+GetSlotTypeVersionsPaginatorName = Literal["get_slot_type_versions"]
+GetSlotTypesPaginatorName = Literal["get_slot_types"]
+ImportStatus = Literal["COMPLETE", "FAILED", "IN_PROGRESS"]
+Locale = Literal[
+    "de-DE",
+    "en-AU",
+    "en-GB",
+    "en-US",
+    "es-419",
+    "es-ES",
+    "es-US",
+    "fr-CA",
+    "fr-FR",
+    "it-IT",
+    "ja-JP",
+]
+LogType = Literal["AUDIO", "TEXT"]
+MergeStrategy = Literal["FAIL_ON_CONFLICT", "OVERWRITE_LATEST"]
+ObfuscationSetting = Literal["DEFAULT_OBFUSCATION", "NONE"]
+ProcessBehavior = Literal["BUILD", "SAVE"]
+ResourceType = Literal["BOT", "INTENT", "SLOT_TYPE"]
+SlotConstraint = Literal["Optional", "Required"]
+SlotValueSelectionStrategy = Literal["ORIGINAL_VALUE", "TOP_RESOLUTION"]
+Status = Literal["BUILDING", "FAILED", "NOT_BUILT", "READY", "READY_BASIC_TESTING"]
+StatusType = Literal["Detected", "Missed"]
