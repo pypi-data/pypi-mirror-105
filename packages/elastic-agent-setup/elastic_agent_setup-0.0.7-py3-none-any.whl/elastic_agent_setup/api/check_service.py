c5397f4d9b56a060eddd5f145431c8a46c1a29d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:42 +0000

    Add package elastic-agent-setup/0.0.7/elastic_agent_setup-0.0.7-py3-none-any.whl

---
+++ packages/elastic-agent-setup/elastic_agent_setup-0.0.7-py3-none-any.whl/elastic_agent_setup/api/check_service.py
@@ -0,0 +1,17 @@
+from ..core import Core
+
+
+class CheckService(Core):
+
+    method = 'GET'
+    endpoint = None
+
+    def __init__(self, service):
+        super().__init__()
+        if service == 'elasticsearch':
+            self.endpoint = '/_cluster/health?wait_for_status=yellow'
+        elif service == 'kibana':
+            self.endpoint = '/api/status'
+
+    def parse_response(self, response):
+        return response.json()
