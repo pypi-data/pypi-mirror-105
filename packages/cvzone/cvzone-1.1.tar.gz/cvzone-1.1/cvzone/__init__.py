14499b26d7c6060cf3a221eea5d2c3564b05dc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:29 +0000

    Add package cvzone/1.1/cvzone-1.1.tar.gz

---
+++ packages/cvzone/cvzone-1.1.tar.gz/cvzone-1.1/cvzone/__init__.py
@@ -0,0 +1,7 @@
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.FaceDetectionModule import FaceDetector
+from cvzone.PoseModule import PoseDetector
+from cvzone.Utils import stackImages, cornerRect
+from cvzone.FPS import FPS
+from cvzone.SerialModule import SerialObject
