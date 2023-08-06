4d7f7215800726ca6ed19dc95490c0a644e522cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:14 +0000

    Add package matching-network/0.1.1/matching_network-0.1.1.tar.gz

---
+++ packages/matching-network/matching_network-0.1.1.tar.gz/matching_network-0.1.1/matching_network/__main__.py
@@ -0,0 +1,36 @@
+# import sys
+# import click
+
+# @click.group()
+# @click.version_option("0.1.1")
+# def main():
+#     """A Lumped Parameters LC matching networks CLI solver.
+
+# urbanij.github.io/projects/matching_networks/
+
+# urbanij.github.io/syRF
+# """
+#     print("Hye")
+#     pass
+
+
+import click
+from notmain import L_section_matching
+
+
+
+@click.command()
+# @click.argument('keyword', required=False)
+@click.option('--from', '-f', 'from_', required=True, type=complex)
+@click.option('--to', '-t', 'to', required=True, type=complex)
+@click.option('--frequency', '--freq', 'frequency', required=False, type=float)
+def match_(from_, to, frequency):
+    print(
+        L_section_matching(
+            input_impedance=from_, 
+            output_impedance=to, 
+            frequency=frequency
+        ).match()
+    )
+
+match_()
