0db5f7ded43fcc4828ab92a8b1c927a3dbd17be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:26 +0000

    Add package combo-lock/0.0.1/combo_lock-0.0.1.tar.gz

---
+++ packages/combo-lock/combo_lock-0.0.1.tar.gz/combo_lock-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='combo_lock',
+    version='0.0.1',
+    packages=['combo_lock'],
+    url='',
+    license='apache-2.0',
+    author='jarbasai',
+    install_requires=["fasteners"],
+    author_email='jarbasai@mailfence.com',
+    description=''
+)
