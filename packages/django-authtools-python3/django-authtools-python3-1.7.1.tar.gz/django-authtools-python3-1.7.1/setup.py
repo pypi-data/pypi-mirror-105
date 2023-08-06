34efe2887bedd397a7d4afb489955a032a1439d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:52 +0000

    Add package django-authtools-python3/1.7.1/django-authtools-python3-1.7.1.tar.gz

---
+++ packages/django-authtools-python3/django-authtools-python3-1.7.1.tar.gz/django-authtools-python3-1.7.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import io
+import os
+
+from setuptools import setup, find_packages
+
+__doc__ = ("Custom user model app for Django featuring email as username and"
+           " class-based views for authentication.")
+
+
+def read(fname):
+    return io.open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8").read()
+
+
+install_requires = [
+    'Django>=1.11',
+]
+setup(
+    # Patch from 1.7.0 and update to resolve some python3 syntax
+    name='django-authtools-python3',
+    packages=["authtools", "authtools.migrations"],
+    version='1.7.1',
+    author='Fusionbox, Inc.',
+    author_email='programmers@fusionbox.com',
+    description=__doc__,
+    long_description='\n\n'.join([read('README.rst'), read('CHANGES.rst')]),
+    url='https://github.com/nguyencg/django-authtools',
+    license='BSD',
+    install_requires=install_requires,
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
