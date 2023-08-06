d0b1a152f57bd3993bf89bfce336389886014695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:08 +0000

    Add package duco/0.1.2/duco-0.1.2.tar.gz

---
+++ packages/duco/duco-0.1.2.tar.gz/duco-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='duco',  
+    version='0.1.2',
+    author="Dan Sinclair",
+    author_email="dansinclair@me.com",
+    description="A python package for the Duino Coin REST API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dansinclair25/duco-py",
+    packages=['duco'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
