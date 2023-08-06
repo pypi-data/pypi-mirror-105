97234b75f62a6b392b01c2ec98b77d443cc6e2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:50 +0000

    Add package pytransliterate/0.1.0/pytransliterate-0.1.0.tar.gz

---
+++ packages/pytransliterate/pytransliterate-0.1.0.tar.gz/pytransliterate-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pytransliterate',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Stephen Hudson',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    packages=['transliterate'],
+    # install_requires=['mpi4py>=2.0',
+    #                   'numpy',                     
+    #                   ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+)
