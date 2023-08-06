c5ece5409373b2395ce7c88a49cbea1cbe10fcda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:53 +0000

    Add package MemeLib/0.1.2/MemeLib-0.1.2.tar.gz

---
+++ packages/MemeLib/MemeLib-0.1.2.tar.gz/MemeLib-0.1.2/memelib/_utils.py
@@ -0,0 +1,13 @@
+import discord
+
+
+def _format(data, embed_color):
+    embed = discord.Embed(
+        title=data["title"],
+        url=data["post_url"],
+        color=embed_color,
+        description=f"{data['author']} | Can't see the image? [Click Here.]({data['img_url']})",
+    )
+    embed.set_image(url=data["image_url"])
+    embed.set_footer(text=f"{data['upvotes']} 👍 | {data['comments']} 💬")
+    return embed
