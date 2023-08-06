4aac08360fd20c5df7f9f51d09fb9e68784fc256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:54 +0000

    Add package django-query-counter/0.1.1/django-query-counter-0.1.1.tar.gz

---
+++ packages/django-query-counter/django-query-counter-0.1.1.tar.gz/django-query-counter-0.1.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+from query_counter import __version__
+
+
+def read_long_description():
+    with open('README.md') as file:
+        return file.read()
+
+
+url = (
+    'https://github.com/conformist-mw/django-query-counter/'
+    'tarball/{0}'.format(__version__)
+)
+
+setup(
+    name='django-query-counter',
+    version=__version__,
+    author='Oleg Smedyuk',
+    author_email='conformist.mw@gmail.com',
+    description=('Debug tool to print sql queries count to the console'),
+    install_requires=['tabulate'],
+    license='MIT',
+    keywords='django sql query count management commands',
+    url=url,
+    packages=[
+        'query_counter',
+    ],
+    long_description=read_long_description(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ],
+)
