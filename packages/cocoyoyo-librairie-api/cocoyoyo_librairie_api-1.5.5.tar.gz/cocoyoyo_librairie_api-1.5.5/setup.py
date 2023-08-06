865aae6fb60ee15475b9797199e7d535b4076d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:26 +0000

    Add package cocoyoyo-librairie-api/1.5.5/cocoyoyo_librairie_api-1.5.5.tar.gz

---
+++ packages/cocoyoyo-librairie-api/cocoyoyo_librairie_api-1.5.5.tar.gz/cocoyoyo_librairie_api-1.5.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.5.5'
+DESCRIPTION = 'Official CocoyoyoLibrairie API packages'
+LONG_DESCRIPTION = 'A package to search books and users on the cocoyoyolibrairie !'
+
+setup(
+    name="cocoyoyo_librairie_api",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="CAMARM-DEV",
+    author_email="armand@camponovo.xyz",
+    license='MIT',
+    packages=['cocoyoyo_librairie_api'],
+    install_requires=['requests~=2.25.1',
+                      'colorama~=0.4.4',
+                      ],
+    keywords='conversion',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
