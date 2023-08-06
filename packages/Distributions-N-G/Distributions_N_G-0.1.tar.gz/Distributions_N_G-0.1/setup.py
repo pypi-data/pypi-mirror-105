40f02a97991ef36a1e29cf907df3d4acd31dc7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:32 +0000

    Add package Distributions-N-G/0.1/Distributions_N_G-0.1.tar.gz

---
+++ packages/Distributions-N-G/Distributions_N_G-0.1.tar.gz/Distributions_N_G-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Distributions_N_G',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Distributions_N_G'],
+      author='Basil ELdho',
+      authoremail='basileljol@gmail.com',
+      zip_safe=False)
