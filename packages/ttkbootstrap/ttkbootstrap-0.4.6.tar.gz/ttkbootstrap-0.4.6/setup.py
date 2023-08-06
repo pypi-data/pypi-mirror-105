d6ace28fa7ad55669fccf6a36cf48b7ed4498253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:24 +0000

    Add package ttkbootstrap/0.4.6/ttkbootstrap-0.4.6.tar.gz

---
+++ packages/ttkbootstrap/ttkbootstrap-0.4.6.tar.gz/ttkbootstrap-0.4.6/setup.py
@@ -0,0 +1,36 @@
+"""
+Because I'm using importlib.resources, which did not exist until python 3.7, this does not support < 3.7 unless I can
+figure out another method for importing package resources.
+"""
+import setuptools
+
+long_description = """
+A collection of modern flat themes inspired by Bootstrap. There are more than a dozen built-in themes, and you also have 
+the ability to easily create your own.
+
+## Links
+- **Documentation:** https://ttkbootstrap.readthedocs.io/en/latest/  
+- **GitHub:** https://github.com/israel-dryer/ttkbootstrap
+"""
+
+setuptools.setup(
+    name="ttkbootstrap",
+    version="0.4.6",
+    author="Israel Dryer",
+    author_email="israel.dryer@gmail.com",
+    description="A collection of modern ttk themes inspired by Bootstrap",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/israel-dryer/ttkbootstrap",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={"": ["*.json", "*.ttf"]},
+    include_package_data=True,
+    install_requires=["pillow>=8.2.0"],
+    python_requires=">=3.7",
+)
