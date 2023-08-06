69a7b33c7dd311a499b1c753f692cec055d2601f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:39 +0000

    Add package python-10minutemail/0.1.1/python-10minutemail-0.1.1.tar.gz

---
+++ packages/python-10minutemail/python-10minutemail-0.1.1.tar.gz/python-10minutemail-0.1.1/README.md
@@ -0,0 +1,53 @@
+# 10MinuteMail
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/python-10minutemail)
+![APM](https://img.shields.io/apm/l/vim-mode)
+[![Build Status](https://www.travis-ci.com/SauravKanchan/10minutemail.svg?branch=main)](https://www.travis-ci.com/SauravKanchan/10minutemail)
+
+Python wrapper for [10minutemail](https://10minutemail.com/)
+
+### Installation
+
+```
+pip install python-10minutemail
+```
+
+### Usage
+
+```python
+from minutemail import Mail
+import time
+
+# Create a new 10 minute mail
+mail = Mail()
+print(mail)
+
+# Keep on checking for a new mail/message
+while True:
+    if mail.new_message():  # Check for new mail
+        print(mail.fetch_message())  # Fetch all the messages
+    time.sleep(2)
+```
+
+### Sample message list
+
+```json
+[
+  {
+    "read": false,
+    "expanded": false,
+    "forwarded": false,
+    "repliedTo": false,
+    "sentDate": "2021-05-10T07:32:41.000+0000",
+    "sentDateFormatted": "May 10, 2021, 7:32:41 AM",
+    "sender": "sauravnk30@gmail.com",
+    "from": "[Ljavax.mail.internet.InternetAddress;@37e8c463",
+    "subject": "Test message",
+    "bodyPlainText": "Test description\r\n",
+    "bodyHtmlContent": "<div dir=\"ltr\">Test description</div>\r\n",
+    "bodyPreview": "Test description\r\n",
+    "id": "2118940165622869807"
+  }
+]
+```
+
+> Warning: If you use this tool/API 
---
+++ packages/python-10minutemail/python-10minutemail-0.1.1.tar.gz/python-10minutemail-0.1.1/src/minutemail.py
@@ -0,0 +1,54 @@
+import requests
+from endpoints import NEW_EMAIL, MESSAGE_AFTER, MESSAGE_COUNT
+
+
+class Mail(object):
+    """
+    Python wrapper for 10minutemail.com
+    """
+
+    def __init__(self):
+        self.session = requests.session()
+        self.message_count = 0
+        self.messages = []
+        self.mail = self.session.get(NEW_EMAIL).json()['address']
+
+    def get_mail(self):
+        """
+        :return: Mail of the current instance
+        """
+        return self.mail
+
+    def get_message(self):
+        """
+        :return: list of messages stored in this instance
+        """
+        return self.messages
+
+    def fetch_message(self):
+        """
+        Fetches for new messages which are not present in the instance
+        :return: List of messages stored in the instance
+        """
+        res = self.session.get(MESSAGE_AFTER + str(self.message_count)).json()
+        self.message_count += len(res)
+        self.messages += res
+        return self.messages
+
+    def new_message(self):
+        """
+        Check whether there are new messages or not
+        :return: bool
+        """
+        return self.session.get(MESSAGE_COUNT).json()['messageCount'] != self.message_count
+
+    def __str__(self):
+        return self.mail
+
+
+if __name__ == "__main__":
+    import time
+    mail = Mail()
+    print(mail.get_mail())
+    while True:
+        time.sleep(2)
