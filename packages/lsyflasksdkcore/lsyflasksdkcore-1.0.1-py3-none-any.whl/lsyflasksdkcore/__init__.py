8ccbfe8e0d0f867f69769d27f21b7e1710afdce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:18 +0000

    Add package lsyflasksdkcore/1.0.1/lsyflasksdkcore-1.0.1-py3-none-any.whl

---
+++ packages/lsyflasksdkcore/lsyflasksdkcore-1.0.1-py3-none-any.whl/lsyflasksdkcore/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from .context import sresponse, eresponse, RequestQuery
+from .model import AutoClass, PagerQuery
+from .schema import PkQuerySchema, PkQuery, PksQuerySchema, PksQuery, \
+    UnionkeyQuery, UnionkeyQuerySchema, UnionkeysQuerySchema, UnionkeysQuery
+
+__all__ = [
+    'PkQuerySchema', 'PkQuery', 'AutoClass', 'PagerQuery', 'sresponse', 'eresponse', 'RequestQuery', 'PksQuerySchema',
+    'PksQuery', 'UnionkeyQuery', 'UnionkeyQuerySchema', 'UnionkeysQuerySchema', 'UnionkeysQuery'
+]
+
+__version__: str = "1.0.1"
