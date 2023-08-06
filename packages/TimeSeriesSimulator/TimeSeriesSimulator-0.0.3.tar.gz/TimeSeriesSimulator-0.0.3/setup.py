d16b110024b4eea909db60c354da570c00f186cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:36 +0000

    Add package TimeSeriesSimulator/0.0.3/TimeSeriesSimulator-0.0.3.tar.gz

---
+++ packages/TimeSeriesSimulator/TimeSeriesSimulator-0.0.3.tar.gz/TimeSeriesSimulator-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# python setup.py sdist bdist_wheel
+# python -m twine upload dist/*
+
+VERSION = '0.0.03'
+DESCRIPTION = 'TimeSeriesSimulation'
+LONG_DESCRIPTION = 'A Package for Agent Based Time Series Modeling'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="TimeSeriesSimulator",
+    version=VERSION,
+    author="Thomas Meli",
+    author_email="<tpmeli@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages
+
+    keywords=['python', 'time series', 'simulator', 'agent'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
