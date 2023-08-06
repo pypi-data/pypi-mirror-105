f45a917d3a22eb4470053d1f33d4770c5044baa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:12 +0000

    Add package sotaai/0.0.8/sotaai-0.0.8.tar.gz

---
+++ packages/sotaai/sotaai-0.0.8.tar.gz/sotaai-0.0.8/setup.py
@@ -0,0 +1,27 @@
+'''
+Core sotaai library setup file
+'''
+
+from setuptools import setup
+
+setup(name='sotaai',
+      version='0.0.8',
+      author='Stateoftheart AI PBC',
+      author_email='admin@stateoftheart.ai',
+      description='Stateoftheart AI Official Library',
+      url='https://github.com/stateoftheartai/sotaai',
+      packages=['sotaai', 'sotaai.cv', 'sotaai.rl'],
+      install_requires=['scikit-image'],
+      python_requires='>=3,<=3.8.0',
+      extras_require={
+          'cv': [
+              'tensorflow', 'tensorflow-datasets', 'torchvision', 'torch',
+              'transformers', 'scikit-image', 'opencv-python==4.5.1.48'
+          ],
+          'neuro': [],
+          'nlp': ['datasets'],
+          'rl': [
+              'gym', 'procgen', 'gym-minigrid', 'box2d', 'gym[atari]',
+              'garage==2020.9.0rc1', 'mpi4py', 'akro'
+          ]
+      })
