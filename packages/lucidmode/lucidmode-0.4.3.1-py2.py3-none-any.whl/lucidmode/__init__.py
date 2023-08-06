2eb8e8e57f54e6e54aeb06fa21c639b6d40ec42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:40 +0000

    Add package lucidmode/0.4.3.1/lucidmode-0.4.3.1-py2.py3-none-any.whl

---
+++ packages/lucidmode/lucidmode-0.4.3.1-py2.py3-none-any.whl/lucidmode/__init__.py
@@ -0,0 +1,8 @@
+"""Transparent and Interpretable Machine Learning Models in Python, with a keras inspired API
+
+``lucidmode`` is a set of interpretable Machine Learning Models.
+
+"""
+__version__ = '0.4.3.1'
+
+__all__ = ['functions', 'models', 'propagate', 'regularization']
