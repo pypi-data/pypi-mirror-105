6f53a46965bf400f3d50ecbec7e2aa17f12ec72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:37 +0000

    Add package pxolly/1.0.0/pxolly-1.0.0.tar.gz

---
+++ packages/pxolly/pxolly-1.0.0.tar.gz/pxolly-1.0.0/pxolly/abc.py
@@ -0,0 +1,45 @@
+import asyncio
+from abc import ABC, abstractmethod
+from typing import Dict, Union
+
+from attrdict import AttrDict
+from pydantic import BaseModel
+
+
+class BaseAPICategoriesABC(ABC):
+    ...
+
+
+class APICategoriesABC(ABC):
+    ...
+
+
+class PxollyAPIABC(ABC):
+
+    @property
+    @abstractmethod
+    def api_instance(self) -> "PxollyAPIABC":
+        ...
+
+    @abstractmethod
+    def make_request(
+            self,
+            method: str,
+            data: Dict = None,
+            dataclass: Union[dict, AttrDict, BaseModel] = AttrDict
+    ) -> dict:
+        ...
+
+    @abstractmethod
+    async def make_request_async(
+            self,
+            method: str,
+            data: Dict = None,
+            dataclass: Union[dict, AttrDict, BaseModel] = AttrDict
+    ) -> dict:
+        ...
+
+    @property
+    @abstractmethod
+    def loop(self) -> asyncio.AbstractEventLoop:
+        ...
