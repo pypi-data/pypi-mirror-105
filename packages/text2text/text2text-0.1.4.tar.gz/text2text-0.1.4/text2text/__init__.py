b4bff8a6b3e5091061f3b0abdf3a6fc54d6f8e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:37 +0000

    Add package text2text/0.1.4/text2text-0.1.4.tar.gz

---
+++ packages/text2text/text2text-0.1.4.tar.gz/text2text-0.1.4/text2text/__init__.py
@@ -0,0 +1,7 @@
+from .transformer import Transformer
+from .translator import Translator
+from .variator import Variator
+from .abstractor import Abstractor
+from .summarizer import Summarizer
+from .questioner import Questioner
+from .handler import Handler
