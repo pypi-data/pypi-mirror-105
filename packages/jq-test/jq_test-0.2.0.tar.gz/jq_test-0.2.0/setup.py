80aa544935be71694bd3c25480b56861b41b4366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:02 +0000

    Add package jq-test/0.2.0/jq_test-0.2.0.tar.gz

---
+++ packages/jq-test/jq_test-0.2.0.tar.gz/jq_test-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jq_test']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'jq-test',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'fikirsiz',
+    'author_email': 'hromus@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
