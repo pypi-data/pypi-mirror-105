79f471cd06a37141861a4ff61a0ed395e43751bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:18 +0000

    Add package tags.py/0.8.7/tags.py-0.8.7.tar.gz

---
+++ packages/tags.py/tags.py-0.8.7.tar.gz/tags.py-0.8.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import re
+
+ver = ''
+with open("tags/__init__.py") as f:
+    ver = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+readme = ''
+
+with open("README.md") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='tags.py',
+    author='priguy914629',
+    version = ver,
+    url='https://github.com/proguy914629bot/tags.py',
+    license='MIT',
+    description='A Tag helper.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.5",
+    #package_dir={"": "src"},
+    packages=["tags"]
+)
