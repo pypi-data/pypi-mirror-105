c1a4e4bc7202761c6b906975ddae95d2a986bcaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:38 +0000

    Add package discord-ext-games/1.1.3/discord-ext-games-1.1.3.tar.gz

---
+++ packages/discord-ext-games/discord-ext-games-1.1.3.tar.gz/discord-ext-games-1.1.3/discord/ext/games/tic_tac_toe/config.py
@@ -0,0 +1,27 @@
+class CONFIG:
+    __slots__ = (
+        "blank",
+        "emoji_x",
+        "emoji_o",
+        "up_left",
+        "up",
+        "up_right",
+        "left",
+        "mid",
+        "right",
+        "down_left",
+        "down",
+        "down_right",
+    )
+    blank = "\N{LARGE BLUE SQUARE}"
+    emoji_x = "\N{REGIONAL INDICATOR SYMBOL LETTER X}"
+    emoji_o = "\N{REGIONAL INDICATOR SYMBOL LETTER O}"
+    up_left = "\N{NORTH WEST ARROW}"
+    up = "\N{UPWARDS BLACK ARROW}"
+    up_right = "\N{NORTH EAST ARROW}"
+    left = "\N{LEFTWARDS BLACK ARROW}"
+    mid = "\N{BLACK SQUARE FOR STOP}"
+    right = "\N{BLACK RIGHTWARDS ARROW}"
+    down_left = "\N{SOUTH WEST ARROW}"
+    down = "\N{DOWNWARDS BLACK ARROW}"
+    down_right = "\N{SOUTH EAST ARROW}"
