7b5ad352d70d39c8f4c5b23133d6d7d1191c3ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:23 +0000

    Add package ipytest/0.10.0b1/ipytest-0.10.0b1.tar.gz

---
+++ packages/ipytest/ipytest-0.10.0b1.tar.gz/ipytest-0.10.0b1/ipytest/__init__.py
@@ -0,0 +1,17 @@
+from ._config import config, autoconfig
+from ._util import clean_tests, reload
+from ._pytest_support import run, ModuleCollectorPlugin
+
+# the pytest exit code
+exit_code = None
+
+
+__all__ = [
+    "autoconfig",
+    "run",
+    "config",
+    "clean_tests",
+    "reload",
+    "running_as_test",
+    "ModuleCollectorPlugin",
+]
