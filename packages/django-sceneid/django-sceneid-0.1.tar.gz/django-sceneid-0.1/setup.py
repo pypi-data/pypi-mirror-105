7735ceded6c6214aa2ee3f6cbd8690c076f33531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:21 +0000

    Add package django-sceneid/0.1/django-sceneid-0.1.tar.gz

---
+++ packages/django-sceneid/django-sceneid-0.1.tar.gz/django-sceneid-0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="django-sceneid",
+    version="0.1",
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    test_suite="tests",
+    url="https://github.com/demozoo/django-sceneid/",
+
+    author="Matt Westcott",
+    author_email="matt@west.co.tt",
+    description="SceneID authentication for Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'Django>=3.0,<4',
+        'requests>=2,<3',
+    ],
+    extras_require={
+        "testing": [
+            'responses>=0.13,<1',
+        ]
+    },
+    license="BSD",
+)
