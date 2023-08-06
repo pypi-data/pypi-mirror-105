df39662cba152420cd86e107c21de1453e87403c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:17 +0000

    Add package phdictionary/0.0.2/phdictionary-0.0.2.tar.gz

---
+++ packages/phdictionary/phdictionary-0.0.2.tar.gz/phdictionary-0.0.2/src/phdictionary/__init__.py
@@ -0,0 +1,7 @@
+from phdictionary.dictionary import get_definition,\
+    get_synonym,\
+    get_french_english,\
+    get_english_french, \
+    get_synonyms_from_file, \
+    get_french_english_from_file, \
+    get_definition_from_file
