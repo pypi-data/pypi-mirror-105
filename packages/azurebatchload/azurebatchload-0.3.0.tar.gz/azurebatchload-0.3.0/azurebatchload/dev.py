7a7e9f9dea8b47b59184f3ac84702002d641fc6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:05 +0000

    Add package azurebatchload/0.3.0/azurebatchload-0.3.0.tar.gz

---
+++ packages/azurebatchload/azurebatchload-0.3.0.tar.gz/azurebatchload-0.3.0/azurebatchload/dev.py
@@ -0,0 +1,33 @@
+# flake8: noqa
+import time
+import os
+from azurebatchload.upload import Upload
+from azurebatchload.download import Download
+from azurebatchload.utils import Utils
+import pandas as pd
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True, verbose=True)
+
+
+# Download(
+#     destination="data",
+#     source="twinfield",
+#     folder="xml_responses/vrk/20201117",
+#     method="single"
+# ).download()
+
+# Upload(
+#     destination="test",
+#     source="data",
+#     method="single",
+#     extension=".xml",
+#     overwrite=True
+# ).upload()
+
+files = Utils(
+    container="test", name_starts_with="data", dataframe=True, extended_info=True
+).list_blobs()
+
+print(files)
