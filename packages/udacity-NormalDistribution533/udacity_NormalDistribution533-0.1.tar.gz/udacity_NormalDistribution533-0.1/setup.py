6f8eecdfbd84189c107b26c087c0e5bf682b8aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:07 +0000

    Add package udacity-NormalDistribution533/0.1/udacity_NormalDistribution533-0.1.tar.gz

---
+++ packages/udacity-NormalDistribution533/udacity_NormalDistribution533-0.1.tar.gz/udacity_NormalDistribution533-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacity_NormalDistribution533',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_NormalDistribution533'],
+      zip_safe=False)
