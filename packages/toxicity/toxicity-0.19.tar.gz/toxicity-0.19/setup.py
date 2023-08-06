16cf60d5af2b68200e288fa5c730450a511b9474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:26 +0000

    Add package toxicity/0.19/toxicity-0.19.tar.gz

---
+++ packages/toxicity/toxicity-0.19.tar.gz/toxicity-0.19/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme:
+  long_description = readme.read()
+
+setup(
+  name = 'toxicity',
+  version = '0.19',
+  packages = find_packages(),
+  license='MIT',
+  description = 'Deep learning classifier of russian toxic comments',
+  # long_description = long_description,
+  author = 'Barsukov Nikita',
+  author_email = 'nikita.s.barsukov@gmail.com',
+  url = 'https://github.com/nsbarsukov/toxic-comments-detector',
+  download_url = 'https://github.com/nsbarsukov/toxic-comments-detector/archive/refs/tags/0.1.tar.gz',
+  keywords = ['toxic', 'comments', 'tensorflow', 'classifier'],
+  install_requires=[
+      'tensorflow',
+      'numpy',
+      'navec'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+  include_package_data=True,
+  zip_safe=False,
+  # package_data = {
+  #         # If any package contains *.txt or *.rst files, include them:
+  #         'toxicity': ['*', '*/*', '*/*/*'],
+  #     },
+  exclude_package_data={"": ["__pycache__/*"]}
+)
