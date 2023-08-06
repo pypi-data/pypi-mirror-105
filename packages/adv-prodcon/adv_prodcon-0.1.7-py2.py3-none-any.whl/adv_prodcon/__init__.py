df55ded993a94b6f7b7ae2e9726a1686743cb027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:45 +0000

    Add package adv-prodcon/0.1.7/adv_prodcon-0.1.7-py2.py3-none-any.whl

---
+++ packages/adv-prodcon/adv_prodcon-0.1.7-py2.py3-none-any.whl/adv_prodcon/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for Advanced Producer Consumer."""
+from .adv_prodcon import *
+
+__author__ = """Andrew Creegan"""
+__email__ = 'andrew.s.creegan@gmail.com'
+__version__ = '0.1.7'
