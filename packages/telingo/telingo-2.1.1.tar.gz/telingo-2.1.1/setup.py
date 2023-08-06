4f7cf35e58724db4ffd81fbd19cf78e04adcbe66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package telingo/2.1.1/telingo-2.1.1.tar.gz

---
+++ packages/telingo/telingo-2.1.1.tar.gz/telingo-2.1.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    version = '2.1.1',
+    name = 'telingo',
+    description = 'System to solve dynamic temporal logic programs.',
+    author = 'Roland Kaminski & Francois Laferriere',
+    license = 'MIT',
+    packages = ['telingo', 'telingo.theory', 'telingo.transformers'],
+    test_suite = 'telingo.tests',
+    zip_safe = False,
+    entry_points = {
+        'console_scripts': [
+            'telingo = telingo:main',
+        ]
+    }
+)
+
