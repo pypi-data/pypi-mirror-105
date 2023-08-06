a4070fd4a1413c5c6076467c12853c8f746050bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:38 +0000

    Add package nester9876/1.0.0/nester9876-1.0.0.tar.gz

---
+++ packages/nester9876/nester9876-1.0.0.tar.gz/nester9876-1.0.0/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+    name    = 'nester9876',
+    version = '1.0.0',
+    py_modules = ['nester9876'],
+    author  = 'dinghongru',
+    author_email = '15910690996@163.com',
+)
