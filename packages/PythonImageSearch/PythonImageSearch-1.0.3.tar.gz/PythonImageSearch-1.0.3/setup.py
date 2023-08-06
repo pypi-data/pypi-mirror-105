774cb8f2fdb1477a29ee10b930fd325415399a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:20 +0000

    Add package PythonImageSearch/1.0.3/PythonImageSearch-1.0.3.tar.gz

---
+++ packages/PythonImageSearch/PythonImageSearch-1.0.3.tar.gz/PythonImageSearch-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PythonImageSearch',
+    version="1.0.3",
+    url='https://github.com/RandomPythonProgrammer/python-image-search',
+    author='RandomPythonProgrammer',
+    author_email="jhc3628@gmail.com",
+    description='Simple image search',
+    long_description="Idk just a random python image search thing, it uses selenium and stuff.",
+    install_requires=['selenium', 'bs4', 'requests'],
+    keywords="Image Search",
+    packages=["Image_Search"]
+)
