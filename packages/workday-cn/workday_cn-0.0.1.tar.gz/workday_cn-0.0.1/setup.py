ce2351d4ad2e003d493397cc3e19aa364cb44535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:14 +0000

    Add package workday-cn/0.0.1/workday_cn-0.0.1.tar.gz

---
+++ packages/workday-cn/workday_cn-0.0.1.tar.gz/workday_cn-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="workday_cn",
+    version="0.0.1",
+    author="lovic",
+    author_email="",
+    description="workday detection",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    # install_requires=[
+    #     'pymysql',
+    #     'pandas',
+    #     'requests',
+    #     'sqlalchemy'
+    # ],
+    python_requires='>=3.6'
+)
