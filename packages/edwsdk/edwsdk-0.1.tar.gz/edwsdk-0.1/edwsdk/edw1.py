c03b22f37c29649a34a0021ef6fb19e0ef8b889c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:57 +0000

    Add package edwsdk/0.1/edwsdk-0.1.tar.gz

---
+++ packages/edwsdk/edwsdk-0.1.tar.gz/edwsdk-0.1/edwsdk/edw1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： hj
+# datetime： 2021/5/13 0013 下午 2:47 
+# ide： PyCharm2020.1.3
+
+def fun1():
+    print("这是edw1模块的函数1")
+
+def fun2():
+    print("这是edw1模块的函数2")
