43e20fc178d5033a69ad52125372829ef8c43407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:20 +0000

    Add package frinx-conductor-workers/1.0.0/frinx_conductor_workers-1.0.0.tar.gz

---
+++ packages/frinx-conductor-workers/frinx_conductor_workers-1.0.0.tar.gz/frinx_conductor_workers-1.0.0/workers/import_workflows.py
@@ -0,0 +1,29 @@
+import os
+import requests
+import traceback
+from workers.frinx_rest import conductor_url_base, conductor_headers
+
+workflow_import_url = conductor_url_base + '/metadata/workflow'
+
+
+def import_workflows(path):
+    if os.path.isdir(path):
+        print('Importing workflows from folder ' + path)
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    try:
+                        print('Importing workflow ' + entry.name)
+                        with open(entry, 'rb') as payload:
+                            r = requests.post(workflow_import_url,
+                                              data=payload, headers=conductor_headers)
+                            print('Response - ' + r.text)
+                    except Exception as err:
+                        print('Error while registering workflow ' + traceback.format_exc())
+                        raise err
+                elif entry.is_dir():
+                    import_workflows(entry.path)
+                else:
+                    print('Ignoring, unknown type ' + entry)
+    else:
+        print('Path to workflows ' + path + ' is not a directory.')
