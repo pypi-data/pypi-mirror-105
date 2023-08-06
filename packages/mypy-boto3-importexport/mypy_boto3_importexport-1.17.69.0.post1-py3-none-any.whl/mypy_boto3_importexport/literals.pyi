f8e945554ec2c8906365027a441feaef2075bb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:57 +0000

    Add package mypy-boto3-importexport/1.17.69.0.post1/mypy_boto3_importexport-1.17.69.0.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-importexport/mypy_boto3_importexport-1.17.69.0.post1-py3-none-any.whl/mypy_boto3_importexport/literals.pyi
@@ -0,0 +1,24 @@
+"""
+Type annotations for importexport service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_importexport.literals import JobType
+
+    data: JobType = "Export"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("JobType", "ListJobsPaginatorName")
+
+JobType = Literal["Export", "Import"]
+ListJobsPaginatorName = Literal["list_jobs"]
