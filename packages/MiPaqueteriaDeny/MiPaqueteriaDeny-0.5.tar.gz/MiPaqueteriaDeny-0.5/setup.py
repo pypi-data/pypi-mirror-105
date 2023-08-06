4e906f5edb85ad3b85f0e1236cfd53f202f7b4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:32 +0000

    Add package MiPaqueteriaDeny/0.5/MiPaqueteriaDeny-0.5.tar.gz

---
+++ packages/MiPaqueteriaDeny/MiPaqueteriaDeny-0.5.tar.gz/MiPaqueteriaDeny-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='MiPaqueteriaDeny',
+    version='0.5',
+    packages=find_packages(),
+    py_modules=['MiPaqueteriaOperaciones']
+)
+
+
+
+
