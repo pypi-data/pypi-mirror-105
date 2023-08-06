9d71b80896a17189ee5e95c2889c16fa60e56716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:37 +0000

    Add package tiramisu-brulee/0.1.3/tiramisu_brulee-0.1.3.tar.gz

---
+++ packages/tiramisu-brulee/tiramisu_brulee-0.1.3.tar.gz/tiramisu_brulee-0.1.3/tiramisu_brulee/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Jacob Reinhold"""
+__email__ = 'jcreinhold@gmail.com'
+__version__ = '0.1.3'
+
+from .loss import *
+from .model import *
+from .util import *
