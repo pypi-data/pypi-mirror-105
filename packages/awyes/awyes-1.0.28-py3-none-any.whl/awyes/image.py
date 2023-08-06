00e1e909243fbb8c151ef3312295124d683edd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:35 +0000

    Add package awyes/1.0.28/awyes-1.0.28-py3-none-any.whl

---
+++ packages/awyes/awyes-1.0.28-py3-none-any.whl/awyes/image.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+
+
+def deploy_images(self):
+    for dockerfile in self.images:
+        pprint([
+            line for line in
+            self.__class__.docker_client.build(
+                fileobj=open(self.root + '/' + dockerfile, 'rb')
+            )
+        ])
