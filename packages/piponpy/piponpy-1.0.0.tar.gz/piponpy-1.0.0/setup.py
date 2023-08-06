d312711ca8ca2b730bd57aed07e7980d6f9061a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:19 +0000

    Add package piponpy/1.0.0/piponpy-1.0.0.tar.gz

---
+++ packages/piponpy/piponpy-1.0.0.tar.gz/piponpy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='piponpy',
+    version='1.0.0',
+    description='Place holder to reserve the pip-on-py name',
+    long_description='Place holder to reserve the pip-on-py name',
+    packages=["piponpy"]
+    )
+
