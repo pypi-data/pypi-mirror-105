cdbbe72c1ff902c9c332148e4501176e0aee2a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:23 +0000

    Add package aadhaar-py/1.0.0/aadhaar_py-1.0.0-py3-none-any.whl

---
+++ packages/aadhaar-py/aadhaar_py-1.0.0-py3-none-any.whl/aadhaar/exceptions.py
@@ -0,0 +1,18 @@
+class NumberOutOfRangeException(Exception):
+
+    pass
+
+
+class MalformedIntegerReceived(Exception):
+
+    pass
+
+
+class EmptyArchiveException(Exception):
+
+    pass
+
+
+class NoXMLFileFound(Exception):
+
+    pass
