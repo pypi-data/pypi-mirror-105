6a90f4816d03e5970e9a11013a22c2d4a6b1eba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:27 +0000

    Add package apread/0.0.4/apread-0.0.4.tar.gz

---
+++ packages/apread/apread-0.0.4.tar.gz/apread-0.0.4/apread/__init__.py
@@ -0,0 +1,4 @@
+# the main apreader
+from apread.apreader import *
+# binary reader
+from apread.binaryReader import *
---
+++ packages/apread/apread-0.0.4.tar.gz/apread-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="apread",
+    version="0.0.4",
+    description="Import data from CatmanAP binary files.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leonbohmann/apreader",
+    author="Leon Bohmann",
+    author_email="info@leonbohmann.de",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
