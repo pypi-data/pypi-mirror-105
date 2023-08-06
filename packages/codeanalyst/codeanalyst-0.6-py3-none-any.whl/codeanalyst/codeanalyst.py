ddda270dfcead9afa9dd660fa49160017b37ceac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:22 +0000

    Add package codeanalyst/0.6/codeanalyst-0.6-py3-none-any.whl

---
+++ packages/codeanalyst/codeanalyst-0.6-py3-none-any.whl/codeanalyst/codeanalyst.py
@@ -0,0 +1,38 @@
+import requests
+import json
+from . import api
+from codeanalyst.data import *
+url = api
+
+def codetest(code, lang, langv = "None", cid = "2c1d7916a28c2ac644a6acb7abdf8847", secret = "36a72c45e217e3097fded87023f138722beaedc1b345ad50e3f7094ff9145548"):
+  
+  if lang.lower() not in languages:
+    raise CompilerError("Unknown language.")
+
+  if langv == "None" and lang in v3_langs:
+    langv = 3
+  elif langv == "None" and lang in v2_langs:
+    langv = 2
+  elif langv == "None" and lang in v1_langs:
+    langv = 1
+  elif langv == "None" and lang in v0_langs:
+    langv = 0
+  
+  
+  jsun = {
+    "clientId": cid,
+    "clientSecret": secret,
+    "script": code,
+    "language": languages.get(lang),
+    "versionIndex": langv}
+  req = requests.post(url, json = jsun)
+  result = json.loads(req.text)
+  return CodeAnalystResult(result)
+
+
+class CodeAnalystResult:
+  def __init__(self, result):
+    self.result = result
+
+class CompilerError(Exception):
+    pass
