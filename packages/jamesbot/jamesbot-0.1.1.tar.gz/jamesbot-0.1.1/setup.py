e5c61bb39d43eb43c2eeca480bd3e46182b64681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package jamesbot/0.1.1/jamesbot-0.1.1.tar.gz

---
+++ packages/jamesbot/jamesbot-0.1.1.tar.gz/jamesbot-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jamesbot", 
+    version="0.1.1",
+    author="Faruk Hammoud, Felipe Freire Pinto",
+    author_email="farukhammoud@student-cs.fr, felipe.fp@student-cs.fr",
+    description="Download and Store financial data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/felipe-fp/JamesBot",
+    packages=setuptools.find_packages(),
+	install_requires=[
+   'pandas',
+   'datetime',
+   'yfinance',
+   'xbbg'
+	],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
