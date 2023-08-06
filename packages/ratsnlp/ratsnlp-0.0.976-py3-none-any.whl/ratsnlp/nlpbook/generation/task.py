74381ab5b03a3454b80cd0cd774fb33d1be647db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:30 +0000

    Add package ratsnlp/0.0.976/ratsnlp-0.0.976-py3-none-any.whl

---
+++ packages/ratsnlp/ratsnlp-0.0.976-py3-none-any.whl/ratsnlp/nlpbook/generation/task.py
@@ -0,0 +1,49 @@
+from transformers import PreTrainedModel
+from transformers.optimization import AdamW
+from pytorch_lightning import LightningModule
+from pytorch_lightning.core.step_result import Result
+from ratsnlp.nlpbook.generation.arguments import GenerationTrainArguments
+from torch.optim.lr_scheduler import ExponentialLR, CosineAnnealingWarmRestarts
+
+
+class GenerationTask(LightningModule):
+
+    def __init__(self,
+                 model: PreTrainedModel,
+                 args: GenerationTrainArguments,
+    ):
+        super().__init__()
+        self.model = model
+        self.args = args
+
+    def configure_optimizers(self):
+        if self.args.optimizer == 'AdamW':
+            optimizer = AdamW(self.parameters(), lr=self.args.learning_rate)
+        else:
+            raise NotImplementedError('Only AdamW is Supported!')
+        if self.args.lr_scheduler == 'cos':
+            scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=1, T_mult=2)
+        elif self.args.lr_scheduler == 'exp':
+            scheduler = ExponentialLR(optimizer, gamma=0.5)
+        else:
+            raise NotImplementedError('Only cos and exp lr scheduler is Supported!')
+        return {
+            'optimizer': optimizer,
+            'scheduler': scheduler,
+        }
+
+    def training_step(self, inputs, batch_idx):
+        # loss, _, _ = self.model(**inputs)
+        # self.log("train_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        # return Result(minimize=loss)
+        outputs = self.model(**inputs)
+        self.log("train_loss", outputs.loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        return outputs.loss
+
+    def validation_step(self, inputs, batch_idx):
+        # loss, _, _ = self.model(**inputs)
+        # self.log("val_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        # return Result(minimize=loss)
+        outputs = self.model(**inputs)
+        self.log("val_loss", outputs.loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        return outputs.loss
