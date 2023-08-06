f056de280f9f8b118b9a29da906b87b46da70fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:24 +0000

    Add package shrike/1.0.0rc3/shrike-1.0.0rc3.tar.gz

---
+++ packages/shrike/shrike-1.0.0rc3.tar.gz/shrike-1.0.0rc3/shrike/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+Python utilities to aid "compliant experimentation" - training
+machine learning models without seeing the training data.
+"""
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+__version__ = "1.0.0-rc3"
