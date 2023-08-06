72d3745516af162a69b19079a4962bbe6d2e310e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:52 +0000

    Add package model-card-generator/0.0.2/model-card-generator-0.0.2.tar.gz

---
+++ packages/model-card-generator/model-card-generator-0.0.2.tar.gz/model-card-generator-0.0.2/src/model_card_generator/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+from model_card import ModelCard
+
+env_dict = {
+    # Model card envs
+    'name': 'MODEL_NAME',
+    'date': 'MODEL_DATE',
+    'version': 'MODEL_VERSION'
+    # ...
+}
+
+def get_env(name):
+    # Verify if env exists
+    return os.environ[env_dict[name]]
+
+mc = ModelCard(
+    get_env('name'), 
+    get_env('date'), 
+    get_env('version')
+)
+
+print("Model name is", mc.name)
+print("Model date is", mc.date)
+print("Model version is", mc.version)
---
+++ packages/model-card-generator/model-card-generator-0.0.2.tar.gz/model-card-generator-0.0.2/src/model_card_generator/model_card.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+class ModelCard:
+    """Machine Learning Model Card"""
+    
+    def __init__(self, name, date, version):
+            self.name = name
+            self.date = date 
+            self.version = version
