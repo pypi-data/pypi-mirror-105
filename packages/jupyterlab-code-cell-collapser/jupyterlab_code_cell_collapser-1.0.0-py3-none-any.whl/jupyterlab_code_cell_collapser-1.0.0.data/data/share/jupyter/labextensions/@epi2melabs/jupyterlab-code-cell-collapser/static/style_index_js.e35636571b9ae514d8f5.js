cde331d71e6755ff3a365a867c41067aa3731a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:49 +0000

    Add package jupyterlab-code-cell-collapser/1.0.0/jupyterlab_code_cell_collapser-1.0.0-py3-none-any.whl

---
+++ packages/jupyterlab-code-cell-collapser/jupyterlab_code_cell_collapser-1.0.0-py3-none-any.whl/jupyterlab_code_cell_collapser-1.0.0.data/data/share/jupyter/labextensions/@epi2melabs/jupyterlab-code-cell-collapser/static/style_index_js.e35636571b9ae514d8f5.js
@@ -0,0 +1,75 @@
+(self["webpackChunk_epi2melabs_jupyterlab_code_cell_collapser"] = self["webpackChunk_epi2melabs_jupyterlab_code_cell_collapser"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/index.css":
+/*!***************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/index.css ***!
+  \***************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "\n\n.jp-CodeCellCollapser {\n  padding: 0;\n  margin: 0;\n  border: 0;\n  height: 100%;\n  position: absolute;\n  left: 5px;\n  top: 0;\n  z-index: 10000;\n  display: block;\n  border-radius: 2px;\n  box-sizing: border-box;\n}\n\n.jp-CodeCellCollapser:hover > button {\n  background-color: #ccc;\n}\n\n.jp-CodeCellCollapser>button {\n  padding: 0;\n  margin: 0;\n  border: 0;\n  height: 100%;\n}\n\n.jp-Cell-inputAreaCollapsed .CodeMirror {\n  max-height: 24px;\n}\n\n.jp-Cell-inputAreaCollapsed .CodeMirror-line {\n  display: none;\n}\n\n.jp-Cell-inputAreaCollapsed .CodeMirror-line:nth-child(1) {\n  display: initial;\n}", "",{"version":3,"sources":["webpack://./style/index.css"],"names":[],"mappings":";;AAEA;EACE,UAAU;EACV,SAAS;EACT,SAAS;EACT,YAAY;EACZ,kBAAkB;EAClB,SAAS;EACT,MAAM;EACN,cAAc;EACd,cAAc;EACd,kBAAkB;EAClB,sBAAsB;AACxB;;AAEA;EACE,sBAAsB;AACxB;;AAEA;EACE,UAAU;EACV,SAAS;EACT,SAAS;EACT,YAAY;AACd;;AAEA;EACE,gBAAgB;AAClB;;AAEA;EACE,aAAa;AACf;;AAEA;EACE,gBAAgB;AAClB","sourcesContent":["\n\n.jp-CodeCellCollapser {\n  padding: 0;\n  margin: 0;\n  border: 0;\n  height: 100%;\n  position: absolute;\n  left: 5px;\n  top: 0;\n  z-index: 10000;\n  display: block;\n  border-radius: 2px;\n  box-sizing: border-box;\n}\n\n.jp-CodeCellCollapser:hover > button {\n  background-color: #ccc;\n}\n\n.jp-CodeCellCollapser>button {\n  padding: 0;\n  margin: 0;\n  border: 0;\n  height: 100%;\n}\n\n.jp-Cell-inputAreaCollapsed .CodeMirror {\n  max-height: 24px;\n}\n\n.jp-Cell-inputAreaCollapsed .CodeMirror-line {\n  display: none;\n}\n\n.jp-Cell-inputAreaCollapsed .CodeMirror-line:nth-child(1) {\n  display: initial;\n}"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/index.css":
+/*!*************************!*\
+  !*** ./style/index.css ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./index.css */ "./node_modules/css-loader/dist/cjs.js!./style/index.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default, options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ "./style/index.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.e35636571b9ae514d8f5.js.map
