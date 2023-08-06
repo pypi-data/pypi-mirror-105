417fc284dadbcfe50adb6389f370f41e815806c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:16 +0000

    Add package py4macro/0.1.2/py4macro-0.1.2-py2.py3-none-any.whl

---
+++ packages/py4macro/py4macro-0.1.2-py2.py3-none-any.whl/py4macro/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+# https://github.com/Py4Macro/py4macro.git
+
+from .py4macro import *
+
+__all__ = ['data','trend']
+
+__author__ = 'Tetsu Haruyama'
+__version__ = '0.1.0'
+__copyright__ = 'Copyright (c) 2021 Tetsu Haruyama'
+
