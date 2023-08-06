630491aa061b56c89288212e6a93633fce975348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:17 +0000

    Add package gaus-bino-disto/0.1.1/gaus_bino_disto-0.1.1.tar.gz

---
+++ packages/gaus-bino-disto/gaus_bino_disto-0.1.1.tar.gz/gaus_bino_disto-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_bino_disto',
+      version='0.1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaus_bino_disto'],
+      author = "Erol SEN",
+      author_email = "ierolsen@outlook.com",
+      zip_safe=False)
