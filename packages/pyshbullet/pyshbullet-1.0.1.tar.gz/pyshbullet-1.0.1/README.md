72f5d21691612608018733a2d1af06e28bca98d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:29 +0000

    Add package pyshbullet/1.0.1/pyshbullet-1.0.1.tar.gz

---
+++ packages/pyshbullet/pyshbullet-1.0.1.tar.gz/pyshbullet-1.0.1/README.md
@@ -0,0 +1,2 @@
+# Long description - still work in progress.
+## The description will be updated in future versions.
---
+++ packages/pyshbullet/pyshbullet-1.0.1.tar.gz/pyshbullet-1.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+setuptools.setup(
+  name='pyshbullet',
+  version='1.0.01',
+  author='98129182',
+  author_email='the98129182@gmail.com',
+  description='Python API wrapper for the pushbullet API',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/98129182/pyshbullet',
+  packages=setuptools.find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.6',
+)
