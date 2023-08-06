8b06863c3ed00e3f4099641a9cdfe13ace8184a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:57 +0000

    Add package wizwalker/1.0.1/wizwalker-1.0.1-py3-none-any.whl

---
+++ packages/wizwalker/wizwalker-1.0.1-py3-none-any.whl/wizwalker/combat/card.py
@@ -0,0 +1,139 @@
+from typing import Union
+
+import wizwalker
+
+
+class CombatCard:
+    """
+    Represents a spell card
+    """
+
+    def __init__(
+        self, combat_handler, spell_window: "wizwalker.memory.window.DynamicWindow",
+    ):
+        self.combat_handler = combat_handler
+
+        self._spell_window = spell_window
+
+    # TODO: add checks before casting
+    async def cast(
+        self, target: Union["CombatCard", "wizwalker.combat.CombatMember", None]
+    ):
+        """
+        Cast this Card on another Card; a Member or with no target
+
+        Args:
+            target: Card, Member, or None if there is no target
+
+        Raises:
+            NotEnoughPips: If you don't have enough pips to cast
+        """
+        if isinstance(target, CombatCard):
+            await self.combat_handler.client.mouse_handler.click_window(
+                self._spell_window
+            )
+            await self.combat_handler.client.mouse_handler.click_window(
+                target._spell_window
+            )
+
+        elif target is None:
+            await self.combat_handler.client.mouse_handler.click_window(
+                self._spell_window
+            )
+
+        else:
+            await self.combat_handler.client.mouse_handler.click_window(
+                self._spell_window
+            )
+            await self.combat_handler.client.mouse_handler.click_window(
+                await target.get_health_text_window()
+            )
+
+    async def discard(self):
+        """
+        Discard this Card
+        """
+        await self.combat_handler.client.mouse_handler.click_window(
+            self._spell_window, right_click=True
+        )
+
+    async def get_graphical_spell(
+        self,
+    ) -> "wizwalker.memory.spell.DynamicGraphicalSpell":
+        """
+        The GraphicalSpell with information about this card
+        """
+        return await self._spell_window.maybe_graphical_spell()
+
+    async def name(self) -> str:
+        """
+        The name of this card
+        """
+        graphical_spell = await self.get_graphical_spell()
+        spell_template = await graphical_spell.spell_template()
+        # name is the actual name; display name is some wack stuff
+        return await spell_template.name()
+
+    async def template_id(self) -> int:
+        """
+        This card's template id
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.template_id()
+
+    async def spell_id(self) -> int:
+        """
+        This card's spell id
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.spell_id()
+
+    # TODO: test with accuracy enchants
+    async def accuracy(self) -> int:
+        """
+        Current accuracy of this card
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.accuracy()
+
+    async def is_treasure_card(self) -> bool:
+        """
+        If this card is a treasure card
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.treasure_card()
+
+    async def is_item_card(self) -> bool:
+        """
+        If this card is an item card
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.item_card()
+
+    async def is_side_board(self) -> bool:
+        """
+        If this card is from the side deck
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.side_board()
+
+    async def is_cloaked(self) -> bool:
+        """
+        If this card is cloaked
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.cloaked()
+
+    async def is_enchanted_from_item_card(self) -> bool:
+        """
+        If this card was enchanted from an item card
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.enchantment_spell_is_item_card()
+
+    async def is_pve_only(self) -> bool:
+        """
+        If this card can only be used in pve
+        """
+        graphical_spell = await self.get_graphical_spell()
+        return await graphical_spell.pve()
---
+++ packages/wizwalker/wizwalker-1.0.1-py3-none-any.whl/wizwalker/memory/memory_objects/enums.py
@@ -0,0 +1,82 @@
+from enum import Enum, IntFlag
+
+
+class DuelPhase(Enum):
+    starting = 0
+    pre_planning = 1
+    planning = 2
+    pre_execution = 3
+    execution = 4
+    resolution = 5
+    victory = 6
+    ended = 7
+    max = 10
+
+
+class SigilInitiativeSwitchMode(Enum):
+    none = 0
+    reroll = 1
+    switch = 2
+
+
+class DuelExecutionOrder(Enum):
+    sequential = 0
+    alternating = 1
+
+
+class PipAquiredByEnum(Enum):
+    unknown = 0
+    normal = 1
+    power = 2
+    normal_to_power_conversion = 4
+    impede_pips = 5
+
+
+class DelayOrder(Enum):
+    any_order = 0
+    first = 1
+    second = 2
+
+
+class WindowStyle(IntFlag):
+    has_back = 1
+    scale_children = 2
+    can_move = 4
+    can_scroll = 16
+    focus_locked = 64
+    can_focus = 128
+    can_dock = 32
+    do_not_capture_mouse = 256
+    is_transparent = 256
+    effect_fadeid = 512
+    effect_highlight = 1024
+    has_no_border = 2048
+    ignore_parent_scale = 4096
+    use_alpha_bounds = 8192
+    auto_grow = 16384
+    auto_shrink = 32768
+    auto_resize = 49152
+
+
+class WindowFlags(IntFlag):
+    visible = 1
+    noclip = 2
+    dock_outside = 131072
+    dock_left = 128
+    dock_top = 512
+    dock_right = 256
+    dock_bottom = 1024
+    parent_size = 786432
+    parent_width = 262144
+    parent_height = 524288
+    hcenter = 32768
+    vcenter = 65536
+    disabled = -2147483648
+
+
+class SpellSourceType(IntFlag):
+    caster = 0
+    pet = 1
+    shadow_creature = 2
+    weapon = 3
+    equipment = 4
