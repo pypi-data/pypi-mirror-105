8b448690ee5fe9e11f5ccd2cc5d23c4c8c7d3976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:03 +0000

    Add package torra/1.0.21/torra-1.0.21.tar.gz

---
+++ packages/torra/torra-1.0.21.tar.gz/torra-1.0.21/setup.py
@@ -0,0 +1,28 @@
+#setuptools.setup is looking at one argv parameter; to "build" and "install":
+#python3 setup.py install
+
+pkname='torra'
+
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+from setuptools import setup
+setup(name=pkname,
+	version='1.0.21',
+	packages=[pkname],
+	#opt
+	python_requires='>=3',
+	install_requires=["appdirs>=1.4.3",
+		'python-libtorrent-bin>=1.2.9;platform_system=="Linux" and platform_machine=="x86_64"'],
+	description='Torrent client',
+	long_description=README,
+	long_description_content_type="text/markdown",
+	url='https://github.com/colin-i/tora',
+	author='bot',
+	author_email='costin.botescu@gmail.com',
+	license='MIT',
+	entry_points = {
+		'console_scripts': [pkname+'='+pkname+'.main:main']
+	}
+)
