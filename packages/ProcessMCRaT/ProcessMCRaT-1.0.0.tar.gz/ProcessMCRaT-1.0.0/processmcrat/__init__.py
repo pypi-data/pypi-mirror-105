08fbc09d7caf484ab238a46ac63efd164dc89068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:47 +0000

    Add package ProcessMCRaT/1.0.0/ProcessMCRaT-1.0.0.tar.gz

---
+++ packages/ProcessMCRaT/ProcessMCRaT-1.0.0.tar.gz/ProcessMCRaT-1.0.0/processmcrat/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+        'processmcrat',
+        'mockobservations',
+        'mclib',
+        'plotting'
+        ]
+__version__='1.0.0' # make sure this matches the setup.py
+
+from .processmcrat import *
+from .mclib import *
+from .mockobservations import *
+from .plotting import *
