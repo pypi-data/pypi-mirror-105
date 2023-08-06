529396e9743c6ccb800ed66b4c41656a6438348e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:49 +0000

    Add package broad-dagster-utils/0.2.3/broad_dagster_utils-0.2.3-py3-none-any.whl

---
+++ packages/broad-dagster-utils/broad_dagster_utils-0.2.3-py3-none-any.whl/dagster_utils/resources/google_storage.py
@@ -0,0 +1,34 @@
+from typing import Iterator
+from dataclasses import dataclass
+
+from dagster import resource
+from dagster.core.execution.context.init import InitResourceContext
+
+import google.auth
+from google.cloud import storage
+
+
+@resource
+def google_storage_client(_: InitResourceContext) -> storage.Client:
+    credentials, project = google.auth.default()
+
+    return storage.Client(project=project, credentials=credentials)
+
+
+@dataclass
+class MockBlob:
+    name: str
+
+    def delete(self) -> None:
+        pass
+
+
+class MockStorageClient:
+    def list_blobs(self, bucket_name: str, prefix: str) -> Iterator[MockBlob]:
+        for i in range(0, 10):
+            yield MockBlob(f"fake_blob_{i}")
+
+
+@resource
+def mock_storage_client(_: InitResourceContext) -> MockStorageClient:
+    return MockStorageClient()
