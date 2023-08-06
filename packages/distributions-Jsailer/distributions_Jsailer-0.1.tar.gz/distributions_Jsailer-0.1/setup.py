cd5b1bf2ff13e47261cb3e45782e491a9c21c0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:08 +0000

    Add package distributions-Jsailer/0.1/distributions_Jsailer-0.1.tar.gz

---
+++ packages/distributions-Jsailer/distributions_Jsailer-0.1.tar.gz/distributions_Jsailer-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_Jsailer',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_Jsailer'],
+      zip_safe=False)
