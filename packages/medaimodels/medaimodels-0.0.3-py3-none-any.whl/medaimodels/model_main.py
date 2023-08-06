75179e8060dcc7ea2b31bd9a318fe89fe46a9fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:49 +0000

    Add package medaimodels/0.0.3/medaimodels-0.0.3-py3-none-any.whl

---
+++ packages/medaimodels/medaimodels-0.0.3-py3-none-any.whl/medaimodels/model_main.py
@@ -0,0 +1,17 @@
+from typing import List
+from model_output import ModelOutput
+
+class ModelMain():
+    """ Class for the main.py file in med ai models
+    Args:
+
+    Attributes:
+
+    """
+    model = None
+
+    def evaluate_model(self, files: List[str]) -> List[ModelOutput]:
+        pass
+
+    def load_model():
+        pass
