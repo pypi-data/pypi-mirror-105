8cbe967ebf0c0e8beb6c1523e5e4f3df2c0c95fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:57 +0000

    Add package toolkit-w/0.0.155/toolkit_w-0.0.155.tar.gz

---
+++ packages/toolkit-w/toolkit_w-0.0.155.tar.gz/toolkit_w-0.0.155/toolkit_w/tellme/tellme.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+# set the sf account name
+sf_account = 'ra45066.eu-west-1'
+from toolkit_w.snowflake.snowflakeq import Snowflakeq
+SQ = Snowflakeq()
+
+
+class Tellme:
+    """ Module for internal analytics - different plots and exploratory data analysis"""
+
+
+
