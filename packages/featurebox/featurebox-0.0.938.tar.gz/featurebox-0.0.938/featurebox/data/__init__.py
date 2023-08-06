dddfc6f1c9edf609fa86d4b8291e103dac7e9c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:49 +0000

    Add package featurebox/0.0.938/featurebox-0.0.938.tar.gz

---
+++ packages/featurebox/featurebox-0.0.938.tar.gz/featurebox-0.0.938/featurebox/data/__init__.py
@@ -0,0 +1,6 @@
+"""
+Available_data: "element_table.csv", "elemental_MEGNet.json", "ie.json", "oe.csv"
+"""
+
+__all__ = ["namesplit", "mp_access", "check_data"]
+
---
+++ packages/featurebox/featurebox-0.0.938.tar.gz/featurebox-0.0.938/featurebox/featurizers/envir/__init__.py
@@ -0,0 +1,2 @@
+"""This part is for environment features, contains bond and atom features."""
+__all__ = ["environment"]
