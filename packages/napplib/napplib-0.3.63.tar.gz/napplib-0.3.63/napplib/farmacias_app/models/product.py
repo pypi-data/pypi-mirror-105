2d99045553870e3248874ff4a6be5bbc99646c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:26 +0000

    Add package napplib/0.3.63/napplib-0.3.63.tar.gz

---
+++ packages/napplib/napplib-0.3.63.tar.gz/napplib-0.3.63/napplib/farmacias_app/models/product.py
@@ -0,0 +1,11 @@
+class OfferEan:
+    def __init__(self, ean='',
+                        qty='',
+                        price='',
+                        sku='',
+                        cnpj=''):
+        self.ean = ean
+        self.qty = qty
+        self.price = price
+        self.sku = sku
+        self.cnpj = cnpj
