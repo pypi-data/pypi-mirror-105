065177b0b5cc350d96de55bcbd7d3f64338c476f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:07 +0000

    Add package gotovrvex/0.1/gotovrvex-0.1.tar.gz

---
+++ packages/gotovrvex/gotovrvex-0.1.tar.gz/gotovrvex-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gotovrvex',
+      version='0.1',
+      description='The robot rides to the specified coordinates',
+      packages=['gotovrvex'],
+      author_email='vl.archit@gmail.com',
+      zip_safe=False)
