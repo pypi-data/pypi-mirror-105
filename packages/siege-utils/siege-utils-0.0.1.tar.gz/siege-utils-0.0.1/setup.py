9b6ecee15c917a932f5f5ad716e94ff6eeb931b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:55 +0000

    Add package siege-utils/0.0.1/siege-utils-0.0.1.tar.gz

---
+++ packages/siege-utils/siege-utils-0.0.1.tar.gz/siege-utils-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="siege-utils",
+    version="0.0.1",
+    author="Jing Yang",
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3"]
+)
