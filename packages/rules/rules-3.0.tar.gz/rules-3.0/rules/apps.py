04d250c029108ec1342d86e7310f9266ee835490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:28 +0000

    Add package rules/3.0/rules-3.0.tar.gz

---
+++ packages/rules/rules-3.0.tar.gz/rules-3.0/rules/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class RulesConfig(AppConfig):
+    name = "rules"
+
+
+class AutodiscoverRulesConfig(RulesConfig):
+    def ready(self):
+        from django.utils.module_loading import autodiscover_modules
+
+        autodiscover_modules("rules")
