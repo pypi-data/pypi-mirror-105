12eb8d36de02996998c39951abfacb6884de1e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:22 +0000

    Add package yuntools/0.1/yuntools-0.1.tar.gz

---
+++ packages/yuntools/yuntools-0.1.tar.gz/yuntools-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yuntools',
+      version='0.1',
+      description='Taiwan No. 1',
+      url='http://github.com/storborg/funniest',
+      author='Michael Chen',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['yuntools'],
+      zip_safe=False)
