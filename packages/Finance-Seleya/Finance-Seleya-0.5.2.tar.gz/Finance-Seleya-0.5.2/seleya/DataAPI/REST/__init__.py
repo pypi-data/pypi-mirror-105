22376f7c1a6983dd37c0601030840e264b64cb7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:53 +0000

    Add package Finance-Seleya/0.5.2/Finance-Seleya-0.5.2.tar.gz

---
+++ packages/Finance-Seleya/Finance-Seleya-0.5.2.tar.gz/Finance-Seleya-0.5.2/seleya/DataAPI/REST/__init__.py
@@ -0,0 +1,19 @@
+from .SEARCH import (
+    company as search_company,
+    bhr_feed as search_bhr_feed,
+    gdelt_feed as search_gdelt_feed,
+    gd_reviews as search_gd_reviews
+)
+
+from .ESG import(
+    feed as esg_feed,
+    factor as esg_factor
+)
+
+from .USER import (
+    login
+)
+
+from .GD import (
+    reviews as gd_reviews
+)
