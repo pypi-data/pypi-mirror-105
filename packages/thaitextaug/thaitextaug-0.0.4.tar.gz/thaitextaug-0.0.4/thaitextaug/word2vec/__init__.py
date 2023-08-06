a86c856062f6b43a576d431ccc89736b7783dae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:08 +0000

    Add package thaitextaug/0.0.4/thaitextaug-0.0.4.tar.gz

---
+++ packages/thaitextaug/thaitextaug-0.0.4.tar.gz/thaitextaug-0.0.4/thaitextaug/word2vec/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Word2Vec
+"""
+
+__all__ = [
+    "Word2VecAug",
+    "BPEmbAug",
+    "Thai2fitAug"
+]
+
+from thaitextaug.word2vec.word2vec import Word2VecAug
+from thaitextaug.word2vec.BPEmb_core import BPEmbAug
+from thaitextaug.word2vec.thai2fit import Thai2fitAug
