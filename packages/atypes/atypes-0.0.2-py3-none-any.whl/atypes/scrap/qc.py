cea4e48efb0e03dea5f4b04383427a8f6adbfa5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:15 +0000

    Add package atypes/0.0.2/atypes-0.0.2-py3-none-any.whl

---
+++ packages/atypes/atypes-0.0.2-py3-none-any.whl/atypes/scrap/qc.py
@@ -0,0 +1,30 @@
+"""
+Types used in qc
+"""
+from typing import Sequence, Union, Any, Iterable, Callable
+import numpy as np
+
+Number = Union[int, float, complex, np.number]
+PlcSample = Union[Number, str]
+Sample = Number
+Waveform = Sequence[Sample]
+Plc = Sequence[PlcSample]
+Chunk = Waveform  # TODO: How to express "fixed size"
+ChkStat = Number
+ChkStatFunc = Callable[[Chunk], Number]
+Segment = Waveform  # + fixed size
+Segments = Iterable[Segment]
+
+# Consider ChunkFilterFunc (and using FilterFunc to indicated funcs used in filter(filter_func, ...)
+BoolChunkFunc = Callable[[Chunk], bool]
+
+# Series = Sequence[Union[int, float, complex, np.number, str]]
+SeriesElement = Any
+Series = Iterable[
+    SeriesElement
+]  # TODO: How to express that the type of elements should be fixed
+
+SignalInputElement = Any
+Signal = Iterable[SignalInputElement]
+
+BoolFilterOutput = Iterable[bool]
