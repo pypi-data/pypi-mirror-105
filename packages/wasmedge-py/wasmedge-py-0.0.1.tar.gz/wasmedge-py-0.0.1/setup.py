0b06c2a377114714aa315b1fc0bc196336b427e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:03 +0000

    Add package wasmedge-py/0.0.1/wasmedge-py-0.0.1.tar.gz

---
+++ packages/wasmedge-py/wasmedge-py-0.0.1.tar.gz/wasmedge-py-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='wasmedge-py',
+    version='0.0.1',
+    author='Wasmer Engineering',
+    author_email='engineering@wasmer.io',
+    description='Wasm Edge',
+    long_description='This is an empty package. We will publish new versions soon.',
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
---
+++ packages/wasmedge-py/wasmedge-py-0.0.1.tar.gz/wasmedge-py-0.0.1/wasmedge/__init__.py
@@ -0,0 +1,1 @@
+print("WasmEdge temporary package. Please ping engineering@wasmer.io for more info while we publish the final package")
