a0af97bf8f9c7f24b0a65590c33f90340b7b68e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package tf_lessons/0.1.1/tf_lessons-0.1.1-py3-none-any.whl

---
+++ packages/tf_lessons/tf_lessons-0.1.1-py3-none-any.whl/tf_lessons/__init__.py
@@ -0,0 +1,4 @@
+"""An amazing sample package!"""
+
+__version__ = "0.1.1"
+from .lesson_1 import *  # noqa
---
+++ packages/tf_lessons/tf_lessons-0.1.1-py3-none-any.whl/tf_lessons/lesson_1.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
