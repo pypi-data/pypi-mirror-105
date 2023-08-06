db8d2decbfef8a271939516aa523e9b27117302a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:34 +0000

    Add package aiomsa/0.1.0a4/aiomsa-0.1.0a4-py3-none-any.whl

---
+++ packages/aiomsa/aiomsa-0.1.0a4-py3-none-any.whl/aiomsa/exceptions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Copyright 2004-present Facebook. All Rights Reserved.
+
+from typing import Optional
+
+
+class AioMsaError(Exception):
+    """Base class for all *aiomsa* errors."""
+
+    pass
+
+
+class DuplicateRouteError(AioMsaError):
+    """Raised if more than one route has the same method and path.
+
+    Args:
+        msg: The exception message.
+    """
+
+    def __init__(self, msg: Optional[str] = None):
+        if msg is None:
+            msg = "A route with the same method and path already exists"
+        super().__init__(msg)
+
+
+class ClientError(AioMsaError):
+    """Base class for all client errors."""
+
+    pass
+
+
+class E2ClientError(ClientError):
+    """Raised if an :class:`~.E2Client` operation fails.
+
+    Args:
+        msg: The exception message.
+    """
+
+    def __init__(self, msg: Optional[str] = None):
+        if msg is None:
+            msg = "E2Client failure"
+        super().__init__(msg)
