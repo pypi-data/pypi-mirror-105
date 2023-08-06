883e492cf3ab10467c732f75a5dbc9240fc79344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:55 +0000

    Add package spectramanipulator/0.1.3/spectramanipulator-0.1.3.tar.gz

---
+++ packages/spectramanipulator/spectramanipulator-0.1.3.tar.gz/spectramanipulator-0.1.3/spectramanipulator/singleton.py
@@ -0,0 +1,18 @@
+
+
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+# as meta class
+# class Singleton(type):
+#     _instances = {}
+#
+#     def __call__(cls, *args, **kwargs):
+#         if cls not in cls._instances:
+#             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+#         return cls._instances[cls]
