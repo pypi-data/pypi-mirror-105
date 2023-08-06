803724b14d7455582ad39de583b8faba48901545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:34 +0000

    Add package fast-trace/3.1.10/fast-trace-3.1.10.tar.gz

---
+++ packages/fast-trace/fast-trace-3.1.10.tar.gz/fast-trace-3.1.10/fast_tracker/utils/functions.py
@@ -0,0 +1,42 @@
+#!/usr/local/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+    created by FAST-DEV 2021/4/9
+"""
+import time
+import sys
+import os
+
+
+def lower_case_name(text):
+    """
+    将驼峰命名转为小写下划线命名
+    :param text:
+    :return:
+    """
+    lst = []
+    for index, char in enumerate(text):
+        if char.isupper() and index != 0:
+            lst.append("_")
+        lst.append(char)
+
+    return "".join(lst).lower()
+
+
+def log(text, *args, level=1):
+    """
+    log日志标准输出
+    :param text: 日志内容（含替换符）
+    :param args: 替换符对应的数据
+    :param int level: 日志级别, 数字越高，打印信息越多，为0表示不打印日志。暂定级别有：0，1，2，3，4 可对应为logging的日志级别
+    :return:
+    """
+    startup_debug = os.environ.get("FAST_DEBUG", "off").lower() in ("on", "true", "1")
+    debug_level = int(os.environ.get('FAST_DEBUG_LEVEL', 0))
+    if startup_debug:
+        if debug_level >= level:
+            text = text % args
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            sys.stdout.write("FAST: %s (%d) - %s\n" % (timestamp, os.getpid(), text))
+            sys.stdout.flush()
