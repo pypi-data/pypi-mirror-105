b3c110c6ecec95b96e288fb2d44e6516cc5012a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:00 +0000

    Add package datetimeformat/v2.0/datetimeformat-v2.0.tar.gz

---
+++ packages/datetimeformat/datetimeformat-v2.0.tar.gz/datetimeformat-v2.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'datetimeformat',
+    packages = ['datetimeformat'],
+    version = 'v2.0',
+    description = 'Infer datetime format from datetime string or standardize datetime and timezone format',
+    author = 'Zhuang Baokun',
+    author_email = 'zhuangbaokun@gmail.com',
+    url = 'https://github.com/zhuangbaokun/datetimeformat',
+    download_url = 'https://github.com/zhuangbaokun/datetimeformat/archive/refs/tags/v2.0.tar.gz',
+    keywords = ['datetime', 'format','detect','infer','standardize datetime and timezone format'],
+    classifiers = [],
+)
