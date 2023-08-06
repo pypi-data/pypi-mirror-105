cb366486359902940a603c0ca2074f1f38df8176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:17 +0000

    Add package hitomi/0.2.1/hitomi-0.2.1.tar.gz

---
+++ packages/hitomi/hitomi-0.2.1.tar.gz/hitomi-0.2.1/hitomi/commands/contract.py
@@ -0,0 +1,23 @@
+from web3.contract import Contract
+
+
+class ContractInit:
+    def __init__(self, web3) -> None:
+        self.web3 = web3
+
+    def __call__(self, address: str, *args, **kwargs) -> Contract:
+        return self.web3.eth.contract(
+            address=self.web3.toChecksumAddress(address),
+            *args,
+            **kwargs,
+        )
+
+    def calculateAddress(self, address: str, nonce: int = None) -> str:
+        address = self.web3.toChecksumAddress(address)
+
+        if nonce is None:
+            nonce = self.web3.eth.getTransactionCount(address)
+
+        return self.web3.keccak(rlp.encode([bytes.fromhex(address[2:]), nonce]))[
+            12:
+        ].hex()
