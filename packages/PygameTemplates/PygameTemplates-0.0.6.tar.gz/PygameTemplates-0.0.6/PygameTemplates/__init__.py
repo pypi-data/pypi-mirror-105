5111e5db97096545d3ced6ba1de5e8e2cb31972b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:02 +0000

    Add package PygameTemplates/0.0.6/PygameTemplates-0.0.6.tar.gz

---
+++ packages/PygameTemplates/PygameTemplates-0.0.6.tar.gz/PygameTemplates-0.0.6/PygameTemplates/__init__.py
@@ -0,0 +1,5 @@
+from PygameTemplates import text_field_base
+from PygameTemplates import error_handling
+from PygameTemplates import button
+from PygameTemplates import label
+from PygameTemplates import text_box
