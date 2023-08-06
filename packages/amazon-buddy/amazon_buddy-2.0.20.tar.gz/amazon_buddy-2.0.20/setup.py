927eb4a33eab6d8ddfa594c5e12b54189c5ac0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:02 +0000

    Add package amazon-buddy/2.0.20/amazon_buddy-2.0.20.tar.gz

---
+++ packages/amazon-buddy/amazon_buddy-2.0.20.tar.gz/amazon_buddy-2.0.20/setup.py
@@ -0,0 +1,35 @@
+import setuptools, os
+
+readme_path = os.path.join(os.getcwd(), "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'amazon_buddy'
+
+setuptools.setup(
+    name="amazon_buddy",
+    version="2.0.20",
+    author="Kristof",
+    description="python wrapper for the amazon_buddy npm package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/py_amazon_buddy",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'beautifulsoup4>=4.9.3',
+        'jsoncodable>=0.1.7',
+        'kcu>=0.0.66',
+        'ksimpleapi>=0.0.40',
+        'noraise>=0.0.16',
+        'randomua>=0.0.7',
+        'requests>=2.25.1',
+        'Unidecode>=1.2.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
