efc2472c3705eb0ec5f4fbb1b3bd94c0c8ea9db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:57 +0000

    Add package mplhep/0.3.5/mplhep-0.3.5-py3-none-any.whl

---
+++ packages/mplhep/mplhep-0.3.5-py3-none-any.whl/mplhep/styles/__init__.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+import sys
+
+from matplotlib.pyplot import style as plt_style
+
+# Short cut to all styles
+from .alice import ALICE
+from .atlas import ATLAS, ATLASAlt, ATLASTex
+from .cms import CMS, ROOT, CMSTex, ROOTTex
+from .lhcb import LHCb, LHCb1, LHCb2, LHCbTex, LHCbTex1, LHCbTex2
+
+__all__ = (
+    "ALICE",
+    "ATLAS",
+    "ATLASAlt",
+    "ATLASTex",
+    "CMS",
+    "ROOT",
+    "CMSTex",
+    "ROOTTex",
+    "LHCb",
+    "LHCb1",
+    "LHCb2",
+    "LHCbTex",
+    "LHCbTex1",
+    "LHCbTex2",
+    "set_style",
+    "fira",
+    "firamath",
+    "fabiola",
+)
+
+
+def set_style(styles=None):
+    """
+    Set the experiment specific plotting style
+
+    Example:
+
+        >>> import mplhep as hep
+        >>> hep.set_style("ATLAS")
+        >>> hep.set_style(mplhep.style.CMS)
+
+    Parameters
+    ----------
+        styles: `str` or `mplhep.style` or `dict` None
+            The experiment style. Will understand a dictionary
+            of rcParams, a mplhep style or its string alias.
+            Pass ``None`` to reset to mpl defaults.
+    """
+
+    if styles is None:
+        return plt_style.use("default")
+    if not isinstance(styles, list):
+        styles = [styles]
+
+    # passed in experiment mplhep.style dict or str alias
+    styles = [
+        style if isinstance(style, dict) else getattr(sys.modules[__name__], f"{style}")
+        for style in styles
+    ]
+
+    plt_style.use(styles)
+
+
+fira = {"font.sans-serif": "Fira Sans"}
+
+firamath = {
+    "mathtext.fontset": "custom",
+    "mathtext.rm": "Fira Math:regular",
+    "mathtext.bf": "Fira Math:medium",
+    "mathtext.sf": "Fira Math",
+    "mathtext.it": "Fira Math:regular:italic",
+    "mathtext.tt": "Fira Mono",
+}
+
+fabiola = {
+    "font.sans-serif": "Comic Sans MS",
+}
