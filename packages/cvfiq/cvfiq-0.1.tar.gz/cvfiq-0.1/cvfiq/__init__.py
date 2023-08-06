d516e0149f439c844cb2e2090318f9467403232d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:00 +0000

    Add package cvfiq/0.1/cvfiq-0.1.tar.gz

---
+++ packages/cvfiq/cvfiq-0.1.tar.gz/cvfiq-0.1/cvfiq/__init__.py
@@ -0,0 +1,6 @@
+from cvfiq.HandTrackingModule import HandDetector
+from cvfiq.FaceMeshModule import FaceMeshDetector
+from cvfiq.FaceDetectionModule import FaceDetector
+from cvfiq.PoseModule import PoseDetector
+from cvfiq.Utils import stackImages, cornerRect
+from cvfiq.FPS import FPS
