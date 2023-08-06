a4ed500a0b0ec99a2859b6fa91c8ad29e343daf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:59 +0000

    Add package mypy-boto3-application-insights/1.17.70.post2/mypy-boto3-application-insights-1.17.70.post2.tar.gz

---
+++ packages/mypy-boto3-application-insights/mypy-boto3-application-insights-1.17.70.post2.tar.gz/mypy-boto3-application-insights-1.17.70.post2/mypy_boto3_application_insights/literals.py
@@ -0,0 +1,60 @@
+"""
+Type annotations for application-insights service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_application_insights.literals import CloudWatchEventSource
+
+    data: CloudWatchEventSource = "CODE_DEPLOY"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "CloudWatchEventSource",
+    "ConfigurationEventResourceType",
+    "ConfigurationEventStatus",
+    "FeedbackKey",
+    "FeedbackValue",
+    "LogFilter",
+    "OsType",
+    "SeverityLevel",
+    "Status",
+    "Tier",
+)
+
+
+CloudWatchEventSource = Literal["CODE_DEPLOY", "EC2", "HEALTH", "RDS"]
+ConfigurationEventResourceType = Literal[
+    "CLOUDFORMATION", "CLOUDWATCH_ALARM", "CLOUDWATCH_LOG", "SSM_ASSOCIATION"
+]
+ConfigurationEventStatus = Literal["ERROR", "INFO", "WARN"]
+FeedbackKey = Literal["INSIGHTS_FEEDBACK"]
+FeedbackValue = Literal["NOT_SPECIFIED", "NOT_USEFUL", "USEFUL"]
+LogFilter = Literal["ERROR", "INFO", "WARN"]
+OsType = Literal["LINUX", "WINDOWS"]
+SeverityLevel = Literal["High", "Low", "Medium"]
+Status = Literal["IGNORE", "PENDING", "RESOLVED"]
+Tier = Literal[
+    "CUSTOM",
+    "DEFAULT",
+    "DOT_NET_CORE",
+    "DOT_NET_WEB",
+    "DOT_NET_WEB_TIER",
+    "DOT_NET_WORKER",
+    "JAVA_JMX",
+    "MYSQL",
+    "ORACLE",
+    "POSTGRESQL",
+    "SQL_SERVER",
+    "SQL_SERVER_ALWAYSON_AVAILABILITY_GROUP",
+]
