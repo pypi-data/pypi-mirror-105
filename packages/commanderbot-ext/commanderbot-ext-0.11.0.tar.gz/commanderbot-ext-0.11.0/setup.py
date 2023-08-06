b2e06760a9ae81d310fdf492c56907d2ebe9f095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:27 +0000

    Add package commanderbot-ext/0.11.0/commanderbot-ext-0.11.0.tar.gz

---
+++ packages/commanderbot-ext/commanderbot-ext-0.11.0.tar.gz/commanderbot-ext-0.11.0/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['commanderbot_ext',
+ 'commanderbot_ext.ext',
+ 'commanderbot_ext.ext.faq',
+ 'commanderbot_ext.ext.help_chat',
+ 'commanderbot_ext.ext.help_chat.sql_store',
+ 'commanderbot_ext.ext.invite',
+ 'commanderbot_ext.ext.jira',
+ 'commanderbot_ext.ext.kick',
+ 'commanderbot_ext.ext.pack',
+ 'commanderbot_ext.ext.ping',
+ 'commanderbot_ext.ext.poster_board',
+ 'commanderbot_ext.ext.quote',
+ 'commanderbot_ext.ext.roles',
+ 'commanderbot_ext.ext.status',
+ 'commanderbot_ext.ext.vote',
+ 'commanderbot_ext.lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy>=1.4.9,<2.0.0',
+ 'aiohttp>=3.7.4,<4.0.0',
+ 'aiosqlite>=0.17.0,<0.18.0',
+ 'beet>=0.23.0',
+ 'commanderbot>=0.7.0,<0.8.0',
+ 'discord.py>=1.7.1,<2.0.0',
+ 'lectern>=0.13.0']
+
+setup_kwargs = {
+    'name': 'commanderbot-ext',
+    'version': '0.11.0',
+    'description': 'A collection of cogs and extensions for discord.py bots.',
+    'long_description': '# CommanderBot Ext\n\nA collection of cogs and extensions for discord.py bots.\n\n[![package-badge]](https://pypi.python.org/pypi/commanderbot-ext/)\n[![version-badge]](https://pypi.python.org/pypi/commanderbot-ext/)\n\n[package-badge]: https://img.shields.io/pypi/v/commanderbot-ext.svg\n[version-badge]: https://img.shields.io/pypi/pyversions/commanderbot-ext.svg\n',
+    'author': 'Arcensoth',
+    'author_email': 'arcensoth@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/CommanderBot-Dev/commanderbot-ext',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
