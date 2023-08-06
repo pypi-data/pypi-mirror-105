03eaf1a075f48dcea34156c89208e9746c7ce4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:51 +0000

    Add package torch-gists/0.0.0.2/torch_gists-0.0.0.2-py3-none-any.whl

---
+++ packages/torch-gists/torch_gists-0.0.0.2-py3-none-any.whl/torch_gists/models/__init__.py
@@ -0,0 +1,3 @@
+from torch_gists.models.resnet import ResNet18, ResNet34, ResNet50, ResNet152, ResNet101
+from torch_gists.models.mobilenetv2 import MobileNetV2
+from torch_gists.models.vgg import VGG
