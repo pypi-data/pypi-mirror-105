64f37c062ef29b06a9f9ed034d3dfef7d6954fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:13 +0000

    Add package my-test-pkg-tnfru/0.0.3/my-test-pkg-tnfru-0.0.3.tar.gz

---
+++ packages/my-test-pkg-tnfru/my-test-pkg-tnfru-0.0.3.tar.gz/my-test-pkg-tnfru-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="my-test-pkg-tnfru",
+    version="0.0.3",
+    author="Lars Mueller",
+    author_email="lars.lmueller@gamil.com",
+    description="Brief test pkg",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+
+    install_requires=[
+        'numpy~=1.19.2',
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
+
