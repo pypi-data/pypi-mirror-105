3298de2c6f6b1ac58ff97b4c4a10a24b95150268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:14 +0000

    Add package SaltIching/0.4/SaltIching-0.4.tar.gz

---
+++ packages/SaltIching/SaltIching-0.4.tar.gz/SaltIching-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='SaltIching',
+      version='0.4',
+      description='Iching for python',
+      url='https://github.com/royalsaltmerchant/salt-iching',
+      author='Royal Salt Merchant',
+      author_email='jumpingafterrain@gmail.com',
+      license='MIT',
+      packages=['iching'],
+      zip_safe=False,
+      include_package_data=True),
+      
