8f0cc6ec4d088527fded16e769b3fe3967fe3bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:04 +0000

    Add package sparkvis/0.1.0/sparkvis-0.1.0.tar.gz

---
+++ packages/sparkvis/sparkvis-0.1.0.tar.gz/sparkvis-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sparkvis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sparklines>=0.4.2,<0.5.0']
+
+setup_kwargs = {
+    'name': 'sparkvis',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Shawn Presser',
+    'author_email': 'shawnpresser@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
