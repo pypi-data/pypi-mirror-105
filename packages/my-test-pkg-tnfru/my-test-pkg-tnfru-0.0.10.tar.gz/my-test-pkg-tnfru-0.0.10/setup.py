4ec119080dc4e35a984ab4d0640934324131d4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:35 +0000

    Add package my-test-pkg-tnfru/0.0.10/my-test-pkg-tnfru-0.0.10.tar.gz

---
+++ packages/my-test-pkg-tnfru/my-test-pkg-tnfru-0.0.10.tar.gz/my-test-pkg-tnfru-0.0.10/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="my-test-pkg-tnfru",
+    version="0.0.10",
+    author="Lars Mueller",
+    author_email="lars.lmueller@gamil.com",
+    description="Brief test pkg",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+
+    install_requires=[
+        'tensorflow>=2.4.0',
+        'ray[tune]',
+        'matplotlib',
+        'wandb',
+        'focal_loss',
+        'pillow'
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
+
