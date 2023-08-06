b3355c55b37b84dd7cdd8ea2d413e40cb7dc7094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:18 +0000

    Add package vihanpdf/1.1/vihanpdf-1.1.tar.gz

---
+++ packages/vihanpdf/vihanpdf-1.1.tar.gz/vihanpdf-1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+import setuptools
+from pathlib import Path
+
+
+
+setuptools.setup(
+    name = "vihanpdf",
+    version = 1.1,
+    long_description = Path("README.md").read_text(),
+    packages = setuptools.find_packages(exclude=["tests", "data"])
+)
