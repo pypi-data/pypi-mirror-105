df0266ef97377b15bbac73809ad3ef42de6b5914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:13 +0000

    Add package chia-plot-manager/1.0.1/chia_plot_manager-1.0.1-py3-none-any.whl

---
+++ packages/chia-plot-manager/chia_plot_manager-1.0.1-py3-none-any.whl/plotmanager/library/commands/plots.py
@@ -0,0 +1,27 @@
+def create(size, memory_buffer, temporary_directory, destination_directory, threads, buckets, bitfield,
+           chia_location='chia', temporary2_directory=None, farmer_public_key=None, pool_public_key=None):
+    flags = dict(
+        k=size,
+        b=memory_buffer,
+        t=temporary_directory,
+        d=destination_directory,
+        r=threads,
+        u=buckets,
+    )
+    if temporary2_directory is not None:
+        flags['2'] = temporary2_directory
+    if farmer_public_key is not None:
+        flags['f'] = farmer_public_key
+    if pool_public_key is not None:
+        flags['p'] = pool_public_key
+    if bitfield is False:
+        flags['e'] = ''
+
+    data = [chia_location, 'plots', 'create']
+    for key, value in flags.items():
+        flag = f'-{key}'
+        data.append(flag)
+        if value == '':
+            continue
+        data.append(str(value))
+    return data
