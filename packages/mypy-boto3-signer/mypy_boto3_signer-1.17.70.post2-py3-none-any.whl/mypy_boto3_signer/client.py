2b804400818df20216b4a858a1028619b4b442eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:23 +0000

    Add package mypy-boto3-signer/1.17.70.post2/mypy_boto3_signer-1.17.70.post2-py3-none-any.whl

---
+++ packages/mypy-boto3-signer/mypy_boto3_signer-1.17.70.post2-py3-none-any.whl/mypy_boto3_signer/client.py
@@ -0,0 +1,295 @@
+"""
+Type annotations for signer service client.
+
+[Open documentation](./client.md)
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_signer import SignerClient
+
+    client: SignerClient = boto3.client("signer")
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Any, Dict, List, Type, overload
+
+from botocore.client import ClientMeta
+
+from mypy_boto3_signer.paginator import (
+    ListSigningJobsPaginator,
+    ListSigningPlatformsPaginator,
+    ListSigningProfilesPaginator,
+)
+from mypy_boto3_signer.waiter import SuccessfulSigningJobWaiter
+
+from .literals import SigningProfileStatus, SigningStatus
+from .type_defs import (
+    AddProfilePermissionResponseTypeDef,
+    DescribeSigningJobResponseTypeDef,
+    DestinationTypeDef,
+    GetSigningPlatformResponseTypeDef,
+    GetSigningProfileResponseTypeDef,
+    ListProfilePermissionsResponseTypeDef,
+    ListSigningJobsResponseTypeDef,
+    ListSigningPlatformsResponseTypeDef,
+    ListSigningProfilesResponseTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    PutSigningProfileResponseTypeDef,
+    RemoveProfilePermissionResponseTypeDef,
+    SignatureValidityPeriodTypeDef,
+    SigningMaterialTypeDef,
+    SigningPlatformOverridesTypeDef,
+    SourceTypeDef,
+    StartSigningJobResponseTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("SignerClient",)
+
+
+class BotocoreClientError(BaseException):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Dict[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AccessDeniedException: Type[BotocoreClientError]
+    BadRequestException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServiceErrorException: Type[BotocoreClientError]
+    NotFoundException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceLimitExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    TooManyRequestsException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+
+class SignerClient:
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client)
+    [Show boto3-stubs documentation](./client.md)
+    """
+
+    meta: ClientMeta
+    exceptions: Exceptions
+
+    def add_profile_permission(
+        self,
+        profileName: str,
+        action: str,
+        principal: str,
+        statementId: str,
+        profileVersion: str = None,
+        revisionId: str = None,
+    ) -> AddProfilePermissionResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.add_profile_permission)
+        [Show boto3-stubs documentation](./client.md#add-profile-permission)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.can_paginate)
+        [Show boto3-stubs documentation](./client.md#can-paginate)
+        """
+
+    def cancel_signing_profile(self, profileName: str) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.cancel_signing_profile)
+        [Show boto3-stubs documentation](./client.md#cancel-signing-profile)
+        """
+
+    def describe_signing_job(self, jobId: str) -> DescribeSigningJobResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.describe_signing_job)
+        [Show boto3-stubs documentation](./client.md#describe-signing-job)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = None,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](./client.md#generate-presigned-url)
+        """
+
+    def get_signing_platform(self, platformId: str) -> GetSigningPlatformResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.get_signing_platform)
+        [Show boto3-stubs documentation](./client.md#get-signing-platform)
+        """
+
+    def get_signing_profile(
+        self, profileName: str, profileOwner: str = None
+    ) -> GetSigningProfileResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.get_signing_profile)
+        [Show boto3-stubs documentation](./client.md#get-signing-profile)
+        """
+
+    def list_profile_permissions(
+        self, profileName: str, nextToken: str = None
+    ) -> ListProfilePermissionsResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.list_profile_permissions)
+        [Show boto3-stubs documentation](./client.md#list-profile-permissions)
+        """
+
+    def list_signing_jobs(
+        self,
+        status: SigningStatus = None,
+        platformId: str = None,
+        requestedBy: str = None,
+        maxResults: int = None,
+        nextToken: str = None,
+        isRevoked: bool = None,
+        signatureExpiresBefore: datetime = None,
+        signatureExpiresAfter: datetime = None,
+        jobInvoker: str = None,
+    ) -> ListSigningJobsResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.list_signing_jobs)
+        [Show boto3-stubs documentation](./client.md#list-signing-jobs)
+        """
+
+    def list_signing_platforms(
+        self,
+        category: str = None,
+        partner: str = None,
+        target: str = None,
+        maxResults: int = None,
+        nextToken: str = None,
+    ) -> ListSigningPlatformsResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.list_signing_platforms)
+        [Show boto3-stubs documentation](./client.md#list-signing-platforms)
+        """
+
+    def list_signing_profiles(
+        self,
+        includeCanceled: bool = None,
+        maxResults: int = None,
+        nextToken: str = None,
+        platformId: str = None,
+        statuses: List[SigningProfileStatus] = None,
+    ) -> ListSigningProfilesResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.list_signing_profiles)
+        [Show boto3-stubs documentation](./client.md#list-signing-profiles)
+        """
+
+    def list_tags_for_resource(self, resourceArn: str) -> ListTagsForResourceResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.list_tags_for_resource)
+        [Show boto3-stubs documentation](./client.md#list-tags-for-resource)
+        """
+
+    def put_signing_profile(
+        self,
+        profileName: str,
+        platformId: str,
+        signingMaterial: "SigningMaterialTypeDef" = None,
+        signatureValidityPeriod: "SignatureValidityPeriodTypeDef" = None,
+        overrides: "SigningPlatformOverridesTypeDef" = None,
+        signingParameters: Dict[str, str] = None,
+        tags: Dict[str, str] = None,
+    ) -> PutSigningProfileResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.put_signing_profile)
+        [Show boto3-stubs documentation](./client.md#put-signing-profile)
+        """
+
+    def remove_profile_permission(
+        self, profileName: str, revisionId: str, statementId: str
+    ) -> RemoveProfilePermissionResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.remove_profile_permission)
+        [Show boto3-stubs documentation](./client.md#remove-profile-permission)
+        """
+
+    def revoke_signature(self, jobId: str, reason: str, jobOwner: str = None) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.revoke_signature)
+        [Show boto3-stubs documentation](./client.md#revoke-signature)
+        """
+
+    def revoke_signing_profile(
+        self, profileName: str, profileVersion: str, reason: str, effectiveTime: datetime
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.revoke_signing_profile)
+        [Show boto3-stubs documentation](./client.md#revoke-signing-profile)
+        """
+
+    def start_signing_job(
+        self,
+        source: "SourceTypeDef",
+        destination: DestinationTypeDef,
+        profileName: str,
+        clientRequestToken: str,
+        profileOwner: str = None,
+    ) -> StartSigningJobResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.start_signing_job)
+        [Show boto3-stubs documentation](./client.md#start-signing-job)
+        """
+
+    def tag_resource(self, resourceArn: str, tags: Dict[str, str]) -> Dict[str, Any]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.tag_resource)
+        [Show boto3-stubs documentation](./client.md#tag-resource)
+        """
+
+    def untag_resource(self, resourceArn: str, tagKeys: List[str]) -> Dict[str, Any]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Client.untag_resource)
+        [Show boto3-stubs documentation](./client.md#untag-resource)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_signing_jobs"]
+    ) -> ListSigningJobsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Paginator.ListSigningJobs)[Show boto3-stubs documentation](./paginators.md#listsigningjobspaginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_signing_platforms"]
+    ) -> ListSigningPlatformsPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Paginator.ListSigningPlatforms)[Show boto3-stubs documentation](./paginators.md#listsigningplatformspaginator)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_signing_profiles"]
+    ) -> ListSigningProfilesPaginator:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Paginator.ListSigningProfiles)[Show boto3-stubs documentation](./paginators.md#listsigningprofilespaginator)
+        """
+
+    def get_waiter(
+        self, waiter_name: Literal["successful_signing_job"]
+    ) -> SuccessfulSigningJobWaiter:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/signer.html#Signer.Waiter.successful_signing_job)[Show boto3-stubs documentation](./waiters.md#successfulsigningjobwaiter)
+        """
