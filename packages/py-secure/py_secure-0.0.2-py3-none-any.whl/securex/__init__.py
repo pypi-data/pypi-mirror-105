a621a1302eb1bc5f8f75a35aacc2322ab4201471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:17 +0000

    Add package py-secure/0.0.2/py_secure-0.0.2-py3-none-any.whl

---
+++ packages/py-secure/py_secure-0.0.2-py3-none-any.whl/securex/__init__.py
@@ -0,0 +1,27 @@
+class top():
+    _alphabet = r"abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_+-=,./;'[]<>?:\"{}|₹"
+
+def en(Text):
+    encrypted = ''
+    for i in Text :
+        encrypted += top._alphabet[int((top._alphabet.find(i) + len(Text)) % len(top._alphabet))]
+    return encrypted
+
+def de(Text):
+    decrypted = ''
+    for i in Text :
+        decrypted +=top._alphabet[int((top._alphabet.find(i) - len(Text)) % len(top._alphabet))]
+    return decrypted
+    
+def ef(text, file):
+  f = open(file, "w")
+  f.write(en(text))
+  f.close
+  return True
+
+def df(text, file):
+  f = open(file, "r")
+  k = f.read()
+  x = de(k)
+  f.close()
+  return x
