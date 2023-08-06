76c157c1b53d60d006ef48cc6547125adf9db154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:37 +0000

    Add package coderfly_scraper/0.1.14/coderfly_scraper-0.1.14.tar.gz

---
+++ packages/coderfly_scraper/coderfly_scraper-0.1.14.tar.gz/coderfly_scraper-0.1.14/src/scraper/scheduler/rabbitmq/defaults.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+"""
+@desc: 
+"""
+
+RABBITMQ_CONNECTION_PARAMETERS = "amqp://guest:guest@localhost:5672/"
+SCHEDULER_MAX_IDLE_TIME = 30  # 最大闲置时间
+SPIDER_PARSE_FAILD_MAX_TIMES = 60  # 允许解析出错的最大次数
