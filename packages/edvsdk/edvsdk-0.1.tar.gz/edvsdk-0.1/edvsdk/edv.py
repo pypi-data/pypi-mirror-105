5ac9d129884a9cb77fbd8baa40de70e04284155b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:51 +0000

    Add package edvsdk/0.1/edvsdk-0.1.tar.gz

---
+++ packages/edvsdk/edvsdk-0.1.tar.gz/edvsdk-0.1/edvsdk/edv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： hj
+# datetime： 2021/5/13 0013 下午 2:18 
+# ide： PyCharm2020.1.3
+
+def fun1():
+    print("这是函数1")
+
+def fun2():
+    print("这是函数2")
---
+++ packages/edvsdk/edvsdk-0.1.tar.gz/edvsdk-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： hj
+# datetime： 2021/5/13 0013 下午 2:18 
+# ide： PyCharm2020.1.3
+
+import setuptools
+setuptools.setup(
+    name="edvsdk",
+    version="0.1",
+    author="hj",
+    author_email="1599712264@qq.com",
+    description="edv sdk",
+    packages=setuptools.find_packages()
+)
