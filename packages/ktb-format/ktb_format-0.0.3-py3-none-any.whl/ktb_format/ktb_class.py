28ad951c951f39f819a47e35ed2c6ad9895c6d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:18 +0000

    Add package ktb-format/0.0.3/ktb_format-0.0.3-py3-none-any.whl

---
+++ packages/ktb-format/ktb_format-0.0.3-py3-none-any.whl/ktb_format/ktb_class.py
@@ -0,0 +1,9 @@
+class ktb:
+    readme_size = 0
+    readme_content = ""
+    variable_count = 0
+    variable_type_array = []
+    variable_offset_array = []
+    variable_count_array = []
+    variable_data_array = []
+    error_check_flag = 'K'
