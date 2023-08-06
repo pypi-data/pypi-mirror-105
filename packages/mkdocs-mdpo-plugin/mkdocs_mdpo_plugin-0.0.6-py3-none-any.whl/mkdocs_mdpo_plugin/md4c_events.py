2046af73876cd5689f868179b85bda34819c89c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:24 +0000

    Add package mkdocs-mdpo-plugin/0.0.6/mkdocs_mdpo_plugin-0.0.6-py3-none-any.whl

---
+++ packages/mkdocs-mdpo-plugin/mkdocs_mdpo_plugin-0.0.6-py3-none-any.whl/mkdocs_mdpo_plugin/md4c_events.py
@@ -0,0 +1,81 @@
+"""Md4C parser preprocessing events for md2po instance."""
+
+import re
+
+
+try:
+    from markdown.extensions.admonition import AdmonitionProcessor
+except ImportError:  # pragma: no cover
+    pass
+try:
+    from pymdownx.details import DetailsProcessor
+except ImportError:  # pragma: no cover
+    pass
+try:
+    from pymdownx.snippets import SnippetPreprocessor
+except ImportError:  # pragma: no cover
+    pass
+try:
+    from pymdownx.tabbed import TabbedProcessor
+except ImportError:  # pragma: no cover
+    pass
+
+
+EVENT_EXTENSIONS = {
+    'text': [
+        'admonition',
+        'def_list',
+        'pymdownx.details',
+        'pymdownx.snippets',
+        'pymdownx.tabbed',
+    ],
+    'msgid': [
+        'def_list',
+    ],
+}
+
+
+def build_md4c_parser_events(mkdocs_build_config):
+    md_extensions = mkdocs_build_config['markdown_extensions']
+
+    def text_event(md2po_instance, block, text):
+        if 'admonition' in md_extensions:
+            if re.match(AdmonitionProcessor.RE, text):
+                md2po_instance.disabled_entries.append(text)
+                return False
+        if 'pymdownx.details' in md_extensions:
+            if re.match(DetailsProcessor.START, text):
+                md2po_instance.disabled_entries.append(text)
+                return False
+        if 'pymdownx.snippets' in md_extensions:
+            if re.match(SnippetPreprocessor.RE_ALL_SNIPPETS, text):
+                md2po_instance.disabled_entries.append(text)
+                return False
+        if 'pymdownx.tabbed' in md_extensions:
+            if re.match(TabbedProcessor.START, text):
+                md2po_instance.disabled_entries.append(text)
+                return False
+
+    def msgid_event(md2po_instance, msgid):
+        if 'def_list' in md_extensions:
+            if msgid.startswith(': '):
+                md2po_instance._disable_next_line = True
+        if 'footnotes' in md_extensions:
+            # footnotes are parsed like link references
+            if msgid.startswith('[^'):
+                md2po_instance._disable_next_line = True
+
+    # load only those events required for the extensions
+    events_functions = {
+        'text': text_event,
+        'msgid': msgid_event,
+    }
+
+    events = {}
+    for event_name, extensions in EVENT_EXTENSIONS.items():
+        for extension in extensions:
+            if extension in md_extensions:
+                events[event_name] = events_functions[event_name]
+                break
+
+    return events
