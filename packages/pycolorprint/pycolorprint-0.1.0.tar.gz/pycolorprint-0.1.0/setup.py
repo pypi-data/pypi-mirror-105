fb646e3798504100f06354d9696acab5748915ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:06 +0000

    Add package pycolorprint/0.1.0/pycolorprint-0.1.0.tar.gz

---
+++ packages/pycolorprint/pycolorprint-0.1.0.tar.gz/pycolorprint-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0' 
+DESCRIPTION = 'Colorful pretty printer for python!'
+LONG_DESCRIPTION = 'A python pretty printer that uses ANSI characters to make terminal output colorful.\n\n(5/10/2021) cprint_iter now returns the ANSI colored string that is printed to console.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="pycolorprint",
+        version=VERSION,
+        author="Alexander Steffen",
+        author_email="alexsteffen55@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package.
+        
+        keywords=['python', 'pretty printer', 'print', 'colorful', 'color', 'color printer', 'color print'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
+
+# cmds:
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
