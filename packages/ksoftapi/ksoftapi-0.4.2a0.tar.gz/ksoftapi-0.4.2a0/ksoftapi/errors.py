74c608e536e0fe8514beffc20d4604b555a718f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:49 +0000

    Add package ksoftapi/0.4.2a0/ksoftapi-0.4.2a0.tar.gz

---
+++ packages/ksoftapi/ksoftapi-0.4.2a0.tar.gz/ksoftapi-0.4.2a0/ksoftapi/errors.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+class BadRequest(Exception):
+    pass
+
+
+class NoResults(Exception):
+    pass
+
+
+class Forbidden(Exception):
+    pass
+
+
+class APIError(Exception):
+    def __init__(self, code: int, message: str):
+        super().__init__(message)
+        self.code: int = code
+        self.message: str = message
