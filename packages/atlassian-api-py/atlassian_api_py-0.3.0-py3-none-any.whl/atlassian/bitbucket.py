7e9babc0d0f124473cef6866dea604250aff9f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:08 +0000

    Add package atlassian-api-py/0.3.0/atlassian_api_py-0.3.0-py3-none-any.whl

---
+++ packages/atlassian-api-py/atlassian_api_py-0.3.0-py3-none-any.whl/atlassian/bitbucket.py
@@ -0,0 +1,221 @@
+import re
+from atlassian.client import AtlassianAPI
+from atlassian.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class Bitbucket(AtlassianAPI):
+
+    def _get_paged(self, url, params):
+        response = self.get(url, params=params)
+        if response.values:
+            values = response.values
+        else:
+            return []
+        limit = params.get("limit")
+        while not response.isLastPage:
+            if limit is not None:
+                params["limit"] = limit - len(values)
+                if params["limit"] < 0:
+                    break
+            params["start"] = response.nextPageStart
+            response = self.get(url, params=params)
+            values += response.values or {}
+        return values
+
+    def get_project_repo(self, project_key, start=0, limit=None):
+        url = '/rest/api/latest/projects/{0}/repos/'.format(project_key)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit is not None:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
+
+    def get_project_repo_name(self, project_key):
+        values = self.get_project_repo(project_key)
+        repo_name = []
+        for value in values:
+            repo_name.append(value.name)
+        return repo_name
+
+    def get_repo_info(self, project_key, repo_key):
+        url = '/rest/api/latest/projects/{0}/repos/{1}'.format(project_key, repo_key)
+        return self.get(url)
+
+    def get_repo_branch(self, project_key, repo_key, start=0, limit=None):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/branches'.format(project_key, repo_key)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit is not None:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
+
+    def get_repo_branch_names(self, project_key, repo_key):
+        values = self.get_repo_branch(project_key, repo_key)
+        branch_name = []
+        for value in values:
+            branch_name.append(value.displayId)
+        return branch_name
+
+    def get_branch_latest_commit(self, project_key, repo_key, branch_name):
+        branches = self.get_repo_branch(project_key, repo_key)
+        for branch in branches:
+            if branch_name == branch.displayId:
+                return branch.latestCommit
+
+    def create_branch(self, project_key, repo_key, branch_name, start_point):
+        """Using service account to create branch failed because of
+        this issue https://jira.atlassian.com/browse/BSERV-9340"""
+        url = '/rest/branch-utils/1.0/projects/{0}/repos/{1}/branches'.format(project_key, repo_key)
+        json = {"name": branch_name, "startPoint": start_point}
+        return self.post(url, json=json)
+
+    def delete_branch(self, project_key, repo_key, branch_name, end_point):
+        url = '/rest/branch-utils/latest/projects/{0}/repos/{1}/branches'.format(project_key, repo_key)
+        json = {"name": branch_name, "endPoint": end_point}
+        return self.delete(url, json=json)
+
+    def get_merged_branch(self, project_key, repo_key, start=0, limit=None):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/branches?base=refs/heads/master&details=true'.\
+            format(project_key, repo_key)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit is not None:
+            params['limit'] = limit
+        branches = self._get_paged(url, params=params)
+
+        merged_branch = []
+        for branch in branches:
+            pull_request = [x for x in dir(branch.metadata) if x.endswith('outgoing-pull-request-metadata')]
+            if pull_request:
+                d_metadata = branch.metadata.__dict__
+                metadata = d_metadata['com.atlassian.bitbucket.server.bitbucket-ref-metadata:outgoing-pull-request-metadata']
+                try:
+                    state = metadata.pullRequest.state
+                except AttributeError:
+                    state = None
+                try:
+                    merged = metadata.merged
+                except AttributeError:
+                    merged = None
+                if state == 'MERGED' or merged:
+                    merged_branch.append(branch.displayId)
+        return merged_branch
+
+    def get_branch_commits(self, project_key, repo_key, branch_name, start=0, limit=None):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/commits/?until={2}'.format(project_key, repo_key, branch_name)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit is not None:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
+
+    def get_branch_committer_info(self, project_key, repo_key, branch_name, start=0, limit=None):
+        commits = self.get_branch_commits(project_key, repo_key, branch_name, start=start, limit=limit)
+        committer = []
+        for commit in commits:
+            committer.append(commit.committer)
+        return committer
+
+    def get_pull_request(self, project_key, repo_key, pr_state="ALL", start=0, limit=None):
+        """
+        :param project_key:
+        :param repo_key:
+        :param pr_state: ALL, OPEN, MERGED, DECLINED
+        :param start:
+        :param limit:
+        :return:
+        """
+        url = '/rest/api/latest/projects/{0}/repos/{1}/pull-requests?state={2}'.format(project_key, repo_key, pr_state)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit is not None:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
+
+    def get_pull_request_source_branch_name(self, project_key, repo_key, pr_id):
+        index = 0
+        while True:
+            index += 25
+            prs = self.get_pull_request(project_key, repo_key, limit=index)
+            for pr in prs:
+                if pr.id == int(pr_id):
+                    return pr.fromRef.displayId
+            return None
+
+    def get_pull_request_destination_branch_name(self, project_key, repo_key, pr_id):
+        index = 0
+        while True:
+            index += 25
+            prs = self.get_pull_request(project_key, repo_key, limit=index)
+            for pr in prs:
+                if pr.id == int(pr_id):
+                    return pr.toRef.displayId
+            return None
+
+    def get_pull_request_relate_jira_key(self, project_key, repo_key, pr_id):
+        source_branch_name = self.get_pull_request_source_branch_name(project_key, repo_key, pr_id)
+        try:
+            jira_key = re.search(r'[A-Z]+-[0-9]+', source_branch_name).group()
+            return jira_key
+        except AttributeError as e:
+            logger.error(e)
+
+    def get_open_pull_request_id(self, project_key, repo_key, pr_state="OPEN", start=0, limit=None):
+        prs = self.get_pull_request(project_key, repo_key, pr_state=pr_state, start=start, limit=limit)
+        pr_id = []
+        for pr in prs:
+            pr_id.append(pr.id)
+        return pr_id
+
+    def get_pull_request_diff(self, project_key, repo_key, pr_id):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/pull-requests/{2}/diff'.format(project_key, repo_key, pr_id)
+        return self.get(url)
+
+    def get_pull_request_comments(self, project_key, repo_slug, pr_id, start=0, limit=None):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/pull-requests/{2}/activities'.format(
+            project_key, repo_slug, pr_id)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit is not None:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
+
+    def add_comment_to_pull_request(self, project_key, repo_slug, pr_id, comment):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/pull-requests/{2}/' \
+              'comments?diffType=EFFECTIVE&markup=true&avatarSize=64'.format(project_key, repo_slug, pr_id)
+        json = {"text": comment}
+        return self.post(url, json=json)
+
+    # TODO not work
+    def remove_comment_from_pull_request(self, project_key, repo_slug, pr_id, comment):
+        comment_values = self.get_pull_request_comments(project_key, repo_slug, pr_id)
+        commit_id = None
+        for comment_value in comment_values:
+            try:
+                if comment == comment_value.comment.text:
+                    commit_id = comment_value.comment.id
+                    break
+            except AttributeError:
+                pass
+        if commit_id:
+            url = '/rest/api/latest/projects/{0}/repos/{1}/pull-requests/{2}/comments/{3}?VERSION'.format(
+                project_key, repo_slug, pr_id, commit_id)
+            return self.delete(url) or {}
+
+    def get_file_change_history(self, project_key, repo_key, branch_name, file_path, start=0, limit=None):
+        url = '/rest/api/latest/projects/{0}/repos/{1}/commits?followRenames=true&path={2}&' \
+              'until=refs%2Fheads%2F{3}&start=0&avatarSize=32'.format(project_key, repo_key, file_path, branch_name)
+        params = {}
+        if start:
+            params["start"] = start
+        if limit is not None:
+            params['limit'] = limit
+        return self._get_paged(url, params=params)
