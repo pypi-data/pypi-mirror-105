df07f098494b6221f0ec4339bf91a4fe41cfb7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:33 +0000

    Add package awyes/1.0.38/awyes-1.0.38-py3-none-any.whl

---
+++ packages/awyes/awyes-1.0.38-py3-none-any.whl/awyes/image.py
@@ -0,0 +1,18 @@
+from pprint import pprint
+from os.path import normpath, join
+
+
+def deploy_images(self):
+    for dockerfile in self.images:
+        self.__class__.docker_client.build(
+            decode=True,
+            tag="",
+            path=self.root_path,
+            dockerfile=normpath(join(
+                self.root_path,
+                self.config_path,
+                dockerfile
+            ))
+        )
+
+    pprint(self.__class__.docker_client.images())
