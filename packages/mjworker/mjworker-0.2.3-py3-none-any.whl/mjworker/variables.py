a94f09043f1aeb08e6768048136374b5d83f5182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:46 +0000

    Add package mjworker/0.2.3/mjworker-0.2.3-py3-none-any.whl

---
+++ packages/mjworker/mjworker-0.2.3-py3-none-any.whl/mjworker/variables.py
@@ -0,0 +1,12 @@
+# 参数名称基本定义
+work_id = 'work_id'
+master_server = 'master_server'
+work_tag = 'work_tag'
+work_timeout = 'work_timeout'
+
+task_pidfile = '/tmp/task.pid'
+publish_pidfile = '/tmp/publish.pid'
+pid_files = [task_pidfile, publish_pidfile]
+sync_pidfile = '/tmp/sync.pid'
+
+task_dic = {}
