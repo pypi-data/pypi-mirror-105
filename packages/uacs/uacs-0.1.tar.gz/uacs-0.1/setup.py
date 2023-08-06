ba72a0e23a76e3db0ef47be0372fad47335c0f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:57 +0000

    Add package uacs/0.1/uacs-0.1.tar.gz

---
+++ packages/uacs/uacs-0.1.tar.gz/uacs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='uacs',
+    version='0.1',
+    url='https://github.com/FLAFLALEBG/Ultimate_Auto_Check_Services',
+    license='GNU General Public License v2 (GPLv2) (GPL-2.0-only)',
+    author='Flavio',
+    author_email='flaviocomblez@gmail.com',
+    description='This script allows to check services under Linux and sends mails in case of crash of one of them.'
+)
