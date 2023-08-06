c714c6cb01c4b25c6d980aa51c0e2a3404ea50b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:52 +0000

    Add package suite-py/1.20.4/suite-py-1.20.4.tar.gz

---
+++ packages/suite-py/suite-py-1.20.4.tar.gz/suite-py-1.20.4/suite_py/lib/logger.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+import logzero
+from logzero import logger as _logger
+
+DEFAULT_FORMAT = "%(color)s[%(levelname)1.4s]%(end_color)s %(message)s"
+# Set a custom formatter
+formatter = logzero.LogFormatter(fmt=DEFAULT_FORMAT)
+logzero.setup_default_logger(formatter=formatter)
+
+
+def setLevel(level):
+    _logger.setLevel(level)
+
+
+def debug(message):
+    _logger.debug(message)
+
+
+def info(message):
+    _logger.info(message)
+
+
+def warning(message):
+    _logger.warning(message)
+
+
+def error(message):
+    _logger.error(message)
