7eb5b721e5bc92abedcb54300718e7a24eb3fff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:54 +0000

    Add package ak-useful-ml-distributions/0.2/ak_useful_ml_distributions-0.2.tar.gz

---
+++ packages/ak-useful-ml-distributions/ak_useful_ml_distributions-0.2.tar.gz/ak_useful_ml_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ak_useful_ml_distributions',
+      version='0.2',
+      description='Distribution Classes for Machine Learning',
+      packages=['ak_useful_ml_distributions'],
+      author='Andrew Kalil',
+      zip_safe=False)
