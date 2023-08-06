397ff9be069981b54dbe099c61ee45f9919e1fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:59 +0000

    Add package k-mxt-w3/1.0.6/k_mxt_w3-1.0.6.tar.gz

---
+++ packages/k-mxt-w3/k_mxt_w3-1.0.6.tar.gz/k_mxt_w3-1.0.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(name='k_mxt_w3',
+                 version='1.0.6',
+                 description='k-mxt and k-mxt-w clustering algorithms',
+                 author='Anastasia Stepanova',
+                 author_email='stacey.stepanova@gmail.com',
+                 url='https://github.com/anstepanova/k_mxt_w',
+                 zip_safe=False,
+                 include_package_data=True,
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 install_requires=['attrs==21.2.0', 'coverage==5.5', 'iniconfig==1.1.1',
+                                   'joblib==1.0.1', 'numpy==1.20.3', 'packaging==20.9',
+                                   'pandas==1.2.4', 'plotly==4.8.2', 'pluggy==0.13.1',
+                                   'py==1.10.0', 'pyparsing==2.4.7', 'pytest==6.2.4',
+                                   'pytest-cov==2.11.1', 'scikit-learn==0.24.2',
+                                   'scipy==1.6.3', 'six==1.16.0', 'threadpoolctl==2.1.0',
+                                   'toml==0.10.2'],
+                 packages=['k_mxt_w3'],
+                 python_requires=">=3.9",
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "Operating System :: OS Independent",
+                 ],
+                 )
