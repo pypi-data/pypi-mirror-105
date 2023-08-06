27a59ebd058dcdb247ca4a06ef8473f51859f481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:09 +0000

    Add package hmorse/1.0.0/hmorse-1.0.0.tar.gz

---
+++ packages/hmorse/hmorse-1.0.0.tar.gz/hmorse-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hmorse']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'hmorse',
+    'version': '1.0.0',
+    'description': 'Morse code but h',
+    'long_description': None,
+    'author': 'vcokltfre',
+    'author_email': 'vcokltfre@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
