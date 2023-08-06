f7e6a9bc75844564355331314afb3cf694d8d545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:11 +0000

    Add package pyltx/0.1.0/pyltx-0.1.0.tar.gz

---
+++ packages/pyltx/pyltx-0.1.0.tar.gz/pyltx-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyltx", # Replace with your own username
+    version="0.1.0",
+    author="Nishad Mandlik",
+    author_email="mandliksg@gmail.com",
+    description="LaTeX generation for tables, math and figures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matiyau/pyltx",
+    project_urls={
+        "Bug Tracker": "https://github.com/matiyau/pyltx/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "pyltx"},
+    packages=setuptools.find_packages(where="pyltx"),
+    install_reqs = required,
+    python_requires=">=3.6",
+)
