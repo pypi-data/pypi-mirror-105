6c8bf883379a92aaa69ad760d9420f73a21ef3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:00 +0000

    Add package quantify-core/0.4.0/quantify-core-0.4.0.tar.gz

---
+++ packages/quantify-core/quantify-core-0.4.0.tar.gz/quantify-core-0.4.0/docs/api_reference.rst
@@ -0,0 +1,125 @@
+.. jupyter-kernel:: python3
+    :id: quantify_core_all_docs
+
+quantify
+=============
+
+.. automodule:: quantify
+   :members:
+
+.. _analysis_api:
+
+analysis
+=============
+
+.. automodule:: quantify.analysis
+    :members:
+
+
+base_analysis
+-------------
+
+.. automodule:: quantify.analysis.base_analysis
+    :members:
+    :show-inheritance:
+
+cosine_analysis
+---------------
+
+.. automodule:: quantify.analysis.cosine_analysis
+    :members:
+    :show-inheritance:
+
+spectroscopy_analysis
+---------------------
+
+.. automodule:: quantify.analysis.spectroscopy_analysis
+    :members:
+    :show-inheritance:
+
+t1_analysis
+-----------
+
+.. automodule:: quantify.analysis.t1_analysis
+    :members:
+    :show-inheritance:
+
+rabi_analysis
+-------------
+
+.. automodule:: quantify.analysis.rabi_analysis
+    :members:
+    :show-inheritance:
+
+fitting_models
+--------------
+
+.. automodule:: quantify.analysis.fitting_models
+    :members:
+    :show-inheritance:
+
+
+data
+====
+
+types
+-----
+
+.. automodule:: quantify.data.types
+    :members:
+
+handling
+--------
+
+.. automodule:: quantify.data.handling
+    :members:
+
+
+measurement
+===========
+
+.. automodule:: quantify.measurement
+    :members:
+
+
+utilities
+=========
+
+experiment_helpers
+------------------
+
+.. automodule:: quantify.utilities.experiment_helpers
+    :members:
+
+
+visualization
+=============
+
+.. automodule:: quantify.visualization
+    :members:
+
+color_utilities
+---------------
+
+.. automodule:: quantify.visualization.color_utilities
+    :members:
+
+
+mpl_plotting
+------------
+
+.. automodule:: quantify.visualization.mpl_plotting
+    :members:
+
+plot_interpolation
+------------------
+
+.. automodule:: quantify.visualization.plot_interpolation
+    :members:
+
+
+SI Utilities
+------------
+
+.. automodule:: quantify.visualization.SI_utilities
+    :members:
---
+++ packages/quantify-core/quantify-core-0.4.0.tar.gz/quantify-core-0.4.0/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("CHANGELOG.rst") as history_file:
+    history = history_file.read()
+
+with open("AUTHORS.rst") as authors_file:
+    authors = authors_file.read()
+
+with open("requirements_dev.txt") as test_reqs:
+    test_requirements = test_reqs.read().splitlines()
+
+requirements = [
+    "numpy!=1.19.4",  # 1.19.4 not allowed because of a bug on windows https://github.com/numpy/numpy/issues/17726
+    "qcodes",
+    "scipy>=1.5.0,!=1.6.0",
+    "h5netcdf",
+    "xarray==0.17.0",  # 0.17.1 will introduce breaking changes, see issue #161
+    "xxhash",
+    "matplotlib",
+    "lmfit",
+    "pyqt5==5.15.1",  # pinned due to #170, https://bugreports.qt.io/browse/PYSIDE-1473
+    "pyqtgraph",
+    "jsonschema",
+    "adaptive",
+    "filelock",
+    "appnope",
+    "uncertainties",
+    # used to display the structure of the experiments container in tutorials
+    # placed here to avoid errors when users download tutorials
+    "directory-tree>=0.0.2",
+]
+
+setup_requirements = [
+    "pytest-runner",
+]
+
+setup(
+    author="The Quantify consortium consisting of Qblox and Orange Quantum Systems",
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Unified quantum computing, solid-state and pulse sequencing physical experimentation framework.",
+    install_requires=requirements,
+    license="BSD-4 license",
+    long_description=readme + "\n\n" + authors + "\n\n" + history,
+    include_package_data=True,
+    keywords="quantify-core",
+    name="quantify-core",
+    packages=find_packages(include=["quantify", "quantify.*"]),
+    package_data={"": ["*.json"]},  # ensures JSON schema are included
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://gitlab.com/quantify-os/quantify-core",
+    version="0.4.0",
+    zip_safe=False,
+)
