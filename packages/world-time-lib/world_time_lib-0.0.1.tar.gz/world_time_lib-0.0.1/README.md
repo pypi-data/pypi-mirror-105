8726dd64391c78355af31b3ab0aac6244b406131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:07 +0000

    Add package world-time-lib/0.0.1/world_time_lib-0.0.1.tar.gz

---
+++ packages/world-time-lib/world_time_lib-0.0.1.tar.gz/world_time_lib-0.0.1/README.md
@@ -0,0 +1,11 @@
+# World Time
+
+Worldtime is a Python library for dealing with word pluralization.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install world_time
+```
---
+++ packages/world-time-lib/world_time_lib-0.0.1.tar.gz/world_time_lib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="world_time_lib",
+    version="0.0.1",
+    author="SDA",
+    author_email="sda@sda.pl",
+    description="Be in time",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
