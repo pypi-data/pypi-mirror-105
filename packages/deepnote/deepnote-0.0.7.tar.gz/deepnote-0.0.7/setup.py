699e1782d40228467f5df7c45cd8b8d0d7b73abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:41 +0000

    Add package deepnote/0.0.7/deepnote-0.0.7.tar.gz

---
+++ packages/deepnote/deepnote-0.0.7.tar.gz/deepnote-0.0.7/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+from deepnote import __version__, __package_name__, __description__, __repository_url__
+
+
+here = pathlib.Path(__file__).parent.resolve()
+
+
+with open(here / 'requirements.txt', 'r') as f:
+    dependencies = f.read().split('\n')
+
+setup(
+    name=__package_name__,  # Required
+    version=__version__,  # Required
+    description=__description__,  # Optional
+    # long_description='',  # Optional
+    # long_description_content_type='text/markdown',  # Optional (see note above)
+    url=__repository_url__,  # Optional
+    author='Soroush Omranpour',  # Optional
+    author_email='soroush.333@gmail.com',  # Optional
+    keywords='midi, deep learning, music',  # Optional
+    packages=['deepnote'],  # Required
+    python_requires='>=3.7, <4',
+    install_requires=dependencies,  # Optional
+    license="MIT license",
+
+    project_urls={  # Optional
+        'Bug Reports': __repository_url__ + '/issues',
+        'Source': __repository_url__,
+    },
+)
