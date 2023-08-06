946ac8913d174714a90c28a92832be44561b6c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:09 +0000

    Add package qtstrap/0.0.22/qtstrap-0.0.22-py3-none-any.whl

---
+++ packages/qtstrap/qtstrap-0.0.22-py3-none-any.whl/qtstrap/__init__.py
@@ -0,0 +1,14 @@
+from .options import *
+from .settings import *
+from .qt import *
+from .utils import *
+from .widgets import *
+from .base_application import BaseApplication, install_app_info, install_ctrlc_handler
+from .base_window import BaseMainWindow
+from .toolbar import BaseToolbar, SettingsToolbar
+from .timestamp import TimeStamp, time_since
+from .decorators import singleton, trace
+
+
+# create a shorter alias for accessing the current QApplication
+App = QApplication.instance
