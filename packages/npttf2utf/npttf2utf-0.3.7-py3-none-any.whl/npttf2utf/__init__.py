b119e6c7295d70b4ece4d4634893930a2b07f430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:28 +0000

    Add package npttf2utf/0.3.7/npttf2utf-0.3.7-py3-none-any.whl

---
+++ packages/npttf2utf/npttf2utf-0.3.7-py3-none-any.whl/npttf2utf/__init__.py
@@ -0,0 +1,90 @@
+import argparse
+import json
+import os
+import zipfile
+from .base.docxhandler import DocxHandler, ET
+from .base.exceptions import *
+from .base.fontmapper import FontMapper
+from .base.txthandler import TxtHandler
+
+
+def main():
+    about = """ 
+    Created by : Sabin Acharya (@trippygeese on github)
+    License    : GNU GENERAL PUBLIC LICENSE v3
+    Version    : 0.3.7
+    Email      : sabin2059@protonmail.com
+    """
+    modes = ['string', 'plain', 'docx']
+    parser = argparse.ArgumentParser(description=about, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-V', '--version', action='version', version="0.1a")
+    parser.add_argument('-m', '--mode', dest='mode', help='Conversion mode ', choices=modes, required=True)
+    parser.add_argument('-if', '--input-font', dest='font',
+                        help='Font used in input file. ("auto" can be used for docx mode)',
+                        default='preeti', required=True)
+    parser.add_argument('-of', '--output-font', dest='outputfont',
+                        help='Font to which output will be mapped to. (If unspecified output font will be set to '
+                             'unicode)',
+                        default='unicode', choices=["Preeti", "unicode"], required=False)
+    parser.add_argument('-dc', '--docx-components', dest='docxcomponents',
+                        help='Component of docx which will be processed. (Comma separated) Available: '
+                             '"body_paragraph,table,shape" (If not specified all components will be processed)',
+                        default='body_paragraph,table,shape', required=False)
+    parser.add_argument('-kf', '--known-unicode-fonts', dest='knownunicodefonts',
+                        help='Fonts to add to known supported unicode fonts while converting to preeti (If '
+                             'Unspecified "Kalimati,Mangal,Noto Sans Devanagari" will be set)',
+                        default='', required=False)
+    parser.add_argument('-i', '--input', dest='input', help='Input string or filepath', required=True)
+    parser.add_argument('-o', '--output', dest='output', help='Output file path. Not required for string mode')
+    parser.add_argument('-mf', '--map-file', dest='mapfile', help='Mapping definition file')
+    args = parser.parse_args()
+    font = args.font
+    op_mode = args.mode
+
+    def splitnclean(string):
+        lis = string.split(",")
+        for index, item in enumerate(lis):
+            lis[index] = item.strip()
+        return lis
+
+    rule_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "map.json")
+    if args.mapfile is not None:
+        rule_file = args.mapfile
+    try:
+        if op_mode == "string":
+            converter = FontMapper(rule_file)
+            if args.outputfont.lower() == 'unicode':
+                print(converter.map_to_unicode(args.input, from_font=args.font))
+            elif args.outputfont.lower() == 'preeti':
+                print(converter.map_to_preeti(args.input, from_font=args.font))
+            else:
+                raise UnsupportedMapToException
+        elif op_mode == "plain" or op_mode == "docx":
+            converter = None
+            if op_mode == "plain":
+                converter = TxtHandler(rule_file)
+            elif op_mode == "docx":
+                converter = DocxHandler(rule_file)
+            converter.map_fonts(original_file_path=args.input, output_file_path=args.output, from_font=args.font,
+                                to_font=args.outputfont, components=splitnclean(args.docxcomponents),
+                                known_unicode_fonts=splitnclean(args.knownunicodefonts))
+            print("The converted file is saved as : {}".format(args.output))
+        else:
+            print("Unsupported operation mode")
+    except MapFileNotFoundException:
+        print("Cannot find the map file '{}'".format(rule_file))
+    except NoMapForOriginException:
+        print("The mapping for selected origin font does not exist")
+    except FileNotFoundError:
+        print("Cannot find the input file at '{}'".format(args.input))
+    except json.decoder.JSONDecodeError:
+        print("Invalid mapping file. {}".format(rule_file))
+    except UnsupportedMapToException:
+        print("Cannot map to given output font ! ({}) ".format(args.outputfont))
+    except TxtAutoModeException:
+        print("Font auto detection does not work on plain text files :(")
+    except (zipfile.BadZipFile, KeyError, UnicodeDecodeError, ET.ParseError):
+        print("The type of file '{}' either does not match the conversion mode (-m) or "
+              "the file is corrupted.".format(args.input))
+    except PermissionError:
+        print("Permission denied to read the input file or write the output file.")
---
+++ packages/npttf2utf/npttf2utf-0.3.7-py3-none-any.whl/npttf2utf/base/docxhandler.py
@@ -0,0 +1,233 @@
+from xml.etree import ElementTree as ET
+from io import StringIO
+from .fontmapper import FontMapper
+from .exceptions import UnsupportedMapToException
+import os
+import zipfile
+import tempfile
+import shutil
+
+
+class DocxHandler:
+
+    def __init__(self, rules_file, default_unicode_font_name="Kalimati"):
+        self.mapper = FontMapper(rules_file)
+        self.supported_ttf_fonts = self.mapper.supported_maps
+        self.default_unicode_font_name = default_unicode_font_name
+        self.known_devanagari_unicode_fonts = ["Kalimati", "Mangal", "Noto Sans Devanagari"]
+
+    @staticmethod
+    def __get_xml(docx_file_path):
+        with zipfile.ZipFile(docx_file_path) as zf:
+            with zf.open('word/document.xml') as xf:
+                xml_content = xf.read().decode('utf-8')
+        return xml_content
+
+    @staticmethod
+    def __save_docx(xml_content, output_file_path, original_file_path):
+        tmp_dir = tempfile.mkdtemp()
+        with zipfile.ZipFile(original_file_path, 'r') as zipObj:
+            zipObj.extractall(tmp_dir)
+            filenames = zipObj.namelist()
+        with open(os.path.join(tmp_dir, 'word/document.xml'), 'w', encoding="utf-8") as f:
+            f.write(xml_content)
+        with zipfile.ZipFile(output_file_path, "w") as docx:
+            for filename in filenames:
+                docx.write(os.path.join(tmp_dir, filename), filename)
+        shutil.rmtree(tmp_dir)
+        return True
+
+    @staticmethod
+    def __register_namespaces(xml):
+        namespaces = dict([node for _, node in ET.iterparse(xml, events=['start-ns'])])
+        for ns in namespaces:
+            ET.register_namespace(ns, namespaces[ns])
+
+    @staticmethod
+    def __get_font_data_from_relation_property(relation_property):
+        used_font = "dummyFontThatWillNeverBeUsed"
+        used_unicode_font = "dummyFontThatWillNeverBeUsed"
+        font_property = relation_property.find(
+            "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}rFonts")
+        if font_property is not None:
+            try:
+                used_font = font_property.attrib[
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}ascii"
+                ]
+            except KeyError:
+                used_unicode_font = font_property.attrib[
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}cs"
+                ]
+                used_font = "Unicode"
+        return font_property, used_font, used_unicode_font
+
+    def __map_now(self, map_to, font_property, text_container=None, used_font=None):
+        # Strip un necessary font related attributes. Make it minimal.. Word processor
+        # will add them back if necessary
+        font_property.attrib = self.__strip_font_attributes(font_property.attrib)
+        if map_to.lower() == "preeti":
+            font_property.attrib[
+                "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}ascii"
+            ] = "Preeti"
+            font_property.attrib[
+                "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}hAnsi"
+            ] = "Preeti"
+            # Replace original text with mapped text
+            if text_container is not None:
+                original_text = text_container.text
+                text_container.text = self.mapper.map_to_preeti(original_text, used_font, unescape_html_input=True)
+        elif map_to.lower() == "unicode":
+            font_property.attrib[
+                "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}cs"
+            ] = self.default_unicode_font_name
+            # Replace original text with mapped text
+            if text_container is not None:
+                original_text = text_container.text
+                text_container.text = self.mapper.map_to_unicode(original_text, used_font, unescape_html_input=True)
+        else:
+            raise UnsupportedMapToException("Document cannot be mapped to target ")
+
+    @staticmethod
+    def __strip_font_attributes(dictionary):
+        # Don't know why but removing cstheme and eastAsiaTheme does magic and fixes map to incorrect font
+        # So yes.... This is needed
+        return {key: value for key, value in dictionary.items()
+                if key not in [
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}cstheme",
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}eastAsiaTheme",
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}ascii",
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}hAnsi",
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}cs"
+                ]}
+
+    def __handle_wp_containers_in_paragraphs(self, paragraphs, from_font="auto", to_font="unicode",
+                                             known_unicode_fonts=None):
+        if known_unicode_fonts is None:
+            known_unicode_fonts = []
+        for paragraph in paragraphs:
+            for relation in paragraph.iterfind("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}r"):
+                relation_property = relation.find("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}rPr")
+                if relation_property is not None:
+                    font_property, used_font, used_unicode_font = self.__get_font_data_from_relation_property(
+                        relation_property
+                    )
+                    text_container = relation.find("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}t")
+                    if text_container is not None:
+                        continue_mapping = True
+                        set_from_font = None
+                        if from_font == "auto":
+                            # Check if the used font is in supported ttf fonts + "unicode"
+                            if used_font in self.supported_ttf_fonts:
+                                set_from_font = used_font
+                                if to_font.lower() == "preeti":
+                                    if used_unicode_font not in (self.known_devanagari_unicode_fonts +
+                                                                 known_unicode_fonts + self.supported_ttf_fonts):
+                                        continue_mapping = False
+                            else:
+                                continue_mapping = False
+                        else:
+                            # Since this is manual mode.. Do no checks and set used font to the font set to "from_font"
+                            # variable. This will change font of whole document to "to_font", every text content will
+                            # be interpreted as "from_font" and will be mapped to "to_font"
+                            # Exception will be thrown while trying to map from/to unsupported fonts
+                            set_from_font = from_font
+                        # All checks must have been done. Now start mapping the fonts
+                        if continue_mapping:
+                            self.__map_now(to_font, font_property,
+                                           text_container=text_container,
+                                           used_font=set_from_font)
+
+            # A "Not so quick" fix for pPr inside w:p inside "w:txbxcontent"
+            for paragraph_property in paragraph.iterfind(
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}pPr"):
+                relation_property = paragraph_property.find(
+                    "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}rPr")
+                if relation_property is not None:
+                    font_property, used_font, used_unicode_font = self.__get_font_data_from_relation_property(
+                        relation_property
+                    )
+                    continue_mapping = True
+                    # If on auto auto mode, only change base font when mapping is possible
+                    if from_font == "auto":
+                        if used_font in self.supported_ttf_fonts:
+                            if to_font.lower() == "preeti":
+                                if used_unicode_font not in (self.known_devanagari_unicode_fonts +
+                                                             known_unicode_fonts + self.supported_ttf_fonts):
+                                    continue_mapping = False
+                        else:
+                            continue_mapping = False
+                    if continue_mapping:
+                        self.__map_now(to_font, font_property)
+        return True
+
+    def detect_used_fonts(self, docx_file_path):
+        detected_supported_fonts = []
+        tree = ET.ElementTree(ET.fromstring(self.__get_xml(docx_file_path)))
+        root = tree.getroot()
+
+        font_property_containers = root.iter("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}rFonts")
+        for container in font_property_containers:
+            used_font = container.attrib.get("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}ascii",
+                                             "SomeUnsupportedFonts6576")
+            if used_font in self.supported_ttf_fonts and used_font not in detected_supported_fonts:
+                detected_supported_fonts.append(used_font)
+        return detected_supported_fonts
+
+    def map_fonts(self, original_file_path, output_file_path="mapped.docx", from_font="auto", to_font="unicode",
+                  components=None, known_unicode_fonts=None):
+        if known_unicode_fonts is None:
+            known_unicode_fonts = []
+        if components is None:
+            components = ["body_paragraph", "table", "shape"]
+        xml_string = self.__get_xml(original_file_path)
+        tree = ET.ElementTree(ET.fromstring(xml_string))
+        root = tree.getroot()
+        # GET INSIDE THE BODY
+        for body in root.iterfind("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}body"):
+            # Process normal paragraphs. They (w:p) lie directly inside body as child
+            general_paragraphs = body.findall("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}p")
+            if "body_paragraph" in components:
+                self.__handle_wp_containers_in_paragraphs(general_paragraphs,
+                                                          from_font=from_font,
+                                                          to_font=to_font,
+                                                          known_unicode_fonts=known_unicode_fonts)
+
+            # Process paragraphs. They (w:p) lie inside "w:tbl" as child of table row and column, but we can just
+            # iterate inside table to get them
+            if "table" in components:
+                for table in body.iterfind("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}tbl"):
+                    # Now since we are not direct child of normal paragraphs but inside a table, We can use .iter()
+                    # to get all the paragraphs inside current table without modifying other components
+                    paragraphs = table.iter("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}p")
+                    self.__handle_wp_containers_in_paragraphs(paragraphs,
+                                                              from_font=from_font,
+                                                              to_font=to_font,
+                                                              known_unicode_fonts=known_unicode_fonts)
+
+            # Process shapes. They lie inside "w:p" (Main paragraphs), find them. Shapes wont be processed in
+            # "body_paragraph" and SHOULD NOT BE as content in shape lie much deeper
+            if "shape" in components:
+                for paragraph in general_paragraphs:
+                    tbx_content = paragraph.iter(
+                        "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}txbxContent")
+                    # They contain  data for (t)e(x)t(b)o(x)/Shapes. They content another "w:p" which has actual
+                    # text content
+                    for txbx in tbx_content:
+                        paragraphs = txbx.iterfind(
+                            "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}p")
+                        self.__handle_wp_containers_in_paragraphs(paragraphs,
+                                                                  from_font=from_font,
+                                                                  to_font=to_font,
+                                                                  known_unicode_fonts=known_unicode_fonts)
+
+        # Make a temp directory for working, extract the original docx file in that temp directory
+        # Replace content of document.xml in temporary directory with modified xml data . Repack everything back
+        # And save the resulting package with the passed filename
+
+        tmp_dir = tempfile.mkdtemp()
+        self.__register_namespaces(StringIO(xml_string))
+        tree.write(os.path.join(tmp_dir, "document.xml"), encoding="utf-8",xml_declaration=True)
+        with open(os.path.join(tmp_dir, "document.xml"), "r", encoding="utf-8", errors="ignore") as f:
+            xml_content = f.read()
+        self.__save_docx(xml_content, output_file_path, original_file_path)
+        shutil.rmtree(tmp_dir)
