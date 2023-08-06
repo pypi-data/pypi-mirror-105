0871dd70c3c769e7c16f40af011a957ab6b58927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:27 +0000

    Add package bitctrl/0.0.9/bitctrl-0.0.9.tar.gz

---
+++ packages/bitctrl/bitctrl-0.0.9.tar.gz/bitctrl-0.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name="bitctrl",
+    version="0.0.9",
+    description='Turn your microbit into a controller',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author='momea007',
+    author_email='momea007@hotmail.com',
+    keywords='microbit',
+    license='MIT',
+    packages=['bitctrl'],
+    install_requires=['pyserial','keyboard'],
+    include_package_data=True,
+    zip_safe=False
+    )
