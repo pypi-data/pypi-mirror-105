117075bc81914c62dd8febc48f310ee61a239d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:34 +0000

    Add package perceptilabs/0.12.3/perceptilabs-0.12.3-cp36-cp36m-win_amd64.whl

---
+++ packages/perceptilabs/perceptilabs-0.12.3-cp36-cp36m-win_amd64.whl/perceptilabs/modelrecommender/encoders/__init__.py
@@ -0,0 +1,5 @@
+from perceptilabs.modelrecommender.encoders.base import EncoderBlueprint
+from perceptilabs.modelrecommender.encoders.numerical import NumericalEncoderBlueprint
+from perceptilabs.modelrecommender.encoders.image import ImageEncoderBlueprint
+from perceptilabs.modelrecommender.encoders.binary import BinaryEncoderBlueprint
+from perceptilabs.modelrecommender.encoders.categorical import CategoricalEncoderBlueprint
