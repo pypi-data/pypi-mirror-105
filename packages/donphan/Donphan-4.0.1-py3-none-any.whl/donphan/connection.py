c75b9b18c6d3165971ad487832cffab9d67d74f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:51 +0000

    Add package donphan/4.0.1/Donphan-4.0.1-py3-none-any.whl

---
+++ packages/donphan/Donphan-4.0.1-py3-none-any.whl/donphan/connection.py
@@ -0,0 +1,135 @@
+from __future__ import annotations
+
+import datetime
+import json
+
+from collections.abc import Callable
+from typing import Any, TYPE_CHECKING, NamedTuple, TypeVar, Literal
+
+import asyncpg
+
+from .consts import CUSTOM_TYPES, POOLS
+
+if TYPE_CHECKING:
+    from asyncpg import Connection
+    from asyncpg.pool import Pool
+
+__all__ = (
+    "create_pool",
+    "MaybeAcquire",
+    "TYPE_CODECS",
+    "OPTIONAL_CODECS",
+)
+
+
+T = TypeVar("T")
+
+
+# Y2K_DT = datetime.datetime(2000, 1, 1, tzinfo=datetime.timezone.utc)
+Y2K_EPOCH = 946684800000000
+
+
+class TypeCodec(NamedTuple):
+    format: Literal["text", "binary", "tuple"]
+    encoder: Callable[..., Any]
+    decoder: Callable[..., Any]
+
+
+def _encode_datetime(value: datetime.datetime) -> tuple[int]:
+    if value.tzinfo is None:
+        value = value.astimezone(datetime.timezone.utc)
+    return (round(value.timestamp() * 1_000_000) - Y2K_EPOCH,)
+
+
+def _decode_timestamp(value: tuple[int]) -> datetime.datetime:
+    return datetime.datetime.fromtimestamp(round(value[0] + Y2K_EPOCH) / 1_000_000, datetime.timezone.utc)
+
+
+TYPE_CODECS: dict[str, TypeCodec] = {
+    "json": TypeCodec("text", json.dumps, json.loads),
+    "jsonb": TypeCodec("text", json.dumps, json.loads),
+}
+
+OPTIONAL_CODECS: dict[str, TypeCodec] = {
+    "timestamp": TypeCodec("tuple", _encode_datetime, _decode_timestamp),
+}
+
+
+async def create_pool(dsn: str, codecs: dict[str, TypeCodec] = {}, **kwargs) -> Pool:
+    r"""|coro|
+
+    Creates the database connection pool.
+
+    Parameters
+    ----------
+    dsn: :class:`str`
+        A database connection string.
+    codecs: Dict[:class:`str`, :class:`.TypeCodec`]
+        A mapping of type to encoder and decoder for custom type codecs.
+    \*\*kwargs: Any
+        Extra keyword arguments to pass to :meth:`asyncpg.create_pool`
+
+    Returns
+    -------
+    :class:`asyncpg.pool.Pool`
+        The new pool which was created.
+    """
+    codecs |= TYPE_CODECS
+
+    async def init(connection: Connection) -> None:
+        for type, codec in codecs.items():
+            await connection.set_type_codec(
+                type, schema="pg_catalog", encoder=codec.encoder, decoder=codec.decoder, format=codec.format
+            )
+
+        for type in CUSTOM_TYPES.values():
+            await type._set_codec(connection)
+
+    pool = await asyncpg.create_pool(dsn, init=init, **kwargs)
+    if pool is None:
+        raise RuntimeError("Could not create pool.")
+    POOLS.append(pool)
+    return pool
+
+
+class MaybeAcquire:
+    """Async helper for acquiring a connection to the database.
+
+    .. container:: operations
+
+        .. describe:: async with x as c
+
+            Yields a database connection to use,
+            if a new one was created it will be closed on exit.
+
+    Parameters
+    ----------
+        connection: Optional[:class:`asyncpg.Connection <asyncpg.connection.Connection>`]
+            A database connection to use.
+            If none is supplied a connection will be acquired from the pool.
+        pool: Optional[:class:`asyncpg.pool.Pool`]
+            A connection pool to use.
+
+    Attributes
+    ----------
+        connection: Optional[:class:`asyncpg.Connection <asyncpg.connection.Connection>`]
+            The supplied database connection, if provided.
+        pool: Optional[:class:`asyncpg.pool.Pool`]
+            The connection pool used to acquire new connections.
+    """
+
+    def __init__(self, connection: Connection = None, /, *, pool: Pool):
+        self.connection = connection
+        self.pool = pool
+        self._cleanup = False
+
+    async def __aenter__(self) -> Connection:
+        if self.connection is None:
+            self._cleanup = True
+            self._connection = c = await self.pool.acquire()
+            return c
+        return self.connection
+
+    async def __aexit__(self, *args):
+        if self._cleanup:
+            await self.pool.release(self._connection)
---
+++ packages/donphan/Donphan-4.0.1-py3-none-any.whl/donphan/creatable.py
@@ -0,0 +1,193 @@
+from __future__ import annotations
+
+from typing import Protocol, TYPE_CHECKING
+
+from .consts import DEFAULT_SCHEMA, NOT_CREATABLE
+from .utils import MISSING, normalise_name, query_builder
+
+if TYPE_CHECKING:
+    from asyncpg import Connection
+
+
+__all__ = ("Creatable",)
+
+
+class Creatable(Protocol):
+    _schema: str
+    _name: str
+
+    def __init_subclass__(
+        cls,
+        *,
+        _name: str = MISSING,
+        schema: str = MISSING,
+    ) -> None:
+        if schema is MISSING:
+            schema = DEFAULT_SCHEMA
+
+        if _name is MISSING:
+            _name = normalise_name(cls.__name__)
+
+        cls._schema = schema
+        cls._name = f"{schema}.{_name}"
+        super().__init_subclass__()
+
+    # region: query generation
+
+    @classmethod
+    def _query_create(
+        cls,
+        if_not_exists: bool,
+    ) -> str:
+        raise NotImplementedError()
+
+    @classmethod
+    @query_builder
+    def _query_create_schema(
+        cls,
+        if_not_exists: bool,
+    ) -> list[str]:
+        builder = ["CREATE SCHEMA"]
+        if if_not_exists:
+            builder.append("IF NOT EXISTS")
+        builder.append(cls._schema)
+        return builder
+
+    @classmethod
+    @query_builder
+    def _query_drop(
+        cls,
+        type: str,
+        if_exists: bool,
+        cascade: bool,
+    ) -> list[str]:
+        builder = ["DROP", type]
+
+        if if_exists:
+            builder.append("IF EXISTS")
+
+        builder.append(cls._name)
+
+        if cascade:
+            builder.append("CASCADE")
+
+        return builder
+
+    # endregion
+
+    # region: public methods
+
+    @classmethod
+    async def create(
+        cls,
+        connection: Connection,
+        *,
+        if_not_exists: bool = True,
+        create_schema: bool = True,
+    ) -> None:
+        """|coro|
+
+        Creates this database object.
+
+        Parameters
+        ----------
+        connection: :class:`asyncpg.Connection <asyncpg.connection.Connection>`
+            The database connection to use for transactions.
+        if_not_exists: :class:`bool`
+            Sets whether creation should continue if the object already exists.
+            Defaults to ``True``.
+        create_schema: :class:`bool`
+            Sets whether the database schema should also be created.
+            Defaults to ``True``.
+        """
+        if create_schema:
+            await cls.create_schema(connection, if_not_exists=if_not_exists)
+        query = cls._query_create(if_not_exists)
+        await connection.execute(query)
+
+    @classmethod
+    async def create_schema(
+        cls,
+        connection: Connection,
+        *,
+        if_not_exists: bool = True,
+    ) -> None:
+        """|coro|
+
+        Creates the schema this database object uses.
+
+        Parameters
+        ----------
+        connection: :class:`asyncpg.Connection <asyncpg.connection.Connection>`
+            The database connection to use for transactions.
+        if_not_exists: :class:`bool`
+            Sets whether creation should continue if the schema already exists.
+            Defaults to ``True``.
+        """
+        query = cls._query_create_schema(if_not_exists)
+        await connection.execute(query)
+
+    @classmethod
+    async def create_all(
+        cls,
+        connection: Connection,
+        /,
+        *,
+        if_not_exists: bool = True,
+        create_schema: bool = True,
+    ) -> None:
+        """|coro|
+
+        Creates all subclasses of this database object.
+
+        Parameters
+        ----------
+        connection: :class:`asyncpg.Connection <asyncpg.connection.Connection>`
+            The database connection to use for transactions.
+        if_not_exists: :class:`bool`
+            Sets whether creation should continue if the object already exists.
+            Defaults to ``True``.
+        create_schema: :class:`bool`
+            Sets whether the database schema should also be created.
+            Defaults to ``True``.
+        """
+        for subcls in cls.__subclasses__():
+            if subcls in NOT_CREATABLE:
+                await subcls.create_all(
+                    connection,
+                    if_not_exists=if_not_exists,
+                    create_schema=create_schema,
+                )
+            else:
+                await subcls.create(
+                    connection,
+                    if_not_exists=if_not_exists,
+                    create_schema=create_schema,
+                )
+
+    @classmethod
+    async def drop(
+        cls,
+        connection: Connection,
+        /,
+        *,
+        if_exists: bool = True,
+        cascade: bool = False,
+    ) -> None:
+        """|coro|
+
+        Drops this object from the database.
+
+        Parameters
+        ----------
+        if_exists: :class:`bool`
+            Sets whether dropping should not error if the object does not exist.
+            Defaults to ``True``.
+        cascade: :class:`bool`
+            Sets whether dropping should cascade.
+            Defaults to ``False``.
+        """
+        query = cls._query_drop(if_exists, cascade)
+        await connection.execute(query)
+
+    # endregion
