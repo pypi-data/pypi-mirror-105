ecca547e28f78b398e58eed6c8d494b2dc8607da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:02 +0000

    Add package fro-AI/0.7.0/fro_AI-0.7.0-py3-none-any.whl

---
+++ packages/fro-AI/fro_AI-0.7.0-py3-none-any.whl/fro_AI/utils/img_utils.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import jetson.utils
+
+
+def roi(img, x1, x2, y1, y2):
+    return img[x1:x2, y1:y2]
+
+
+class RtpStream(object):
+    def __init__(self,
+                 remote_ip='192.168.55.100',
+                 bitrate=4000000,
+                 codec='h264',
+                 remote_port=1234) -> None:
+        super().__init__()
+
+        self.remote_ip = remote_ip
+        self.bitrate = bitrate
+        self.codec = codec
+        self.remote_port = remote_port
+
+    def enable(self):
+        if hasattr(self, 'rtp'):
+            return
+        self.rtp = jetson.utils.videoOutput(
+            f"rtp://{self.remote_ip}:{self.remote_port}", [
+                "--headless",
+                f"--output-codec={self.codec}",
+                f"--bitrate={self.bitrate}",
+            ])
+
+    def disable(self):
+        del self.rtp
+
+    def put(self, bgr):
+        if hasattr(self, 'rtp'):
+            rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+            cuda_mem = jetson.utils.cudaFromNumpy(rgb)
+            self.rtp.Render(cuda_mem)
