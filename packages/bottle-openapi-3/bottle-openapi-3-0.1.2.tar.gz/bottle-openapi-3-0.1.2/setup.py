1071eb8195b1539f999d6fbe9916ae696b0cca72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:31 +0000

    Add package bottle-openapi-3/0.1.2/bottle-openapi-3-0.1.2.tar.gz

---
+++ packages/bottle-openapi-3/bottle-openapi-3-0.1.2.tar.gz/bottle-openapi-3-0.1.2/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+VERSION = '0.1.2'
+
+setup(
+    version=VERSION,
+    download_url='https://github.com/cope-systems/bottle-openapi-3/archive/v{0}.tar.gz'.format(VERSION),
+)
