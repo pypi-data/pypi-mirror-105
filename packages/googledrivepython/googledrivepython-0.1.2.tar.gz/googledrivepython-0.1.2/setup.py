bcfb8587475f33e9940a081966ddc543381258c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:14 +0000

    Add package googledrivepython/0.1.2/googledrivepython-0.1.2.tar.gz

---
+++ packages/googledrivepython/googledrivepython-0.1.2.tar.gz/googledrivepython-0.1.2/setup.py
@@ -0,0 +1,30 @@
+
+from distutils.core import setup
+setup(
+  name = 'googledrivepython',         
+  packages = ['googledrivepython'],   
+  version = '0.1.2',      
+  license='MIT',        
+  description = 'A python Google Drive API v3 wrapper',   
+  author = 'Luyanda Dhlamini',                   
+  author_email = 'luyanda.dhlamini@gmail.com',  
+  url = 'https://github.com/luyandadhlamini',   
+  download_url = 'https://github.com/luyandadhlamini/googledrivepython/archive/refs/tags/v0.1.2.tar.gz', 
+  keywords = ['Python', 'Google Drive', 'drive', 'API', 'wrapper' ],   
+  install_requires=[            
+          'oauth2-client',
+          'mimetypes',
+          'google-api-python-client'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
