5eb58c5660709579fa07c919a3c1c7b2f2c83e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:21 +0000

    Add package causalicp/0.1.1/causalicp-0.1.1.tar.gz

---
+++ packages/causalicp/causalicp-0.1.1.tar.gz/causalicp-0.1.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='causalicp',
+    version='0.1.1',
+    author='Juan L Gamella',
+    author_email='juangamella@gmail.com',
+    packages=['causalicp'],
+    scripts=[],
+    url='https://github.com/juangamella/icp',
+    license='BSD 3-Clause License',
+    description='Python implementation of the Invariant Causal Prediction (ICP) algorithm for causal discovery.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=['numpy>=1.17.3', 'scipy>=1.4.0', 'termcolor>=1.1.0']
+)
