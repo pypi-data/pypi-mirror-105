0b6d3349d6df39b97887055d24058a6c273f847c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:16 +0000

    Add package nucml/1.0.5.dev1/nucml-1.0.5.dev1.tar.gz

---
+++ packages/nucml/nucml-1.0.5.dev1.tar.gz/nucml-1.0.5.dev1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nucml", # Replace with your own username
+    version="1.0.5.dev1",
+    author="Pedro Jr Vicente Valdez",
+    author_email="pedro.vicentevz@berkeley.edu",
+    description="ML-oriented tools for navigating the nuclear data evaluation pipeline.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pedrojrv/nucml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+
+    ],
+    keywords="nuclear ml nucml nuclearml nucleardata data cross section",
+    project_urls={
+        'Documentation': 'https://pedrojrv.github.io/nucml'
+    },
+    license="GNU General Public License v3 or later (GPLv3+)",
+    include_package_data=True,
+    install_requires=["natsort", "pandas", "plotly", "matplotlib", "scikit-learn", "seaborn", "imageio", "wandb", "google-cloud-bigquery"], 
+    python_requires='>=3.6',
+)
+
+
