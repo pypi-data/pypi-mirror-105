c5cff64bd89373ebf31e6aab702977efb0fc57a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:26 +0000

    Add package paspymod/0.0.3/paspymod-0.0.3-py2.py3-none-any.whl

---
+++ packages/paspymod/paspymod-0.0.3-py2.py3-none-any.whl/paspymod/logger.py
@@ -0,0 +1,5 @@
+import logging
+from .utility import f_check as f
+
+logging.basicConfig(level = f().loaded['debug_level'], datefmt='%Y-%m-%d %H:%M:%S', \
+    format='%(asctime)s %(name)s %(levelname)-8s %(message)s')
