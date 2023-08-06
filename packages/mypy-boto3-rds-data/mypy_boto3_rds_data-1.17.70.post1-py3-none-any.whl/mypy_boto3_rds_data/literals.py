8d707d71a1f1081fe160729b5c50456c419d4855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:42 +0000

    Add package mypy-boto3-rds-data/1.17.70.post1/mypy_boto3_rds_data-1.17.70.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-rds-data/mypy_boto3_rds_data-1.17.70.post1-py3-none-any.whl/mypy_boto3_rds_data/literals.py
@@ -0,0 +1,26 @@
+"""
+Type annotations for rds-data service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_rds_data/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_rds_data.literals import DecimalReturnType
+
+    data: DecimalReturnType = "DOUBLE_OR_LONG"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("DecimalReturnType", "TypeHint")
+
+
+DecimalReturnType = Literal["DOUBLE_OR_LONG", "STRING"]
+TypeHint = Literal["DATE", "DECIMAL", "JSON", "TIME", "TIMESTAMP", "UUID"]
