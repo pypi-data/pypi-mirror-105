957dda81f5aa2f2f2501f0c7a839415808b0dd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:14 +0000

    Add package PaqueteAECA/0.3/PaqueteAECA-0.3.tar.gz

---
+++ packages/PaqueteAECA/PaqueteAECA-0.3.tar.gz/PaqueteAECA-0.3/LibreriaAE_2.py
@@ -0,0 +1,5 @@
+def sumar(a, b):
+    print (a + b)
+
+def restar(a, b):
+    print (a - b)
---
+++ packages/PaqueteAECA/PaqueteAECA-0.3.tar.gz/PaqueteAECA-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(
+    name='PaqueteAECA',
+    version='0.3',
+    packages=find_packages(),
+    py_modules=['LibreriaAE_1', 'LibreriaAE_2']
+)
