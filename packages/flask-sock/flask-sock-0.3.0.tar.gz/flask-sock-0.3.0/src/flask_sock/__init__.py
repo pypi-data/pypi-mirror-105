469c706a012ed549f9998e3b3586a5eb5483309b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:30 +0000

    Add package flask-sock/0.3.0/flask-sock-0.3.0.tar.gz

---
+++ packages/flask-sock/flask-sock-0.3.0.tar.gz/flask-sock-0.3.0/src/flask_sock/__init__.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from flask import Blueprint, request, abort
+from simple_websocket import Server, ConnectionClosed
+
+
+class Sock:
+    def __init__(self, app=None):
+        self.app = None
+        self.bp = None
+        if app is None:
+            self.bp = Blueprint('__flask_sock', __name__)
+        else:
+            self.app = app
+            self.init_app(app)
+
+    def init_app(self, app):
+        if self.app is None:
+            app.register_blueprint(self.bp)
+
+    def route(self, path, **kwargs):
+        def decorator(f):
+            @wraps(f)
+            def websocket_route(*args, **kwargs):
+                try:
+                    f(Server(request.environ), *args, **kwargs)
+                except ConnectionClosed:
+                    pass
+                return ''
+
+            kwargs['websocket'] = True
+            return (self.app or self.bp).route(path, **kwargs)(websocket_route)
+
+        return decorator
