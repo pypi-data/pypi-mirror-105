3a64906d9986805c4b804f50211bd12319e1188a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:10 +0000

    Add package fast-trace/3.1.11/fast-trace-3.1.11.tar.gz

---
+++ packages/fast-trace/fast-trace-3.1.11.tar.gz/fast-trace-3.1.11/fast_tracker/agent/sss.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin python3
+# -*- coding: utf-8 -*-
+
+"""
+    created by iprobeyang@gmail.com 2021/5/11
+"""
+import re
+
+a = re.compile('fast_pymysql').match("fast_pymysql")
+print(a)
