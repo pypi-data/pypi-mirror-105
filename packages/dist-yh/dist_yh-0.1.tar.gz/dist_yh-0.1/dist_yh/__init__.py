642266700752545656a6519d3750861dc61ab464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package dist-yh/0.1/dist_yh-0.1.tar.gz

---
+++ packages/dist-yh/dist_yh-0.1.tar.gz/dist_yh-0.1/dist_yh/__init__.py
@@ -0,0 +1,8 @@
+import sys 
+sys.path.insert(0,'D:/work & study/data_science_ AI_online_courses/Machine' +
+                'learning engineer nanodegree/Module2 -Software engineering/lesson 4 - Introduction to opp/4a_binomial_package')
+
+
+from dist_yh.Gaussiandistribution import Gaussian
+from dist_yh.Binomialdistribution import Binomial
+
---
+++ packages/dist-yh/dist_yh-0.1.tar.gz/dist_yh-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_yh',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_yh'],
+      zip_safe=False)
