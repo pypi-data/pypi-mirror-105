152e2efc549890a8d5d888fa1a9ca2aa647fa692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:38 +0000

    Add package mjworker/0.1.7/mjworker-0.1.7.tar.gz

---
+++ packages/mjworker/mjworker-0.1.7.tar.gz/mjworker-0.1.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+ 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mjworker',
+    version='0.1.7',
+    author='huyan',
+    description='agent application for mj-monitor',
+    author_email='lixiuhu_lee@163.com',
+    url='https://gitlab.dataenlighten.com/devops/monitor',
+    py_modules=['mjworker'],
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+        'psutil',
+        'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        mjworker=mjworker:cli
+    ''',
+)
