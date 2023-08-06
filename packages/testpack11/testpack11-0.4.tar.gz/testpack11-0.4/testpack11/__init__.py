7491bd512bbd2be1b69abb4f18d148055a0e37c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:22 +0000

    Add package testpack11/0.4/testpack11-0.4.tar.gz

---
+++ packages/testpack11/testpack11-0.4.tar.gz/testpack11-0.4/testpack11/__init__.py
@@ -0,0 +1,2 @@
+import testpack11.sort
+import testpack11.sort
---
+++ packages/testpack11/testpack11-0.4.tar.gz/testpack11-0.4/testpack11/sort.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def Sort(nums: List[int]) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if nums[i] > nums[j]:
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+
+    return nums
+
+
+def Search(nums: List[int], key: int) -> bool:
+    for num in nums:
+        if num == key:
+            return True
+
+    return False
