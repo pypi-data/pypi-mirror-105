ebacf538e2f144e855d177422b9d893e20e46dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:13 +0000

    Add package holour/1.1.3/holour-1.1.3.tar.gz

---
+++ packages/holour/holour-1.1.3.tar.gz/holour-1.1.3/holour/msg/__init__.py
@@ -0,0 +1,4 @@
+from holour.msg.geometry import Vector3, Pose, Poses
+from holour.msg.status import Status
+from holour.msg.force_torque import ForceTorque
+from holour.msg.script import Script, WaypointConfig
