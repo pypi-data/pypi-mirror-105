a52fa0572dcc3203c12b8f0b1c301006c5243b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:12 +0000

    Add package gbjl-probability/1.2/gbjl_probability-1.2.tar.gz

---
+++ packages/gbjl-probability/gbjl_probability-1.2.tar.gz/gbjl_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gbjl_probability',
+      version='1.2',
+      description='Analyze Gaussian and Binomial distributions',
+      packages=['gbjl_probability'],
+      author='Jorge Nustes',
+      author_email='jlnustes@gmail.com',
+      zip_safe=False)
