c3b1f9b019bfd604ced0750c3c0ba68bdf680733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:34 +0000

    Add package mypy-boto3-timestream-write/1.17.71/mypy_boto3_timestream_write-1.17.71-py3-none-any.whl

---
+++ packages/mypy-boto3-timestream-write/mypy_boto3_timestream_write-1.17.71-py3-none-any.whl/mypy_boto3_timestream_write/literals.py
@@ -0,0 +1,28 @@
+"""
+Type annotations for timestream-write service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_timestream_write.literals import DimensionValueType
+
+    data: DimensionValueType = "VARCHAR"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("DimensionValueType", "MeasureValueType", "TableStatus", "TimeUnit")
+
+
+DimensionValueType = Literal["VARCHAR"]
+MeasureValueType = Literal["BIGINT", "BOOLEAN", "DOUBLE", "VARCHAR"]
+TableStatus = Literal["ACTIVE", "DELETING"]
+TimeUnit = Literal["MICROSECONDS", "MILLISECONDS", "NANOSECONDS", "SECONDS"]
