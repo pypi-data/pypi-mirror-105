935fc399aa7dcb95631c0d8811f570ca2a66aad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:13 +0000

    Add package sli/0.0.0.0a5/sli-0.0.0.0a5.tar.gz

---
+++ packages/sli/sli-0.0.0.0a5.tar.gz/sli-0.0.0.0a5/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sli",
+    version="0.0.0.0a5",
+    author="Andrew Mallory",
+    author_email="amallory@paloaltonetworks.com",
+    description="Document generation library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://paloaltonetworks.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'skilletlib',
+        'panforge',
+        'click',
+        'pycryptodome',
+        'paramiko',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sli= sli.cli:cli',
+        ]
+    },
+)
