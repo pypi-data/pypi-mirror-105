4a8eca778ea22b37557513cde9e81db880a54da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:53 +0000

    Add package sc-toolbox/0.5.0/sc_toolbox-0.5.0-py3-none-any.whl

---
+++ packages/sc-toolbox/sc_toolbox-0.5.0-py3-none-any.whl/sc_toolbox/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for sc-toolbox."""
+
+__author__ = "Lukas Heumos"
+__email__ = "lukas.heumos@posteo.net"
+__version__ = "0.5.0"
+
+from sc_toolbox.api import plot
+from sc_toolbox.api import calc
+from sc_toolbox.api import util
