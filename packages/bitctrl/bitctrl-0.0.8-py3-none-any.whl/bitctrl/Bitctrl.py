51ec199b26ef6812beaecfcfdd37e191f46278a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:26 +0000

    Add package bitctrl/0.0.8/bitctrl-0.0.8-py3-none-any.whl

---
+++ packages/bitctrl/bitctrl-0.0.8-py3-none-any.whl/bitctrl/Bitctrl.py
@@ -0,0 +1,61 @@
+#I have never made a module before, so if you got any advice feel free to tell me.
+
+#IMPORTANT:
+#TO USE THIS WITH MICROBIT, YOUR MICROBIT WILL HAVE TO TURN THIS PYTHON SCRIPT INTO A .hex FILE AND INSTALL IT ON TO YOUR MICROBIT:
+
+# serial.redirect_to_usb()
+# def on_forever():
+#     if input.button_is_pressed(Button.AB):
+#         while input.button_is_pressed(Button.AB):
+#             serial.write_line("A")
+#             pause(15)
+#     if input.button_is_pressed(Button.A):
+#         while input.button_is_pressed(Button.A):
+#             serial.write_line("A")
+#             pause(15)
+#     if input.button_is_pressed(Button.B):
+#         while input.button_is_pressed(Button.B):
+#             serial.write_line("B")
+#             pause(15)
+# forever(on_forever)
+#If you plan on using this in pygame, your milliseconds in "pause" should be half of that number,
+#and then half of that number; so if you have 60 fps in pygame, you divide that by two, so 30, then divide
+#30 by two, which is 15 - hence why I have it pausing for 15 milliseconds
+
+import serial
+import keyboard
+import time
+
+#When bitctrl returns something, like 'B', it sends back the string 'B' which you can use for other stuff in your main script(s). Example:
+# from bitctrl import Bitctrl
+# while True:
+#     bitctrl = Bitctrl.bitctrl('a','b','c')
+#     if bitctrl == 'B':
+#         print("epic")
+
+#Get ports
+s_inst = serial.Serial()
+#The microbit's baudrate
+s_inst.baudrate = 115200
+s_inst.port ='COM3'
+s_inst.open()
+
+
+def bitctrl(buttonA,buttonB,buttonAB,keyboard_press=True):
+    #'while True:' remove if you want to use in pygame, basically if you want to use it in another while loop. If you wish to always listen, then remove the comment from while True:
+    #but be sure not to use it inside of another while loop, or loop then. Because it will stop all other code from running, so it is best to have it commented out, and implement that function
+    #in your own script, like the example shown above.
+        if s_inst.in_waiting:
+            packet = s_inst.readline().decode('utf')
+            if packet.strip() == 'AB':
+                if keyboard_press:
+                    keyboard.press_and_release(buttonAB)
+                return 'AB'
+            if packet.strip() == 'A':
+                if keyboard_press:
+                    keyboard.press_and_release(buttonA)
+                return 'A'
+            if packet.strip() == 'B':
+                if keyboard_press == True:
+                    keyboard.press_and_release(buttonB)
+                return 'B'
