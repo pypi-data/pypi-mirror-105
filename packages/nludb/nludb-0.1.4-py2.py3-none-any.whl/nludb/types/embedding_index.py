0d64c87225f72762a0fe7666192737359e409c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:47 +0000

    Add package nludb/0.1.4/nludb-0.1.4-py2.py3-none-any.whl

---
+++ packages/nludb/nludb-0.1.4-py2.py3-none-any.whl/nludb/types/embedding_index.py
@@ -0,0 +1,69 @@
+from typing import List
+from dataclasses import dataclass
+from nludb.types.base import NludbRequest, NludbResponse
+from nludb.types.search import Hit
+@dataclass
+class IndexCreateRequest(NludbRequest):
+  name: str
+  model: str
+  upsert: bool = True
+  externalId: str = None
+  externalType: str = None
+  metadata: any = None
+
+@dataclass
+class IndexCreateResponse(NludbResponse):
+  id: str
+
+@dataclass
+class IndexInsertRequest(NludbRequest):
+  indexId: str
+  value: str
+  externalId: str = None
+  externalType: str = None
+  metadata: any = None
+  reindex: bool = True
+
+@dataclass
+class IndexInsertResponse(NludbResponse):
+  indexId: str = None
+  id: str = None
+
+  @staticmethod
+  def safely_from_dict(d: any) -> "IndexInsertResponse":
+    return IndexInsertResponse(
+      indexId = d.get('indexId', None),
+      id = d.get('id', None)
+    )
+
+@dataclass
+class IndexEmbedRequest(NludbRequest):
+  indexId: str
+
+@dataclass
+class IndexEmbedResponse(NludbResponse):
+  indexId: str
+
+  @staticmethod
+  def safely_from_dict(d: any) -> "IndexEmbedResponse":
+    return IndexEmbedResponse(
+      indexId = d.get('indexId', None)
+    )
+
+@dataclass
+class IndexSearchRequest(NludbRequest):
+  indexId: str
+  query: str
+  k: int = 1
+  includeMetadata: bool = False
+
+@dataclass
+class IndexSearchResponse(NludbResponse):
+  hits: List[Hit] = None
+
+  @staticmethod
+  def safely_from_dict(d: any) -> "IndexSearchResponse":
+    hits = [Hit.safely_from_dict(h) for h in d.get("hits", [])]
+    return IndexSearchResponse(
+      hits=hits
+    )
