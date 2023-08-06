af9e39e2d2e421bee0f41487ce0551b02d806a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:52 +0000

    Add package mypy-boto3-cloudhsmv2/1.17.69.0.post1/mypy-boto3-cloudhsmv2-1.17.69.0.post1.tar.gz

---
+++ packages/mypy-boto3-cloudhsmv2/mypy-boto3-cloudhsmv2-1.17.69.0.post1.tar.gz/mypy-boto3-cloudhsmv2-1.17.69.0.post1/mypy_boto3_cloudhsmv2/literals.py
@@ -0,0 +1,51 @@
+"""
+Type annotations for cloudhsmv2 service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_cloudhsmv2.literals import BackupPolicy
+
+    data: BackupPolicy = "DEFAULT"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "BackupPolicy",
+    "BackupRetentionType",
+    "BackupState",
+    "ClusterState",
+    "DescribeBackupsPaginatorName",
+    "DescribeClustersPaginatorName",
+    "HsmState",
+    "ListTagsPaginatorName",
+)
+
+
+BackupPolicy = Literal["DEFAULT"]
+BackupRetentionType = Literal["DAYS"]
+BackupState = Literal["CREATE_IN_PROGRESS", "DELETED", "PENDING_DELETION", "READY"]
+ClusterState = Literal[
+    "ACTIVE",
+    "CREATE_IN_PROGRESS",
+    "DEGRADED",
+    "DELETED",
+    "DELETE_IN_PROGRESS",
+    "INITIALIZED",
+    "INITIALIZE_IN_PROGRESS",
+    "UNINITIALIZED",
+    "UPDATE_IN_PROGRESS",
+]
+DescribeBackupsPaginatorName = Literal["describe_backups"]
+DescribeClustersPaginatorName = Literal["describe_clusters"]
+HsmState = Literal["ACTIVE", "CREATE_IN_PROGRESS", "DEGRADED", "DELETED", "DELETE_IN_PROGRESS"]
+ListTagsPaginatorName = Literal["list_tags"]
