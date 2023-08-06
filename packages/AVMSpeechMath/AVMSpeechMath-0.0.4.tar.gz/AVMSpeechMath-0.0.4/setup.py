a1df8812685e2bea68557991ea9aa735b891a269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:32 +0000

    Add package AVMSpeechMath/0.0.4/AVMSpeechMath-0.0.4.tar.gz

---
+++ packages/AVMSpeechMath/AVMSpeechMath-0.0.4.tar.gz/AVMSpeechMath-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AVMSpeechMath",
+    version="0.0.4",
+    author="AlejandroV",
+    author_email="avmmodules@gmail.com",
+    description="Perform voice operations in Spanish with Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avmmodules/AVMSpeechMath",
+    project_urls={
+        "Bug Tracker": "https://github.com/avmmodules/AVMSpeechMath/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+)
