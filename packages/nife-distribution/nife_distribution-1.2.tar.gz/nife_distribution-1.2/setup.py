419c430f0462707210859e6e75dcdbf259131288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:07 +0000

    Add package nife-distribution/1.2/nife_distribution-1.2.tar.gz

---
+++ packages/nife-distribution/nife_distribution-1.2.tar.gz/nife_distribution-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nife_distribution',
+      version='1.2',
+      description='Gaussian and Binomial  distributions',
+      packages=['nife_distribution'],
+      author = 'Nifesimi Ademoye',
+      author_email = 'nifesimifrank@gmail.com',
+      zip_safe=False)
