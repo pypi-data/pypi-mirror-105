6032c689ae4aa074c55f37e2f0d91b6e3098ec88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:22 +0000

    Add package pytorchvideo-nightly/0.1.0.post20210510/pytorchvideo-nightly-0.1.0.post20210510.tar.gz

---
+++ packages/pytorchvideo-nightly/pytorchvideo-nightly-0.1.0.post20210510.tar.gz/pytorchvideo-nightly-0.1.0.post20210510/pytorchvideo/data/ucf101.py
@@ -0,0 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+from .labeled_video_dataset import labeled_video_dataset
+
+
+"""
+    Action recognition video dataset for UCF101
+    <https://www.crcv.ucf.edu/data/UCF101.php>
+"""
+Ucf101 = labeled_video_dataset
