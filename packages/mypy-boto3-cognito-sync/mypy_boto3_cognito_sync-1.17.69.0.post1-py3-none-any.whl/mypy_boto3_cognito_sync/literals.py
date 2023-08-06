76074917e047bf655e9330abf644f245ab0582a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package mypy-boto3-cognito-sync/1.17.69.0.post1/mypy_boto3_cognito_sync-1.17.69.0.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-cognito-sync/mypy_boto3_cognito_sync-1.17.69.0.post1-py3-none-any.whl/mypy_boto3_cognito_sync/literals.py
@@ -0,0 +1,28 @@
+"""
+Type annotations for cognito-sync service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_cognito_sync.literals import BulkPublishStatus
+
+    data: BulkPublishStatus = "FAILED"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("BulkPublishStatus", "Operation", "Platform", "StreamingStatus")
+
+
+BulkPublishStatus = Literal["FAILED", "IN_PROGRESS", "NOT_STARTED", "SUCCEEDED"]
+Operation = Literal["remove", "replace"]
+Platform = Literal["ADM", "APNS", "APNS_SANDBOX", "GCM"]
+StreamingStatus = Literal["DISABLED", "ENABLED"]
