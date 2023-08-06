100a5a876f44c03b8d7c6bcf16c7f0d42a9143ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:28 +0000

    Add package connect-openapi-client/22.0.6/connect-openapi-client-22.0.6.tar.gz

---
+++ packages/connect-openapi-client/connect-openapi-client-22.0.6.tar.gz/connect-openapi-client-22.0.6/connect/__init__.py
@@ -0,0 +1,8 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect Python OpenAPI Client.
+#
+# Copyright (c) 2021 Ingram Micro. All Rights Reserved.
+#
+import pkgutil
+
+__path__ = pkgutil.extend_path(__path__, __name__)
