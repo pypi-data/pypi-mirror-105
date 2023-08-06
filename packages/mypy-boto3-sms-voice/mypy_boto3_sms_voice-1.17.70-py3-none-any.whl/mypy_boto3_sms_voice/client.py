7aeecb630f4add8759d0a21017c669a2ed5a0c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:59 +0000

    Add package mypy-boto3-sms-voice/1.17.70/mypy_boto3_sms_voice-1.17.70-py3-none-any.whl

---
+++ packages/mypy-boto3-sms-voice/mypy_boto3_sms_voice-1.17.70-py3-none-any.whl/mypy_boto3_sms_voice/client.py
@@ -0,0 +1,144 @@
+"""
+Type annotations for sms-voice service client.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html)
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_sms_voice import SMSVoiceClient
+
+    client: SMSVoiceClient = boto3.client("sms-voice")
+    ```
+"""
+from typing import Any, Dict, Type
+
+from botocore.client import ClientMeta
+
+from mypy_boto3_sms_voice.type_defs import (
+    EventDestinationDefinitionTypeDef,
+    GetConfigurationSetEventDestinationsResponseTypeDef,
+    ListConfigurationSetsResponseTypeDef,
+    SendVoiceMessageResponseTypeDef,
+    VoiceMessageContentTypeDef,
+)
+
+__all__ = ("SMSVoiceClient",)
+
+
+class BotocoreClientError(BaseException):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Dict[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AlreadyExistsException: Type[BotocoreClientError]
+    BadRequestException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    InternalServiceErrorException: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    NotFoundException: Type[BotocoreClientError]
+    TooManyRequestsException: Type[BotocoreClientError]
+
+
+class SMSVoiceClient:
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client)
+    [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html)
+    """
+
+    meta: ClientMeta
+    exceptions: Exceptions
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.can_paginate)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#can-paginate)
+        """
+
+    def create_configuration_set(self, ConfigurationSetName: str = None) -> Dict[str, Any]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.create_configuration_set)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#create-configuration-set)
+        """
+
+    def create_configuration_set_event_destination(
+        self,
+        ConfigurationSetName: str,
+        EventDestination: EventDestinationDefinitionTypeDef = None,
+        EventDestinationName: str = None,
+    ) -> Dict[str, Any]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.create_configuration_set_event_destination)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#create-configuration-set-event-destination)
+        """
+
+    def delete_configuration_set(self, ConfigurationSetName: str) -> Dict[str, Any]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.delete_configuration_set)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#delete-configuration-set)
+        """
+
+    def delete_configuration_set_event_destination(
+        self, ConfigurationSetName: str, EventDestinationName: str
+    ) -> Dict[str, Any]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.delete_configuration_set_event_destination)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#delete-configuration-set-event-destination)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = None,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#generate-presigned-url)
+        """
+
+    def get_configuration_set_event_destinations(
+        self, ConfigurationSetName: str
+    ) -> GetConfigurationSetEventDestinationsResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.get_configuration_set_event_destinations)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#get-configuration-set-event-destinations)
+        """
+
+    def list_configuration_sets(
+        self, NextToken: str = None, PageSize: str = None
+    ) -> ListConfigurationSetsResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.list_configuration_sets)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#list-configuration-sets)
+        """
+
+    def send_voice_message(
+        self,
+        CallerId: str = None,
+        ConfigurationSetName: str = None,
+        Content: VoiceMessageContentTypeDef = None,
+        DestinationPhoneNumber: str = None,
+        OriginationPhoneNumber: str = None,
+    ) -> SendVoiceMessageResponseTypeDef:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.send_voice_message)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#send-voice-message)
+        """
+
+    def update_configuration_set_event_destination(
+        self,
+        ConfigurationSetName: str,
+        EventDestinationName: str,
+        EventDestination: EventDestinationDefinitionTypeDef = None,
+    ) -> Dict[str, Any]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.70/reference/services/sms-voice.html#SMSVoice.Client.update_configuration_set_event_destination)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_sms_voice/client.html#update-configuration-set-event-destination)
+        """
