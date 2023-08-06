89816f0cf97f9799efeb086b5ba2caffc30cbdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:16 +0000

    Add package mypy-boto3-directconnect/1.17.71/mypy-boto3-directconnect-1.17.71.tar.gz

---
+++ packages/mypy-boto3-directconnect/mypy-boto3-directconnect-1.17.71.tar.gz/mypy-boto3-directconnect-1.17.71/mypy_boto3_directconnect/literals.py
@@ -0,0 +1,89 @@
+"""
+Type annotations for directconnect service literal definitions.
+
+[Open documentation](./literals.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_directconnect.literals import AddressFamily
+
+    data: AddressFamily = "ipv4"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "AddressFamily",
+    "BGPPeerState",
+    "BGPStatus",
+    "ConnectionState",
+    "DescribeDirectConnectGatewayAssociationsPaginatorName",
+    "DescribeDirectConnectGatewayAttachmentsPaginatorName",
+    "DescribeDirectConnectGatewaysPaginatorName",
+    "DirectConnectGatewayAssociationProposalState",
+    "DirectConnectGatewayAssociationState",
+    "DirectConnectGatewayAttachmentState",
+    "DirectConnectGatewayAttachmentType",
+    "DirectConnectGatewayState",
+    "GatewayType",
+    "HasLogicalRedundancy",
+    "InterconnectState",
+    "LagState",
+    "LoaContentType",
+    "VirtualInterfaceState",
+)
+
+
+AddressFamily = Literal["ipv4", "ipv6"]
+BGPPeerState = Literal["available", "deleted", "deleting", "pending", "verifying"]
+BGPStatus = Literal["down", "unknown", "up"]
+ConnectionState = Literal[
+    "available",
+    "deleted",
+    "deleting",
+    "down",
+    "ordering",
+    "pending",
+    "rejected",
+    "requested",
+    "unknown",
+]
+DescribeDirectConnectGatewayAssociationsPaginatorName = Literal[
+    "describe_direct_connect_gateway_associations"
+]
+DescribeDirectConnectGatewayAttachmentsPaginatorName = Literal[
+    "describe_direct_connect_gateway_attachments"
+]
+DescribeDirectConnectGatewaysPaginatorName = Literal["describe_direct_connect_gateways"]
+DirectConnectGatewayAssociationProposalState = Literal["accepted", "deleted", "requested"]
+DirectConnectGatewayAssociationState = Literal[
+    "associated", "associating", "disassociated", "disassociating", "updating"
+]
+DirectConnectGatewayAttachmentState = Literal["attached", "attaching", "detached", "detaching"]
+DirectConnectGatewayAttachmentType = Literal["PrivateVirtualInterface", "TransitVirtualInterface"]
+DirectConnectGatewayState = Literal["available", "deleted", "deleting", "pending"]
+GatewayType = Literal["transitGateway", "virtualPrivateGateway"]
+HasLogicalRedundancy = Literal["no", "unknown", "yes"]
+InterconnectState = Literal[
+    "available", "deleted", "deleting", "down", "pending", "requested", "unknown"
+]
+LagState = Literal["available", "deleted", "deleting", "down", "pending", "requested", "unknown"]
+LoaContentType = Literal["application/pdf"]
+VirtualInterfaceState = Literal[
+    "available",
+    "confirming",
+    "deleted",
+    "deleting",
+    "down",
+    "pending",
+    "rejected",
+    "unknown",
+    "verifying",
+]
